958d69171c039ecff093ec7df91e9d2900428e9a
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2019-04-12 09:10:13 +1200

    A094914

--- src/irvine/oeis/a094/A094914.java
+++ src/irvine/oeis/a094/A094914.java
@@ -1,6 +1,8 @@
 package irvine.oeis.a094;
+// 2019-04-11, Georg Fischer: abs() inserted
 
 import irvine.oeis.LinearRecurrence;
+import irvine.math.z.Z;
 
 /**
  * A094914 Absolute value of n^2 + n - 1354363.
@@ -11,5 +13,10 @@ public class A094914 extends LinearRecurrence {
   /** Construct the sequence. */
   public A094914() {
     super(new long[] {1, -3, 3}, new long[] {1354361, 1354357, 1354351});
+  }
+  
+  @Override
+  public Z next() {
+  	return super.next().abs();
   }
 }

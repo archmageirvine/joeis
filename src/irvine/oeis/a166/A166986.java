450789774f18ffe01f2ca420957518bcfcce4ef0
Sean A. Irvine <sairvin@gmail.com>
2019-04-13 09:26:18 +1200

    A166986

--- src/irvine/oeis/a166/A166986.java
+++ src/irvine/oeis/a166/A166986.java
@@ -1,15 +1,20 @@
 package irvine.oeis.a166;
 
-import irvine.oeis.LinearRecurrence;
+import irvine.math.cr.CR;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
 
 /**
  * A166986 <code>a(n) =</code> 2*floor((n+2)/log(2)) <code>- 4 =</code> 2*Sum_{i=2,..,n+1}(A022934(i)).
  * @author Sean A. Irvine
  */
-public class A166986 extends LinearRecurrence {
+public class A166986 implements Sequence {
 
-  /** Construct the sequence. */
-  public A166986() {
-    super(new long[] {-1, 1, 0, 0, 0, 0, 0, 0, 0, 1}, new long[] {4, 6, 10, 12, 16, 18, 20, 24, 26, 30});
+  private static final CR LOG2 = CR.TWO.log();
+  private long mN = 2;
+
+  @Override
+  public Z next() {
+    return CR.valueOf(++mN).divide(LOG2).floor(32).multiply2().subtract(4);
   }
 }

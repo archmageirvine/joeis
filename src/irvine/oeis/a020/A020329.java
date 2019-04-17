9331ba369a125760596b280a55d09b1781a28853
Sean A. Irvine <sairvin@gmail.com>
2019-04-17 15:58:21 +1200

    Sequences

--- src/irvine/oeis/a020/A020329.java
+++ src/irvine/oeis/a020/A020329.java
@@ -12,6 +12,7 @@ import irvine.oeis.Sequence;
  */
 public class A020329 implements Sequence {
 
+private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
   private final TreeSet<Z> mC = new TreeSet<>();
   private long mN = 3;
 
@@ -19,6 +20,9 @@ public class A020329 implements Sequence {
   public Z next() {
     while (true) {
       final Z c = Binomial.binomial(++mN, 4);
+      if (mVerbose && mN % 10000 == 0) {
+        System.out.println("n=" + mN);
+      }
       boolean found = false;
       for (final Z v : mC) {
         if (mC.contains(c.subtract(v))) {

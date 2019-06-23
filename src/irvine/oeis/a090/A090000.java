aec718b98094759e43f24d58c3ef12005f075e57
Sean A. Irvine <sairvin@gmail.com>
2019-06-23 22:15:52 +1200

    A090000

---
+++ src/irvine/oeis/a090/A090000.java
@@ -0,0 +1,33 @@
+package irvine.oeis.a090;
+
+import irvine.math.z.Z;
+import irvine.oeis.a000.A000040;
+
+/**
+ * A090000 Length of longest contiguous block of 1's in binary expansion of n-th prime.
+ * @author jmason
+ * @author Sean A. Irvine
+ */
+public class A090000 extends A000040 {
+
+  /* Calculate the maximum length of longest contiguous block of 1's in binary expansion of n */
+  private int maxBitLength(Z p) {
+    int currMax = 0;
+    int currLen = 0;
+    for (int k = 0; k < p.bitLength(); ++k) {
+      if (p.testBit(k)) {
+        if (++currLen > currMax) {
+          currMax = currLen;
+        }
+      } else {
+        currLen = 0;
+      }
+    }
+    return currMax;
+  }
+
+  @Override
+  public Z next() {
+    return Z.valueOf(maxBitLength(super.next()));
+  }
+}
--- test/irvine/oeis/a090/AllTests.java
+++ test/irvine/oeis/a090/AllTests.java
@@ -12,6 +12,7 @@ public class AllTests extends TestSuite {
 
   public static Test suite() {
     final TestSuite suite = new TestSuite();
+    suite.addTestSuite(A090000Test.class);
     suite.addTestSuite(A090017Test.class);
     suite.addTestSuite(A090018Test.class);
     suite.addTestSuite(A090019Test.class);

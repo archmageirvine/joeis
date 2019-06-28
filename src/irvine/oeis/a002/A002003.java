94048f352d322723a3385ac9b4b991590610d723
Sean A. Irvine <sairvin@gmail.com>
2019-06-29 10:31:24 +1200

    Regenerate bas sequences

--- src/irvine/oeis/a002/A002003.java
+++ src/irvine/oeis/a002/A002003.java
@@ -5,7 +5,7 @@ import irvine.math.z.Z;
 import irvine.oeis.Sequence;
 
 /**
- * A002003 <code>2*Sum_{k=0..n-1} C(n-1,k)*C(n+k,k)</code>.
+ * A002003 <code>a(n) = 2 * Sum_{k=0..n-1} C(n-1,k)*C(n+k,k)</code>.
  * @author Sean A. Irvine
  */
 public class A002003 implements Sequence {
--- src/irvine/oeis/a024/A024274.java
+++ src/irvine/oeis/a024/A024274.java
@@ -4,7 +4,7 @@ import irvine.math.z.Z;
 import irvine.oeis.a009.A009683;
 
 /**
- * A024274 Expansion of <code>tan(sinh(x)).x/2</code>.
+ * A024274 Expansion of e.g.f. <code>tan(sinh(x))*x/2</code> (even powers only).
  * @author Sean A. Irvine
  */
 public class A024274 extends A009683 {

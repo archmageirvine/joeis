572b0cc507a336be33afe552efc86d4a92a7b73a
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2019-09-16 14:33:38 +1200

    Sequence

---
+++ src/irvine/oeis/a161/A161791.java
@@ -0,0 +1,31 @@
+package irvine.oeis.a161;
+
+import irvine.factor.factor.Cheetah;
+import irvine.math.LongUtils;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A161791.
+ * @author Sean A. Irvine
+ */
+public class A161791 implements Sequence {
+
+  private long mN = 3;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final long r = LongUtils.reverse(++mN);
+      for (final Z dd : Cheetah.factor(mN).divisors()) {
+        final long d = dd.longValue();
+        if (2 * d > mN) {
+          break;
+        }
+        if (d > 1 && d + mN / d == r) {
+          return Z.valueOf(mN);
+        }
+      }
+    }
+  }
+}

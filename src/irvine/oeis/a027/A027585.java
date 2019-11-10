03b8cb34ad35b2ab392a92e343bda0f5b4ce5668
Sean A. Irvine <sairvin@gmail.com>
2019-11-10 14:45:44 +1300

    Sequence

---
+++ src/irvine/oeis/a027/A027585.java
@@ -0,0 +1,23 @@
+package irvine.oeis.a027;
+
+import irvine.math.polynomial.Polynomial;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A027585.
+ * @author Sean A. Irvine
+ */
+public class A027585 implements Sequence {
+
+  private Polynomial<Z> mA = Polynomial.create(0, 1, 2, 3);
+  private int mN = 0;
+
+  @Override
+  public Z next() {
+    if (++mN > mA.degree()) {
+      mA = A027584.specialPartitionTransform(A027584.specialPartitionTransform(mA, mN + 1), mN);
+    }
+    return mA.coeff(mN);
+  }
+}

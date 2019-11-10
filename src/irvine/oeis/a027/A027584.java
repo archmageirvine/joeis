a1f6de0db3b7c2ec13e52a657236dd6833c92c6b
Sean A. Irvine <sairvin@gmail.com>
2019-11-10 13:27:22 +1300

    Sequence

---
+++ src/irvine/oeis/a027/A027584.java
@@ -0,0 +1,44 @@
+package irvine.oeis.a027;
+
+import irvine.math.group.IntegerField;
+import irvine.math.group.PolynomialRingField;
+import irvine.math.polynomial.Polynomial;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A027584.
+ * @author Sean A. Irvine
+ */
+public class A027584 implements Sequence {
+
+  private static final PolynomialRingField<Z> RING = new PolynomialRingField<>(IntegerField.SINGLETON);
+  private Polynomial<Z> mA = Polynomial.create(0, 1, 2, 3, 4, 5);
+  private int mN = 0;
+
+  private Polynomial<Z> partitionTransform(final Polynomial<Z> a, final int n) {
+    Polynomial<Z> num = RING.one();
+    Polynomial<Z> den = RING.one();
+    int prev = 0;
+    for (final Z z : a) {
+      final int u = z.intValueExact();
+      if (u > prev) { // distinct values
+        if ((u & 1) == 0) {
+          num = RING.multiply(num, RING.onePlusXToTheN(u), n);  // even u occurs at most one
+        } else {
+          den = RING.multiply(den, RING.oneMinusXToTheN(u), n); // odd u any number of times
+        }
+        prev = u;
+      }
+    }
+    return RING.series(num, den, n);
+  }
+
+  @Override
+  public Z next() {
+    if (++mN > mA.degree()) {
+      mA = partitionTransform(partitionTransform(mA, mN + 1), mN);
+    }
+    return mA.coeff(mN);
+  }
+}

57ca150b9b42a5947dc55242064ae3f80501c888
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2019-11-22 10:21:06 +1300

    Sequences

---
+++ src/irvine/oeis/a027/A027680.java
@@ -0,0 +1,13 @@
+package irvine.oeis.a027;
+
+/**
+ * A027680.
+ * @author Sean A. Irvine
+ */
+public class A027680 extends A027667 {
+
+  @Override
+  protected int pathLength() {
+    return 5;
+  }
+}

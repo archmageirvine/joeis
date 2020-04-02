a72d423dfa67eea417c85a394a7d752270203e5d
Sean A. Irvine <sairvin@gmail.com>
2020-04-02 17:06:02 +1300

    Contribution from Georg Fischer

---
+++ test/irvine/oeis/a038/A038247Test.java
@@ -0,0 +1,10 @@
+package irvine.oeis.a038;
+
+import irvine.oeis.AbstractSequenceTest;
+
+/**
+ * Tests the corresponding class.
+ * @author Sean A. Irvine
+ */
+public class A038247Test extends AbstractSequenceTest {
+}
---
+++ test/irvine/oeis/a038/A038336Test.java
@@ -0,0 +1,10 @@
+package irvine.oeis.a038;
+
+import irvine.oeis.AbstractSequenceTest;
+
+/**
+ * Tests the corresponding class.
+ * @author Sean A. Irvine
+ */
+public class A038336Test extends AbstractSequenceTest {
+}

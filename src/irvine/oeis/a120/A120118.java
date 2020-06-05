f0000db4b6b2b3599aafafdd899189cf9063ec5b
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-06-05 17:02:20 +1200

    spelling

--- src/irvine/oeis/a120/A120118.java
+++ src/irvine/oeis/a120/A120118.java
@@ -10,6 +10,6 @@ public class A120118 extends LinearRecurrence {
 
   /** Construct the sequence. */
   public A120118() {
-    super(new long[] {-1, 0, -1, 0, 0, 2, 0, 1, 0, 1}, new long[] {2, 4, 7, 11, 16, 26, 43, 71, 116, 186});
+    super(new long[] {-1, 0, -1, 0, 0, 2, 0, 1, 0, 1}, new long[] {1, 2, 4, 7, 11, 16, 26, 43, 71, 116});
   }
 }

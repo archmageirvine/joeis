0ca89ab81559ffed6ecb862a56ec0d323ca45ba4
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-06-10 11:25:58 +1200

    PositionSequence

---
+++ src/irvine/oeis/PositionSequence.java
@@ -0,0 +1,48 @@
+package irvine.oeis;
+
+import irvine.math.z.Z;
+
+/**
+ * A sequence consisting of position of consecutive records in another sequence.
+ * Derived from <code>RecordPositionSequence</code>.
+ * @author Georg Fischer
+ */
+public class PositionSequence implements Sequence {
+
+  private final Sequence mSeq; // the underlying sequence
+  private Z mValue; // return the positions of this number
+  private long mN; // current index
+
+  /**
+   * Creates a sequence of the positions where a digit occurs in another sequence.
+   * @param seq underlying sequence
+   * @param offset offset of <code>seq</code>
+   * @param value the positions of this number are returned
+   */
+  public PositionSequence(final int offset, final Sequence seq, final long value) {
+    mSeq = seq;
+    mValue = Z.valueOf(value);
+    mN = offset - 1;
+  }
+
+  /**
+   * Determine whether the current number has the property which includes it in the sequence.
+   * @param term number to be tested
+   * This method may be overwritten in order to test some other property.
+   * @return true iff the current number <code>mK</code> has some property.
+   */
+  protected boolean isOk(Z term) {
+    return term.compareTo(mValue) == 0;
+  } // isOk
+
+  @Override
+  public Z next() {
+    while (true) {
+      ++mN;
+      final Z t = mSeq.next();
+      if (isOk(t)) {
+        return Z.valueOf(mN);
+      }
+    }
+  }
+}

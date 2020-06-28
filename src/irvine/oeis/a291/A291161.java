4e1f85da99498757b293f11f3cd1ed860120187d
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-06-29 10:50:14 +1200

    Finite sequences

---
+++ src/irvine/oeis/a291/A291161.java
@@ -0,0 +1,15 @@
+package irvine.oeis.a291;
+
+import irvine.oeis.FiniteSequence;
+
+/**
+ * A291161 Sequence of black numbers on the double zero (American) roulette wheel read clockwise.
+ * @author Sean A. Irvine
+ */
+public class A291161 extends FiniteSequence {
+
+  /** Construct the sequence. */
+  public A291161() {
+    super(28, 26, 11, 20, 17, 22, 15, 24, 13, 10, 29, 8, 31, 6, 33, 4, 35, 2, 28, 26, 11, 20, 17, 22, 15, 24, 13, 10, 29, 8, 31, 6, 33, 4, 35, 2);
+  }
+}
---
+++ src/irvine/oeis/a291/A291162.java
@@ -0,0 +1,15 @@
+package irvine.oeis.a291;
+
+import irvine.oeis.FiniteSequence;
+
+/**
+ * A291162 Sequence of black numbers on the double zero (American) roulette wheel read counterclockwise.
+ * @author Sean A. Irvine
+ */
+public class A291162 extends FiniteSequence {
+
+  /** Construct the sequence. */
+  public A291162() {
+    super(2, 35, 4, 33, 6, 31, 8, 29, 10, 13, 24, 15, 22, 17, 20, 11, 26, 28, 2, 35, 4, 33, 6, 31, 8, 29, 10, 13, 24, 15, 22, 17, 20, 11, 26, 28);
+  }
+}

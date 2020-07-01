0b20d00e97a593f862e73b4dd1505e1764d19939
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-07-01 18:21:57 +1200

    Sequences

---
+++ src/irvine/oeis/a085/A085753.java
@@ -0,0 +1,29 @@
+package irvine.oeis.a085;
+
+import irvine.math.z.Z;
+import irvine.oeis.SemiprimeSequence;
+import irvine.oeis.Sequence;
+
+/**
+ * A085753.
+ * @author Sean A. Irvine
+ */
+public class A085753 implements Sequence {
+
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private int mN = 0;
+
+  @Override
+  public Z next() {
+    final Z nn = Z.valueOf(++mN).pow(mN);
+    if (mVerbose) {
+      System.out.println("[" + mN + "]");
+    }
+    return new SemiprimeSequence() {
+      @Override
+      protected Z candidate(final int n) {
+        return nn.add(n);
+      }
+    }.next();
+  }
+}
---
+++ src/irvine/oeis/a085/A085917.java
@@ -0,0 +1,29 @@
+package irvine.oeis.a085;
+
+import irvine.math.z.Z;
+import irvine.oeis.SemiprimeSequence;
+import irvine.oeis.Sequence;
+
+/**
+ * A085917.
+ * @author Sean A. Irvine
+ */
+public class A085917 implements Sequence {
+
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private int mN = 0;
+
+  @Override
+  public Z next() {
+    ++mN;
+    if (mVerbose) {
+      System.out.println("[" + mN + "]");
+    }
+    return new SemiprimeSequence(1) {
+      @Override
+      protected Z candidate(final int n) {
+        return Z.valueOf(n).shiftLeft(mN).subtract(1);
+      }
+    }.next();
+  }
+}

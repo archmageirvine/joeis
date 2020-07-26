d17448451dd33a2cbd59e8cd81065f91ac26d751
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-07-27 10:47:06 +1200

    A033961 working

--- src/irvine/oeis/a033/A033961.java
+++ src/irvine/oeis/a033/A033961.java
@@ -1,7 +1,6 @@
 package irvine.oeis.a033;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,8 +13,6 @@ import irvine.util.Point;
  * @author Sean A. Irvine
  */
 public class A033961 implements Sequence {
-
-  // todo odd cases is working, even is not
 
   // See Iuliana Dochkova-Todorova, "Computer Methods and New Values for Cut Set Catalan Numbers"
 
@@ -40,7 +37,7 @@ public class A033961 implements Sequence {
     throw new RuntimeException();
   }
 
-  private boolean ok(final long v, final List<Point> pts, final HashMap<Point, Long> lambda, final int i) {
+  private boolean ok(final long v, final List<Point> pts, final HashMap<Point, Integer> lambda, final int i) {
     long t = v;
     for (final Point p : pts) {
       t += zeta(p.left(), p.right(), i) * lambda.get(p);
@@ -48,23 +45,22 @@ public class A033961 implements Sequence {
     return t >= 0;
   }
 
-  private boolean bump(final List<Point> pts, final HashMap<Point, Long> lambda) {
-    for (int k = 0; k < pts.size(); ++k) {
-      final Point p = pts.get(k);
+  private boolean bump(final List<Point> pts, final HashMap<Point, Integer> lambda) {
+    for (final Point p : pts) {
       final int a = p.left();
       final int b = p.right();
       final int lim = (mN + 2) / (a + b + 2);
-      final long v = lambda.get(p);
+      final int v = lambda.get(p);
       if (v < lim) {
         lambda.put(p, v + 1);
         return true;
       }
-      lambda.put(p, 0L);
+      lambda.put(p, 0);
     }
     return false;
   }
 
-  private Z count(final List<Point> allowed,  final HashMap<Point, Long> lambda, final int i) {
+  private Z count(final List<Point> allowed, final HashMap<Point, Integer> lambda, final int i) {
     if (i > (mN - 3) / 2) {
       return Z.ONE;
     }
@@ -73,12 +69,12 @@ public class A033961 implements Sequence {
     final List<Point> pts = new ArrayList<>();
     for (final Point p : allowed) {
       final long zeta = zeta(p.left(), p.right(), i);
-      final Long fix = lambda.get(p);
+      final Integer fix = lambda.get(p);
       if (fix != null) {
         v += zeta * fix;
       } else if (zeta != 0) {
         pts.add(p);
-        lambda.put(p, 0L);
+        lambda.put(p, 0);
       }
     }
     //System.out.println(i + " " + v + " " + pts);
@@ -100,55 +96,11 @@ public class A033961 implements Sequence {
 
   }
 
-  private void dumpEqns(final List<Point> allowed,  final HashMap<Point, Long> lambda) {
-    final long[] v = new long[(mN - 3) / 2 + 1];
-    Arrays.fill(v, 2);
-    final List<List<Point>> eqn = new ArrayList<>();
-    eqn.add(null);
-    eqn.add(null);
-    for (int i = 2; i <= (mN - 3) / 2; ++i) {
-      final List<Point> pts = new ArrayList<>();
-      eqn.add(pts);
-      final StringBuilder sb = new StringBuilder("2");
-      for (final Point p : allowed) {
-        final long zeta = zeta(p.left(), p.right(), i);
-        final Long fix = lambda.get(p);
-        if (fix != null) {
-          v[i] += zeta * fix;
-        } else if (zeta != 0) {
-          pts.add(p);
-        }
-        if (zeta != 0) {
-          if (zeta > 0) {
-            sb.append('+');
-            if (zeta > 1) {
-              sb.append(zeta);
-            }
-            sb.append(p);
-          } else {
-            sb.append('-');
-            if (zeta < -1) {
-              sb.append(-zeta);
-            }
-            sb.append(p);
-          }
-        }
-      }
-      System.out.println("Eqn(" + i + ") -> " + sb.toString());
-    }
-    System.out.println("Reduced equations (note the zeta terms not included here:");
-    for (int k = 2; k < v.length; ++k) {
-      System.out.println(v[k] + " " + eqn.get(k));
-    }
-  }
-
   @Override
   public Z next() {
     ++mN;
-    System.out.println("n=" + mN);
-    final HashMap<Point, Long> lambda = new HashMap<>();
     Z sum = Z.ZERO;
-
+    final HashMap<Point, Integer> lambda = new HashMap<>();
     final ArrayList<Point> allowed = new ArrayList<>();
     for (int a = 1; 2 * a <= mN - 1 - a; ++a) {
       for (int b = a; 2 * b <= mN - 1 - a; ++b) {
@@ -161,18 +113,16 @@ public class A033961 implements Sequence {
 
       // Compute all allowed variables lambda_{a,b}
       final ArrayList<Point> subAllowed = new ArrayList<>();
-      for (int a = 1; 2 * a <= mN - 1 - a; ++a) {
-        for (int b = a; 2 * b <= mN - 1 - a; ++b) {
-          if (a + b >= (mN - 1) / 2) {
-            subAllowed.add(new Point(a, b));
-          }
+      for (final Point p : allowed) {
+        if (p.left() + p.right() >= (mN - 1) / 2) {
+          subAllowed.add(p);
         }
       }
 
       // Using the last equation from (4) we choose exactly one lambda to be 1
       for (int k = -1; k < subAllowed.size(); ++k) { // -1 lets all variables be 0
         for (int j = 0; j < subAllowed.size(); ++j) {
-          lambda.put(subAllowed.get(j), j == k ? 1L : 0L);
+          lambda.put(subAllowed.get(j), j == k ? 1 : 0);
         }
         //dumpEqns(allowed, lambda);
         sum = sum.add(count(allowed, lambda, 2));
@@ -181,50 +131,50 @@ public class A033961 implements Sequence {
       // even
 
       // Compute all allowed variables lambda_{a,b}
-      final ArrayList<Point> subAllowed = new ArrayList<>();
-      for (int a = 1; 2 * a <= mN - 1 - a; ++a) {
-        for (int b = a; 2 * b <= mN - 1 - a; ++b) {
-          if (a + b >= mN / 2) {
-            subAllowed.add(new Point(a, b));
-          }
+      final ArrayList<Point> subAllowed1 = new ArrayList<>();
+      for (final Point p : allowed) {
+        if (p.left() + p.right() == mN / 2 - 1) {
+          subAllowed1.add(p);
         }
       }
-
-      final ArrayList<Point> subAllowed1 = new ArrayList<>();
-      for (int a = 1; 2 * a <= mN - 1 - a; ++a) {
-        for (int b = a; 2 * b <= mN - 1 - a; ++b) {
-          if (a + b == mN / 2 - 1) {
-            subAllowed1.add(new Point(a, b));
-          }
+      final ArrayList<Point> subAllowed2 = new ArrayList<>();
+      for (final Point p : allowed) {
+        if (p.left() + p.right() >= mN / 2) {
+          subAllowed2.add(p);
         }
       }
 
       // Choose single variable from second set
       for (final Point p : subAllowed1) {
-        lambda.put(p, 0L);
+        lambda.put(p, 0);
       }
-      for (int k = -1; k < subAllowed.size(); ++k) { // -1 lets all variables be 0
-        for (int j = 0; j < subAllowed.size(); ++j) {
-          lambda.put(subAllowed.get(j), j == k ? 1L : 0L);
+      for (int k = -1; k < subAllowed2.size(); ++k) { // -1 lets all variables be 0
+        for (int j = 0; j < subAllowed2.size(); ++j) {
+          lambda.put(subAllowed2.get(j), j == k ? 1 : 0);
         }
-        //dumpEqns(allowed, lambda);
         sum = sum.add(count(allowed, lambda, 2));
       }
 
-      // Choose one or two of them k and j
-      for (final Point p : subAllowed) {
-        lambda.put(p, 0L);
+      // Choose two of them k and j
+      for (final Point p : subAllowed2) {
+        lambda.put(p, 0);
       }
-      for (int k = -1; k < subAllowed1.size(); ++k) { // -1 allows for a 0
+      for (int k = 0; k < subAllowed1.size() - 1; ++k) { // set two variables
         for (int j = k + 1; j < subAllowed1.size(); ++j) {
           for (int i = 0; i < subAllowed1.size(); ++i) {
-            lambda.put(subAllowed1.get(i), i == k || i == j ? 1L : 0L);
+            lambda.put(subAllowed1.get(i), i == k || i == j ? 1 : 0);
           }
+          sum = sum.add(count(allowed, lambda, 2));
         }
-        //dumpEqns(allowed, lambda);
-        sum = sum.add(count(allowed, lambda, 2));
       }
-
+      for (int v = 1; v <= 2; ++v) {
+        for (int k = 0; k < subAllowed1.size(); ++k) { // Set one variable to v
+          for (int i = 0; i < subAllowed1.size(); ++i) {
+            lambda.put(subAllowed1.get(i), i == k ? v : 0);
+          }
+          sum = sum.add(count(allowed, lambda, 2));
+        }
+      }
     }
 
     return sum;

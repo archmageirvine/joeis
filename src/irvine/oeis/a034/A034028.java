6c8c405dae326f4341379a36a164027410aaf45d
Sean A. Irvine <sairvin@gmail.com>
2020-08-02 10:39:02 +1200

    Stats

---
+++ src/irvine/oeis/a034/A034028.java
@@ -0,0 +1,15 @@
+package irvine.oeis.a034;
+
+import irvine.oeis.ComplementSequence;
+
+/**
+ * A034028.
+ * @author Sean A. Irvine
+ */
+public class A034028 extends ComplementSequence {
+
+  /** Construct the sequence. */
+  public A034028() {
+    super(new A034027());
+  }
+}

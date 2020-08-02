3593f8aefed344dfd6ede6e1146437bbce7a4613
Sean A. Irvine <sairvin@gmail.com>
2020-08-03 09:34:47 +1200

    Sequences

--- src/irvine/oeis/a003/A003338.java
+++ src/irvine/oeis/a003/A003338.java
@@ -13,13 +13,11 @@ public class A003338 extends A003336 {
     while (mA.isEmpty() || mA.first().compareTo(pow(mM)) > 0) {
       final Z mp1 = pow(mM);
       for (int k2 = 1; k2 <= mM; ++k2) {
+        final Z z2 = mp1.add(pow(k2));
         for (int k3 = k2; k3 <= mM; ++k3) {
+          final Z z3 = z2.add(pow(k3));
           for (int k4 = k3; k4 <= mM; ++k4) {
-            mA.add(mp1
-                .add(pow(k2))
-                .add(pow(k3))
-                .add(pow(k4))
-                ); 
+            mA.add(z3.add(pow(k4)));
           }
         }
       }

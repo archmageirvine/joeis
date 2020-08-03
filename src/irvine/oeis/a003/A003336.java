92df55a79408076f9dc7e565b3a0dc11d460905b
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-08-04 10:45:57 +1200

    Interim fix so it compiles

--- src/irvine/oeis/a003/A003336.java
+++ src/irvine/oeis/a003/A003336.java
@@ -14,10 +14,18 @@ public class A003336 implements Sequence {
 
   protected final TreeSet<Z> mA = new TreeSet<>();
   private final ArrayList<Z> mPowers = new ArrayList<>();
-  protected int mM = 1; // mM^power() is next to be considered
+  protected int mM = min(); // mM^power() is next to be considered
 
   protected int power() {
     return 4;
+  }
+
+  protected int min() {
+    return 1;
+  }
+
+  protected int numberTerms() {
+    return 2;
   }
 
   // Remembers powers, computing new powers as needed
@@ -32,7 +40,7 @@ public class A003336 implements Sequence {
   public Z next() {
     while (mA.isEmpty() || mA.first().compareTo(pow(mM)) > 0) {
       final Z mp = pow(mM);
-      for (int k = 1; k <= mM; ++k) {
+      for (int k = min(); k <= mM; ++k) {
         mA.add(mp.add(pow(k)));  // i.e. m^p + k^p, k >= 1
       }
       ++mM;

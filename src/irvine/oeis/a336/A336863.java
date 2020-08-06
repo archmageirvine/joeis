667eb971d3f5718659f8f89ae6165a3e6bfceead
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-08-06 14:31:50 +1200

    Sequences

---
+++ src/irvine/oeis/a336/A336863.java
@@ -0,0 +1,58 @@
+package irvine.oeis.a336;
+
+import java.util.HashSet;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+import irvine.util.Point;
+
+/**
+ * A336863.
+ * @author Sean A. Irvine
+ */
+public class A336863 implements Sequence {
+
+  private int mN = -2;
+  private long mCount = 0;
+
+  protected final HashSet<Point> mVisited = new HashSet<>();
+
+  {
+    mVisited.add(new Point(0, 0));
+    mVisited.add(new Point(1, 0));
+  }
+
+  private void search(final int n, final int x, final int y) {
+    if (n == 1) {
+      // Check if we are next to the origin
+      if (y == 0 && (x == 1 || x == -1)) {
+        ++mCount;
+      } else if (x == 0 && y + 2 * ((x + y) & 1) == 1) {
+        ++mCount;
+      }
+      return;
+    }
+    if (Math.abs(x) + Math.abs(y) > n) {
+      return; // Can never make it back in remaining steps
+    }
+    final Point v = new Point(x, y);
+    if (!mVisited.contains(v)) {
+      mVisited.add(v);
+      search(n - 1, x + 1, y);
+      search(n - 1, x - 1, y);
+      search(n - 1, x, y - 1 + 2 * ((x + y) & 1));
+      mVisited.remove(v);
+    }
+  }
+
+  @Override
+  public Z next() {
+    mN += 2;
+    if (mN == 0) {
+      return Z.ONE;
+    }
+    mCount = 0;
+    search(mN - 2, 1, 1);
+    return Z.valueOf(mCount);
+  }
+}

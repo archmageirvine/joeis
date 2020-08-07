8c2d53ccce718a97f5fb8ca5c15ef3293c130a87
Sean A. Irvine <sairvin@gmail.com>
2020-08-08 11:13:56 +1200

    Sequences

---
+++ src/irvine/oeis/a034/A034204.java
@@ -0,0 +1,23 @@
+package irvine.oeis.a034;
+
+import irvine.math.group.SymmetricGroup;
+import irvine.math.polynomial.CycleIndex;
+import irvine.math.polynomial.WreathExponentiation;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A034204.
+ * @author Sean A. Irvine
+ */
+public class A034204 implements Sequence {
+
+  private static final CycleIndex Z2 = SymmetricGroup.create(2).cycleIndex();
+  private int mN = 0;
+
+  @Override
+  public Z next() {
+    return WreathExponentiation.exponentiation(Z2, SymmetricGroup.create(++mN).cycleIndex()).applyOnePlusXToTheN(9).coeff(9).toZ();
+  }
+}
+
---
+++ src/irvine/oeis/a034/A034217.java
@@ -0,0 +1,28 @@
+package irvine.oeis.a034;
+
+import irvine.math.group.SymmetricGroup;
+import irvine.math.polynomial.CycleIndex;
+import irvine.math.polynomial.Polynomial;
+import irvine.math.polynomial.WreathExponentiation;
+import irvine.math.q.Q;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A034217.
+ * @author Sean A. Irvine
+ */
+public class A034217 implements Sequence {
+
+  private static final CycleIndex Z3 = SymmetricGroup.create(3).cycleIndex();
+  private static final CycleIndex Z5 = SymmetricGroup.create(5).cycleIndex();
+  private final Polynomial<Q> mA = WreathExponentiation.exponentiation(Z3, Z5).applyOnePlusXToTheN();
+  private int mN = -1;
+
+  @Override
+  public Z next() {
+    Z r = mA.coeff(++mN).toZ();
+    return Z.ZERO.equals(r) ? null : r;
+  }
+}
+

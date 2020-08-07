d8bacfc1e1e6e3d06d46183b83960889d50de945
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-08-07 14:28:13 +1200

    Honeycomb polygons

--- src/irvine/oeis/a005/A005396.java
+++ src/irvine/oeis/a005/A005396.java
@@ -1,43 +1,16 @@
 package irvine.oeis.a005;
 
-import java.util.HashSet;
-
 import irvine.math.z.Z;
-import irvine.oeis.Sequence;
-import irvine.util.Point;
+import irvine.oeis.a006.A006774;
 
 /**
  * A005396 Number of 2n-step polygons on honeycomb.
  * @author Sean A. Irvine
  */
-public class A005396 implements Sequence {
-
-  private int mN = 2;
-
-  private final HashSet<Point> mVisited = new HashSet<>();
-
-  {
-    mVisited.add(new Point(0, 0));
-    mVisited.add(new Point(1, 0));
-  }
-
-  private Z b(final int n, final int x, final int y) {
-    final Point v = new Point(x, y);
-    if (n == 0) {
-      return x == 0 && y == 0 ? Z.ONE : Z.ZERO;
-    } else if (mVisited.contains(v)) {
-      return Z.ZERO;
-    } else {
-      mVisited.add(v);
-      final Z c = b(n - 1, x + 1, y).add(b(n - 1, x - 1, y)).add(b(n - 1, x, y - 1 + 2 * ((x + y) & 1)));
-      mVisited.remove(v);
-      return c;
-    }
-  }
+public class A005396 extends A006774 {
 
   @Override
   public Z next() {
-    mN += 2;
-    return b(mN - 2, 1, 1).multiply(6);
+    return super.next().multiply(Math.max(1, mN));
   }
 }

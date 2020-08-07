da1cde913ba5bd89217d9700e5307d26e4737434
Sean A. Irvine <sairvin@gmail.com>
2020-08-08 08:52:16 +1200

    Sequences

---
+++ src/irvine/oeis/a034/A034200.java
@@ -0,0 +1,23 @@
+package irvine.oeis.a034;
+
+import irvine.math.group.SymmetricGroup;
+import irvine.math.polynomial.CycleIndex;
+import irvine.math.polynomial.WreathExponentiation;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A034200.
+ * @author Sean A. Irvine
+ */
+public class A034200 implements Sequence {
+
+  private static final CycleIndex Z2 = SymmetricGroup.create(2).cycleIndex();
+  private int mN = 0;
+
+  @Override
+  public Z next() {
+    return WreathExponentiation.exponentiation(Z2, SymmetricGroup.create(++mN).cycleIndex()).applyOnePlusXToTheN(5).coeff(5).toZ();
+  }
+}
+

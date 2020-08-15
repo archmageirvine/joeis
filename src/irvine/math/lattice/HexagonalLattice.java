71b48662945f3b91a5196f2ea8c66474db08aba9
Sean A. Irvine <sairvin@gmail.com>
2020-08-16 09:50:32 +1200

    Kagome

--- src/irvine/math/lattice/HexagonalLattice.java
+++ src/irvine/math/lattice/HexagonalLattice.java
@@ -4,66 +4,13 @@ package irvine.math.lattice;
  * The hexagonal lattice. Each point has six neighbours.
  * @author Sean A. Irvine
  */
-public class HexagonalLattice extends AbstractLattice {
+public class HexagonalLattice extends SquareLattice {
 
-  protected static final int BITS_PER_COORDINATE = 32;
-  private static final long MASK = (1L << BITS_PER_COORDINATE) - 1;
-  private static final long BIAS = 1L << (BITS_PER_COORDINATE - 1);
-  protected static final long Y1 = 1L << BITS_PER_COORDINATE;
-  private static final long ORIGIN = pack(0, 0);
   protected static final long[] DELTA = {2, -2, 1 + Y1, 1 - Y1, -1 + Y1, -1 - Y1};
-
-  private static long pack(final long x, final long y) {
-    return ((BIAS + y) << BITS_PER_COORDINATE) + BIAS + x;
-  }
 
   @Override
   public int neighbourCount(final long point) {
     return 6;
-  }
-
-  @Override
-  public int dimension() {
-    return 2;
-  }
-
-  @Override
-  public long origin() {
-    return ORIGIN;
-  }
-
-  @Override
-  public long ordinate(final long point, final int ordinate) {
-    assert ordinate >= 0 && ordinate <= 1;
-    return (point >> (ordinate * BITS_PER_COORDINATE) & MASK) - BIAS;
-  }
-
-  /**
-   * Convenience method to return the <code>x</code>-coordinate of a point.
-   * @param point the point
-   * @return <code>x</code> coordinate
-   */
-  public long x(final long point) {
-    return (point & MASK) - BIAS;
-  }
-
-  /**
-   * Convenience method to return the <code>y</code>-coordinate of a point.
-   * @param point the point
-   * @return <code>y</code> coordinate
-   */
-  public long y(final long point) {
-    return ((point >> BITS_PER_COORDINATE) & MASK) - BIAS;
-  }
-
-  /**
-   * Convenience method to convert an <code>(x,y)</code> pair into a point.
-   * @param x <code>x</code>-coordinate
-   * @param y <code>y</code>-coordinate
-   * @return point
-   */
-  public long toPoint(final long x, final long y) {
-    return pack(x, y);
   }
 
   @Override

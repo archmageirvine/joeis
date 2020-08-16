39a6bf13231a8ea13803403a0ab2e146cf76473b
Sean A. Irvine <sairvin@gmail.com>
2020-08-16 17:19:13 +1200

    Z^6 lattice

--- src/irvine/math/lattice/Z6Lattice.java
+++ src/irvine/math/lattice/Z6Lattice.java
@@ -1,48 +1,51 @@
 package irvine.math.lattice;
 
 /**
- * The <code>Z^5</code> lattice.
+ * The <code>Z^6</code> lattice.
  * @author Sean A. Irvine
  */
-public class Z5Lattice extends AbstractLattice {
+public class Z6Lattice extends AbstractLattice {
 
-  // 5-dimensional lattice
-  // Coordinate of a point is (t,u,x,y,z).
-  // Pack (t,u,x,y,z) into a single long with 12-bits per entry.  To avoid
+  // 6-dimensional lattice
+  // Coordinate of a point is (t,u,v,x,y,z).
+  // Pack (t,u,v,x,y,z) into a single long with 10-bits per entry.  To avoid
   // having to worry about propagation of negatives, bias each coordinate by
-  // 2^11.  Hence we can deal with lattice extent of roughly 2^11 in any
+  // 2^9.  Hence we can deal with lattice extent of roughly 2^9 in any
   // direction (an amount that will not be exhausted in a brute force
   // counting of paths, etc.)
   // Neighbours change one coordinate by +/- 1.
-  protected static final int BITS_PER_COORDINATE = 12;
-  private static final int Z_SHIFT = 4 * BITS_PER_COORDINATE;
-  private static final int Y_SHIFT = 3 * BITS_PER_COORDINATE;
-  private static final int X_SHIFT = 2 * BITS_PER_COORDINATE;
+  protected static final int BITS_PER_COORDINATE = 10;
+  private static final int Z_SHIFT = 5 * BITS_PER_COORDINATE;
+  private static final int Y_SHIFT = 4 * BITS_PER_COORDINATE;
+  private static final int X_SHIFT = 3 * BITS_PER_COORDINATE;
+  private static final int V_SHIFT = 2 * BITS_PER_COORDINATE;
   protected static final long MASK = (1L << BITS_PER_COORDINATE) - 1;
   protected static final long BIAS = 1L << (BITS_PER_COORDINATE - 1);
-  protected static final long ORIGIN = pack(0, 0, 0, 0, 0);
+  protected static final long ORIGIN = pack(0, 0, 0, 0, 0, 0);
   protected static final long Z1 = 1L << Z_SHIFT;
   protected static final long Y1 = 1L << Y_SHIFT;
   protected static final long X1 = 1L << X_SHIFT;
+  protected static final long V1 = 1L << V_SHIFT;
   protected static final long U1 = 1L << BITS_PER_COORDINATE;
-  protected static final long[] DELTAS = {1, -1, U1, -U1, X1, -X1, Y1, -Y1, Z1, -Z1};
+  protected static final long[] DELTAS = {1, -1, U1, -U1, V1, -V1, X1, -X1, Y1, -Y1, Z1, -Z1};
 
-  private static long pack(final long t, final long u, final long x, final long y, final long z) {
+  private static long pack(final long t, final long u, final long v, final long x, final long y, final long z) {
     return ((BIAS + z) << Z_SHIFT)
       + ((BIAS + y) << Y_SHIFT)
       + ((BIAS + x) << X_SHIFT)
+      + ((BIAS + v) << V_SHIFT)
       + ((BIAS + u) << BITS_PER_COORDINATE)
       + BIAS + t;
   }
 
   @Override
   public int neighbourCount(final long point) {
-    return 10;
+    return 12;
   }
 
   @Override
   public int dimension() {
-    return 5;
+    return 6;
   }
 
   @Override
@@ -57,16 +60,17 @@ public class Z5Lattice extends AbstractLattice {
   }
 
   /**
-   * Convenience method to convert an <code>(t,u,x,y,z)</code> pair into a point.
+   * Convenience method to convert an <code>(t,u,v,x,y,z)</code> pair into a point.
    * @param t <code>t</code>-coordinate
    * @param u <code>u</code>-coordinate
+   * @param v <code>v</code>-coordinate
    * @param x <code>x</code>-coordinate
    * @param y <code>y</code>-coordinate
    * @param z <code>z</code>-coordinate
    * @return point
    */
-  public long toPoint(final long t, final long u, final long x, final long y, final long z) {
-    return pack(t, u, x, y, z);
+  public long toPoint(final long t, final long u, final long v, final long x, final long y, final long z) {
+    return pack(t, u, v, x, y, z);
   }
 
   @Override
--- src/irvine/oeis/a010/A010577.java
+++ src/irvine/oeis/a010/A010577.java
@@ -1,5 +1,8 @@
 package irvine.oeis.a010;
 
+import irvine.math.lattice.ParallelWalker;
+import irvine.math.lattice.SelfAvoidingWalker;
+import irvine.math.lattice.Z6Lattice;
 import irvine.math.z.Z;
 import irvine.oeis.Sequence;
 
@@ -9,116 +12,13 @@ import irvine.oeis.Sequence;
  */
 public class A010577 implements Sequence {
 
-  // 6-dimensional cubic lattice
-  // Coordinate of a point is (x,y,z,t,u,v).
-  // Pack (x,y,z,t,u,v) into a single int with 10-bits per entry.  To avoid
-  // having to worry about propagation of negatives, bias each coordinate by
-  // 2^9.  Hence we can deal with lattice extent of roughly 2^9 in any
-  // direction (an amount that will not be exhausted in a brute force
-  // counting of paths, etc.)
-  // Neighbours change one coordinate by +/- 1.
-  protected static final int BITS_PER_COORDINATE = 10;
-  private static final int U_SHIFT = 2 * BITS_PER_COORDINATE;
-  private static final int T_SHIFT = 3 * BITS_PER_COORDINATE;
-  private static final int Z_SHIFT = 4 * BITS_PER_COORDINATE;
-  private static final int Y_SHIFT = 5 * BITS_PER_COORDINATE;
-  protected static final long MASK = (1 << BITS_PER_COORDINATE) - 1;
-  protected static final long BIAS = 1 << (BITS_PER_COORDINATE - 1);
-  protected static final long ORIGIN = (BIAS << U_SHIFT)
-    | (BIAS << T_SHIFT)
-    | (BIAS << Y_SHIFT)
-    | (BIAS << Z_SHIFT)
-    | (BIAS << BITS_PER_COORDINATE)
-    | BIAS;
-  protected static final long X1 = 1L << Y_SHIFT;
-  protected static final long Y1 = 1L << Z_SHIFT;
-  protected static final long Z1 = 1L << T_SHIFT;
-  protected static final long T1 = 1L << U_SHIFT;
-  protected static final long U1 = 1L << BITS_PER_COORDINATE;
-  private static final long[] DELTAS = {X1, -X1, Y1, -Y1, Z1, -Z1, T1, -T1, U1, -U1, 1, -1};
-
-  protected static long v(final long point) {
-    return point & MASK;
-  }
-
-  protected static long u(final long point) {
-    return (point >>> BITS_PER_COORDINATE) & MASK;
-  }
-
-  protected static long t(final long point) {
-    return (point >>> U_SHIFT) & MASK;
-  }
-
-  protected static long z(final long point) {
-    return (point >>> T_SHIFT) & MASK;
-  }
-
-  protected static long y(final long point) {
-    return (point >>> Z_SHIFT) & MASK;
-  }
-
-  protected static long x(final long point) {
-    return (point >>> Y_SHIFT) & MASK;
-  }
-
-  protected int mN = -1;
-  private long[] mPath = null;
-
-  protected void setPathLength(final int n) {
-    mPath = new long[n];
-  }
-
-  protected void setPathElement(final int pos, final long value) {
-    mPath[pos] = value;
-  }
-
-  protected long getPathElement(final int pos) {
-    return mPath[pos];
-  }
-
-  protected boolean contains(final long point, final int n) {
-    for (int k = 0; k <= n; ++k) {
-      if (mPath[k] == point) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  protected boolean check(final long point, final int n) {
-    return !contains(point, n);
-  }
-
-  protected long count(final long point) {
-    return 12;
-  }
-
-  protected long count(final long point, final int n) {
-    if (n == mN) {
-      return count(point);
-    }
-    mPath[n] = point;
-    long count = 0;
-    for (final long delta : DELTAS) {
-      final long newPoint = point + delta;
-      if (check(newPoint, n)) {
-        count += count(newPoint, n + 1);
-      }
-    }
-    return count;
-  }
-
-  protected Z firstTerm() {
-    return Z.ONE;
-  }
+  private final Z6Lattice mLattice = new Z6Lattice();
+  private final ParallelWalker mWalker = new ParallelWalker(mLattice, 8, () -> new SelfAvoidingWalker(mLattice));
+  private final long mC = mLattice.neighbour(mLattice.origin(), 0);
+  private int mN = -1;
 
   @Override
   public Z next() {
-    if (++mN == 0) {
-      return firstTerm();
-    }
-    setPathLength(mN);
-    setPathElement(0, ORIGIN);
-    return Z.valueOf(count(ORIGIN + 1, 1));
+    return ++mN == 0 ? Z.ONE : Z.valueOf(mWalker.count(mN, 12, 1, mLattice.origin(), mC));
   }
 }

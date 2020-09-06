55e908cc541566545bccdebc73a3452c9d20ef95
Sean A. Irvine <sairvin@gmail.com>
2020-09-07 08:39:33 +1200

    A334754

---
+++ src/irvine/oeis/a334/A334754.java
@@ -0,0 +1,29 @@
+package irvine.oeis.a334;
+
+import irvine.math.z.Z;
+import irvine.oeis.a000.A000796;
+
+/**
+ * A334754.
+ * @author Sean A. Irvine
+ */
+public class A334754 extends A000796 {
+
+  {
+    super.next(); // skip 3
+  }
+  private int mNextDigit = super.next().intValue();
+
+  @Override
+  public Z next() {
+    long syndrome = 0;
+    int c = 0;
+    while ((syndrome & (1 << mNextDigit)) == 0) {
+      syndrome |= 1 << mNextDigit;
+      ++c;
+      mNextDigit = super.next().intValue();
+    }
+    return Z.valueOf(c);
+  }
+}
+

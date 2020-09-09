ddbf57dd1c7def285fa7dee49b1d6796d2f38e47
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-09-09 15:48:16 +1200

    A034853

--- src/irvine/oeis/a034/A034853.java
+++ src/irvine/oeis/a034/A034853.java
@@ -17,7 +17,6 @@ public class A034853 extends A034781 {
 
   private Polynomial<Z> r(final int diameter, final int n) {
     final Z[] r = new Z[n + 1];
-    //r[0] = Z.ZERO;
     for (int k = 0; k < r.length; ++k) {
       r[k] = t(k, diameter);
     }
@@ -25,8 +24,11 @@ public class A034853 extends A034781 {
   }
 
   private Z s(final int points, final int h) {
+    if (points <= 1) {
+      return Z.valueOf(points);
+    }
     Z s = Z.ZERO;
-    for (int k = 1; k <= h; ++k) {
+    for (int k = 0; k <= h; ++k) {
       s = s.add(t(points, k));
     }
     return s;
@@ -34,42 +36,27 @@ public class A034853 extends A034781 {
 
   private Polynomial<Z> sx(final int diameter, final int n) {
     final Z[] s = new Z[n + 1];
-    s[0] = Z.ZERO;
-    for (int k = 1; k < s.length; ++k) {
+    for (int k = 0; k < s.length; ++k) {
       s[k] = s(k, diameter);
     }
     return Polynomial.create(s);
   }
 
   private Z d(final int points, final int diameter) {
-    // todo odd d case looks to be working, even is not right
-    // todo I think is is s or sx that is wrong
     final int d = diameter / 2;
     if ((diameter & 1) == 0) {
-      //return Z.ONE.max(RING.subtract(r(d, points), RING.multiply(r(d - 1, points), sx(d - 1, points), points)).coeff(points).subtract(1));
-      return RING.subtract(r(d, points), RING.multiply(r(d - 1, points), sx(d - 1, points).shift(-1), points)).coeff(points - 1);
-      //return Z.ONE.max(RING.subtract(r(d, points), RING.multiply(r(d - 1, points), sx(d - 1, points), points)).coeff(points).divide2());
+      return RING.subtract(r(d, points), RING.multiply(r(d - 1, points), sx(d - 1, points), points)).coeff(points);
     } else {
-      return RING.add(RING.pow(r(d, points), 2, points), r(d, points).substitutePower(2, points)).coeff(points).divide2();
+      return RING.add(RING.pow(r(d, points), 2, points), r(d, points / 2).substitutePower(2, points)).coeff(points).divide2();
     }
   }
 
   @Override
   public Z next() {
-    int d=4;
-    for (int p = 3; p < 20; ++p) {
-      System.out.println("T(" + p + "," + d + ")=" + d(p, d));
-    }
-
-
-    //System.out.println("R7: " + r(7, 20));
-    //System.out.println("S3: " + sx(3, 15));
     if (++mD >= mN) {
       ++mN;
       mD = 2;
     }
-    System.out.println("(" + mN + "," + mD + ")");
     return d(mN, mD);
   }
-
 }

9ac90a1e553b9a54c7b09f3bd8ad1dd8f976c6b9
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2020-09-23 13:28:25 +1200

    Contribution from Georg Fischer, stirling2.2020-09-22.11.tgz

---
+++ src/irvine/oeis/a052/A052856.java
@@ -0,0 +1,24 @@
+package irvine.oeis.a052;
+// Manually modified 2020-09-22
+
+import irvine.math.z.Z;
+import irvine.oeis.PrependSequence;
+import irvine.oeis.Stirling2TransformSequence;
+import irvine.oeis.a000.A000142;
+
+/**
+ * A052856 E.g.f.: (1-3*exp(x)+exp(2*x))/(exp(x)-2).
+ * @author Georg Fischer
+ */
+public class A052856 extends PrependSequence {
+
+  /** Construct the sequence. */
+  public A052856() {
+    super(1, new Stirling2TransformSequence(new A000142(), 0), 0);
+  }
+  
+  @Override
+  public Z next() {
+    return super.next().add(Z.ONE);
+  }
+}
---
+++ src/irvine/oeis/a140/A140585.java
@@ -0,0 +1,19 @@
+package irvine.oeis.a140;
+// Manually modified 2020-09-22
+
+import irvine.oeis.PrependSequence;
+import irvine.oeis.Stirling2TransformSequence;
+import irvine.oeis.a005.A005651;
+
+
+/**
+ * A140585 Total number of all hierarchical orderings for all set partitions of n.
+ * @author Georg Fischer
+ */
+public class A140585 extends PrependSequence {
+
+  /** Construct the sequence. */
+  public A140585() {
+    super(1, new Stirling2TransformSequence(new A005651(), 0));
+  }
+}

4647d84753bfcbff8184c8c8e4419ee7eac00935
Sean A. Irvine <sairvin@gmail.com>
2020-10-18 09:54:06 +1300

    Sequences

---
+++ src/irvine/oeis/a338/A338210.java
@@ -0,0 +1,41 @@
+package irvine.oeis.a338;
+
+import java.util.Arrays;
+
+import irvine.math.lattice.Hunter;
+import irvine.math.lattice.Lattices;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A338210 Perimeter poly fixed poly.
+ * @author Sean A. Irvine
+ */
+public class A338210 implements Sequence {
+
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private int mN = -1;
+  private int mM = 0;
+  private long[] mPerimeterCounts = new long[0];
+
+  @Override
+  public Z next() {
+    if (++mM >= mPerimeterCounts.length) {
+      if(++mN == 0) {
+        return Z.ONE; // exactly 1 polyomino with no perimeter
+      }
+      mM = 0;
+      mPerimeterCounts = new long[2 * mN + 3];
+      final Hunter h = new Hunter(Lattices.Z2, true) {
+        {
+          setKeeper((animal, forbidden) -> ++mPerimeterCounts[animal.perimeterSize(Lattices.Z2)]);
+        }
+      };
+      h.count(mN);
+      if (mVerbose) {
+        System.out.println(mN + " perimeter polynomial " + Arrays.toString(mPerimeterCounts));
+      }
+    }
+    return Z.valueOf(mPerimeterCounts[mM]);
+  }
+}
---
+++ src/irvine/oeis/a338/A338213.java
@@ -0,0 +1,55 @@
+package irvine.oeis.a338;
+
+import java.util.ArrayList;
+
+import irvine.math.lattice.Canons;
+import irvine.math.lattice.Hunter;
+import irvine.math.lattice.Lattices;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A338213 Perimeter poly free poly.
+ * @author Sean A. Irvine
+ */
+public class A338213 implements Sequence {
+
+  private int mN = -1;
+  private final ArrayList<long[]> mPerimeterCounts = new ArrayList<>();
+  {
+    mPerimeterCounts.add(new long[] {1L});
+  }
+
+  private int s(final int n) {
+    // minimum perimeter for n cells
+    return (int) Math.ceil(Math.sqrt(8 * n - 4) + 2);
+  }
+
+  @Override
+  public Z next() {
+    ++mN;
+    if (s(mPerimeterCounts.size()) <= mN - mPerimeterCounts.size()) {
+      final int n = mPerimeterCounts.size();
+      final long[] counts = new long[2 * n + 3];
+      mPerimeterCounts.add(counts);
+      final Hunter h = new Hunter(Lattices.Z2, true) {
+        {
+          setKeeper((animal, forbidden) -> {
+            if (Canons.Z2_FREE.isCanonical(animal)) {
+              ++counts[animal.perimeterSize(Lattices.Z2)];
+            }
+          });
+        }
+      };
+      h.count(n);
+    }
+    Z sprawl = Z.ZERO;
+    for (int k = 0; k < mPerimeterCounts.size(); ++k) {
+      final long[] c = mPerimeterCounts.get(k);
+      if (c.length > mN - k) {
+        sprawl = sprawl.add(c[mN - k]);
+      }
+    }
+    return sprawl;
+  }
+}

28731d8c19b8103655c8348889fa0c8547035c9b
Sean A. Irvine <sairvin@gmail.com>
2020-10-24 11:26:32 +1300

    Sequence

--- src/irvine/oeis/a337/A337867.java
+++ src/irvine/oeis/a337/A337867.java
@@ -15,6 +15,7 @@ public class A337867 implements Sequence {
 
 	// After George Sicherman C program
 
+	private static final int MAX = 100;
 	private static final int[] REFL_BITS = {
 		0, 1, 4, 5, 2, 3, 6, 7, 8, 9, 12, 13, 10, 11, 14, 15
 	};
@@ -49,23 +50,23 @@ public class A337867 implements Sequence {
 		}
 	}
 
-	int mNCells;
+	private int mNCells;
 	boolean xflag;
-	int nwcells;
-	int nbcells;
-	final Cell[] mCells = new Cell[100];
-	final Cell[] wcells = new Cell[100];
-	final Cell[] bcells = new Cell[100];
+	private int mNWorkingCells;
+	private int mNBestCells;
+	private final Cell[] mCells = new Cell[MAX];
+	private final Cell[] mWorkingCells = new Cell[MAX];
+	private final Cell[] mBestCell = new Cell[MAX];
 
 	{
-		for (int k = 0; k < 100; ++k) {
+		for (int k = 0; k < MAX; ++k) {
 			mCells[k] = new Cell();
-			wcells[k] = new Cell();
-			bcells[k] = new Cell();
+			mWorkingCells[k] = new Cell();
+			mBestCell[k] = new Cell();
 		}
 	}
 
-	private void add(final int xnew, final int ynew, final int sector) {
+	protected void add(final int xnew, final int ynew, final int sector) {
 		if (sector != 0) {
 			mCells[mNCells].mX = xnew;
 			mCells[mNCells].mY = ynew;
@@ -82,60 +83,60 @@ public class A337867 implements Sequence {
 
 	/* Turn a polyabolo all different ways and choose the lexically first. */
 	private void canonize() {
-		nwcells = mNCells;
+		mNWorkingCells = mNCells;
 		for (int c = 0; c < mNCells; c++) {
-			wcells[c].mX = mCells[c].mX;
-			wcells[c].mY = mCells[c].mY;
-			wcells[c].mSector = mCells[c].mSector;
+			mWorkingCells[c].mX = mCells[c].mX;
+			mWorkingCells[c].mY = mCells[c].mY;
+			mWorkingCells[c].mSector = mCells[c].mSector;
 		}
 
-		nbcells = 0;
+		mNBestCells = 0;
 		for (int refl = 0; refl < 2; refl++) {
 			for (int rot = 0; rot < 4; rot++) {
 				int ymin = Integer.MAX_VALUE;
 				int xmin = Integer.MAX_VALUE;
-				for (int c = 0; c < nwcells; c++) {
-					if (wcells[c].mX < xmin) {
-						xmin = wcells[c].mX;
+				for (int c = 0; c < mNWorkingCells; c++) {
+					if (mWorkingCells[c].mX < xmin) {
+						xmin = mWorkingCells[c].mX;
 					}
-					if (wcells[c].mY < ymin) {
-						ymin = wcells[c].mY;
+					if (mWorkingCells[c].mY < ymin) {
+						ymin = mWorkingCells[c].mY;
 					}
 				}
-				for (int c = 0; c < nwcells; c++) {
-					wcells[c].mX -= xmin;
-					wcells[c].mY -= ymin;
+				for (int c = 0; c < mNWorkingCells; c++) {
+					mWorkingCells[c].mX -= xmin;
+					mWorkingCells[c].mY -= ymin;
 				}
-				Arrays.sort(wcells, 0, nwcells);
+				Arrays.sort(mWorkingCells, 0, mNWorkingCells);
 
-				if (nbcells == 0 || lexBefore()) {
-					nbcells = nwcells;
-					for (int c = 0; c < nwcells; c++) {
-						bcells[c].mX = wcells[c].mX;
-						bcells[c].mY = wcells[c].mY;
-						bcells[c].mSector = wcells[c].mSector;
+				if (mNBestCells == 0 || lexBefore()) {
+					mNBestCells = mNWorkingCells;
+					for (int c = 0; c < mNWorkingCells; c++) {
+						mBestCell[c].mX = mWorkingCells[c].mX;
+						mBestCell[c].mY = mWorkingCells[c].mY;
+						mBestCell[c].mSector = mWorkingCells[c].mSector;
 					}
 				}
 				// Rotate
-				for (int c = 0; c < nwcells; c++) {
-					final int t = wcells[c].mY;
-					wcells[c].mY = -wcells[c].mX;
-					wcells[c].mX = t;
-					wcells[c].mSector = ROT_BITS[wcells[c].mSector];
+				for (int c = 0; c < mNWorkingCells; c++) {
+					final int t = mWorkingCells[c].mY;
+					mWorkingCells[c].mY = -mWorkingCells[c].mX;
+					mWorkingCells[c].mX = t;
+					mWorkingCells[c].mSector = ROT_BITS[mWorkingCells[c].mSector];
 				}
 			}
 			// Reflect
-			for (int c = 0; c < nwcells; c++) {
-				wcells[c].mX = -wcells[c].mX;
-				wcells[c].mSector = REFL_BITS[wcells[c].mSector];
+			for (int c = 0; c < mNWorkingCells; c++) {
+				mWorkingCells[c].mX = -mWorkingCells[c].mX;
+				mWorkingCells[c].mSector = REFL_BITS[mWorkingCells[c].mSector];
 			}
 		}
 	}
 
 	// Compare the current working polyabolo lexically to the "best" one. */
 	private boolean lexBefore() {
-		for (int c = 0; c < nwcells; c++) {
-			final int d = wcells[c].compareTo(bcells[c]);
+		for (int c = 0; c < mNWorkingCells; c++) {
+			final int d = mWorkingCells[c].compareTo(mBestCell[c]);
 			if (d > 0) {
 				return false;
 			}
@@ -147,7 +148,7 @@ public class A337867 implements Sequence {
 	}
 
 
-	private boolean vacant(int x, int y) {
+	protected boolean vacant(int x, int y) {
 		for (int c = 0; c < mNCells; c++) {
 			if (mCells[c].mX == x && mCells[c].mY == y) {
 				return false;
@@ -176,16 +177,107 @@ public class A337867 implements Sequence {
 	}
 
 	private void show() {
-		mRetained.put(toString(bcells, nbcells), deepCopy(bcells, nbcells));
+		mRetained.put(toString(mBestCell, mNBestCells), deepCopy(mBestCell, mNBestCells));
+	}
+
+	protected void update(final int x, final int y, final int v) {
+		// Orthogonal moves
+		if ((v & 0x7) != 0) {  /* go down */
+			final int ynew = y + 1;
+			if (vacant(x, ynew)) {
+				add(x, ynew, 0xc);
+				add(x, ynew, 0xa);
+				if ((v & 0x2) != 0) {
+					add(x, ynew, 0x3);
+				}
+				if ((v & 0x4) != 0) {
+					add(x, ynew, 0x5);
+				}
+			}
+		}
+		if ((v & 0xb) != 0) {  /* go right */
+			final int xnew = x + 1;
+			if (vacant(xnew, y)) {
+				add(xnew, y, 0xc);
+				add(xnew, y, 0x5);
+				if ((v & 1) != 0) {
+					add(xnew, y, 0x3);
+				}
+				if ((v & 8) != 0) {
+					add(xnew, y, 0xa);
+				}
+			}
+		}
+		if ((v & 0xd) != 0) {  /* go left */
+			final int xnew = x - 1;
+			if (vacant(xnew, y)) {
+				add(xnew, y, 0xa);
+				add(xnew, y, 0x3);
+				if ((v & 1) != 0) {
+					add(xnew, y, 0x5);
+				}
+				if ((v & 8) != 0) {
+					add(xnew, y, 0xc);
+				}
+			}
+		}
+		if ((v & 0xe) != 0) {  /* go up */
+			final int ynew = y - 1;
+			if (vacant(x, ynew)) {
+				add(x, ynew, 0x5);
+				add(x, ynew, 0x3);
+				if ((v & 2) != 0) {
+					add(x, ynew, 0xa);
+				}
+				if ((v & 4) != 0) {
+					add(x, ynew, 0xc);
+				}
+			}
+		}
+		// Diagonal moves.
+		if ((v & 0x3) != 0) {  /* down right. */
+			final int xnew = x + 1;
+			final int ynew = y + 1;
+			if (vacant(xnew, ynew)) {
+				add(xnew, ynew, 0x5);
+				add(xnew, ynew, 0xa);
+				add(xnew, ynew, 0xc);
+			}
+		}
+		if ((v & 0x5) != 0) {  /* down left. */
+			final int xnew = x - 1;
+			final int ynew = y + 1;
+			if (vacant(xnew, ynew)) {
+				add(xnew, ynew, 0x3);
+				add(xnew, ynew, 0xa);
+				add(xnew, ynew, 0xc);
+			}
+		}
+		if ((v & 0xa) != 0) {  /* up right. */
+			final int xnew = x + 1;
+			final int ynew = y - 1;
+			if (vacant(xnew, ynew)) {
+				add(xnew, ynew, 0x3);
+				add(xnew, ynew, 0x5);
+				add(xnew, ynew, 0xc);
+			}
+		}
+		if ((v & 0xc) != 0) {  /* up left. */
+			final int xnew = x - 1;
+			final int ynew = y - 1;
+			if (vacant(xnew, ynew)) {
+				add(xnew, ynew, 0x3);
+				add(xnew, ynew, 0x5);
+				add(xnew, ynew, 0xa);
+			}
+		}
 	}
 
 	@Override
 	public Z next() {
-		if (++mN > 99) {
+		if (++mN >= MAX) {
 			throw new UnsupportedOperationException();
 		}
-		xflag = true; // polyfetts , false for polyaboloes
-
 		if (mRetained.isEmpty()) {
 			final Cell t = new Cell();
 			t.mX = 0;
@@ -216,134 +308,12 @@ public class A337867 implements Sequence {
 						add(0, 0, 0);    /* 0 means change nothing */
 						mCells[c].mSector = v;  /* restore the value */
 					}
-
 					// Neighbor cell adjacencies.  Use a whole separate clause for -x.
-					if (xflag) {
-						// todo subclass -x case
-						// Orthogonal moves
-						if ((v & 0x7) != 0) {  /* go down */
-							final int ynew = y + 1;
-							if (vacant(x, ynew)) {
-								add(x, ynew, 0xc);
-								add(x, ynew, 0xa);
-								if ((v & 0x2) != 0) {
-									add(x, ynew, 0x3);
-								}
-								if ((v & 0x4) != 0) {
-									add(x, ynew, 0x5);
-								}
-							}
-						}
-						if ((v & 0xb) != 0) {  /* go right */
-							final int xnew = x + 1;
-							if (vacant(xnew, y)) {
-								add(xnew, y, 0xc);
-								add(xnew, y, 0x5);
-								if ((v & 1) != 0) {
-									add(xnew, y, 0x3);
-								}
-								if ((v & 8) != 0) {
-									add(xnew, y, 0xa);
-								}
-							}
-						}
-						if ((v & 0xd) != 0) {  /* go left */
-							final int xnew = x - 1;
-							if (vacant(xnew, y)) {
-								add(xnew, y, 0xa);
-								add(xnew, y, 0x3);
-								if ((v & 1) != 0) {
-									add(xnew, y, 0x5);
-								}
-								if ((v & 8) != 0) {
-									add(xnew, y, 0xc);
-								}
-							}
-						}
-						if ((v & 0xe) != 0) {  /* go up */
-							final int ynew = y - 1;
-							if (vacant(x, ynew)) {
-								add(x, ynew, 0x5);
-								add(x, ynew, 0x3);
-								if ((v & 2) != 0) {
-									add(x, ynew, 0xa);
-								}
-								if ((v & 4) != 0) {
-									add(x, ynew, 0xc);
-								}
-							}
-						}
-						// Diagonal moves.
-						if ((v & 0x3) != 0) {  /* down right. */
-							final int xnew = x + 1;
-							final int ynew = y + 1;
-							if (vacant(xnew, ynew)) {
-								add(xnew, ynew, 0x5);
-								add(xnew, ynew, 0xa);
-								add(xnew, ynew, 0xc);
-							}
-						}
-						if ((v & 0x5) != 0) {  /* down left. */
-							final int xnew = x - 1;
-							final int ynew = y + 1;
-							if (vacant(xnew, ynew)) {
-								add(xnew, ynew, 0x3);
-								add(xnew, ynew, 0xa);
-								add(xnew, ynew, 0xc);
-							}
-						}
-						if ((v & 0xa) != 0) {  /* up right. */
-							final int xnew = x + 1;
-							final int ynew = y - 1;
-							if (vacant(xnew, ynew)) {
-								add(xnew, ynew, 0x3);
-								add(xnew, ynew, 0x5);
-								add(xnew, ynew, 0xc);
-							}
-						}
-						if ((v & 0xc) != 0) {  /* up left. */
-							final int xnew = x - 1;
-							final int ynew = y - 1;
-							if (vacant(xnew, ynew)) {
-								add(xnew, ynew, 0x3);
-								add(xnew, ynew, 0x5);
-								add(xnew, ynew, 0xa);
-							}
-						}
-					} else {
-						if ((v & 0x1) != 0) {  /* go down */
-							final int ynew = y + 1;
-							if (vacant(x, ynew)) {
-								add(x, ynew, 0xc);
-								add(x, ynew, 0xa);
-							}
-						}
-						if ((v & 0x2) != 0) {  /* go right */
-							final int xnew = x + 1;
-							if (vacant(xnew, y)) {
-								add(xnew, y, 0xc);
-								add(xnew, y, 0x5);
-							}
-						}
-						if ((v & 0x4) != 0) {  /* go left */
-							final int xnew = x - 1;
-							if (vacant(xnew, y)) {
-								add(xnew, y, 0xa);
-								add(xnew, y, 0x3);
-							}
-						}
-						if ((v & 0x8) != 0) {  /* go up */
-							final int ynew = y - 1;
-							if (vacant(x, ynew)) {
-								add(x, ynew, 0x5);
-								add(x, ynew, 0x3);
-							}
-						}
-					}
+					update(x, y, v);
 				}
 			}
 		}
-		//System.out.println(mRetained);
 		return Z.valueOf(mRetained.size());
 	}
+
 }

144e8a7415ee3bf9a63add3402c62c4a17408b84
Sean A. Irvine <sairvin@gmail.com>
2021-01-17 14:05:58 +1300

    Walk

---
+++ src/irvine/oeis/a038/A038515.java
@@ -0,0 +1,27 @@
+package irvine.oeis.a038;
+
+import irvine.math.lattice.Lattices;
+import irvine.math.lattice.ParallelWalker;
+import irvine.math.lattice.SelfAvoidingCycler;
+import irvine.math.lattice.SelfAvoidingWalker;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A038515 Number of self-avoiding walks on a tetrahedral (diamond) net, having 2n steps and forming a closed loop.
+ * @author Sean A. Irvine
+ */
+public class A038515 implements Sequence {
+
+  private int mN = -2;
+  private final long mC = Lattices.DIAMOND.neighbour(Lattices.DIAMOND.origin(), 0);
+  private final ParallelWalker mWalker = new ParallelWalker(8,
+    () -> new SelfAvoidingWalker(Lattices.DIAMOND),
+    () -> new SelfAvoidingCycler(Lattices.DIAMOND, true));
+
+  @Override
+  public Z next() {
+    mN += 2;
+    return mN == 0 ? Z.ONE : Z.valueOf(mWalker.count(mN, 1, 15, Lattices.DIAMOND.origin(), mC));
+  }
+}

e7a729b81d7749c3cd9e871a72433f0c02de7870
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-02-16 09:41:21 +1300

    Sequences

---
+++ src/irvine/oeis/a038/A038840.java
@@ -0,0 +1,28 @@
+package irvine.oeis.a038;
+
+import irvine.math.z.Z;
+
+/**
+ * A038840.
+ * @author Sean A. Irvine
+ */
+public class A038840 extends A038692 {
+
+  private long mN = 2;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final Z n3 = Z.valueOf(++mN).pow(3);
+      final String s = n3.toString();
+      final char last = s.charAt(s.length() - 1);
+      if (last == '2' || (last & 1) == 1) {
+        for (int k = 1; k < s.length(); ++k) {
+          if (new Z(s.substring(0, k)).isProbablePrime() && isPrimeDecomposable(s.substring(k))) {
+            return n3;
+          }
+        }
+      }
+    }
+  }
+}

7f4d3b5d689c508b743f54177abc29dede554dec
Sean A. Irvine <sairvin@gmail.com>
2021-02-16 17:59:07 +1300

    Sequences

---
+++ src/irvine/oeis/a038/A038838.java
@@ -0,0 +1,24 @@
+package irvine.oeis.a038;
+
+import irvine.factor.factor.Cheetah;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A038838.
+ * @author Sean A. Irvine
+ */
+public class A038838 implements Sequence {
+
+  private Z mN = Z.EIGHT;
+
+  @Override
+  public Z next() {
+    while (true) {
+      mN = mN.add(1);
+      if (Cheetah.factor(mN.makeOdd()).maxExponent() > 1) {
+        return mN;
+      }
+    }
+  }
+}

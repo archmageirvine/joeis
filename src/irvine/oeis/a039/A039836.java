29dfe15238e36fee7ff964a937cd1dfcf9facbaf
Sean A. Irvine <sairvin@gmail.com>
2021-02-28 13:28:10 +1300

    Sequences

---
+++ src/irvine/oeis/a039/A039836.java
@@ -0,0 +1,59 @@
+package irvine.oeis.a039;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+import irvine.util.array.LongDynamicBooleanArray;
+
+/**
+ * A039836.
+ * @author Sean A. Irvine
+ */
+public class A039836 implements Sequence {
+
+  // Direct backtracking search
+
+  private int mN = 0;
+  private int mMax = 0;
+  private final int[] mS = new int[100];
+
+  private boolean isOk(final int k, final int m, final LongDynamicBooleanArray seen) {
+    for (int j = 0; j < m; ++j) {
+      if (seen.isSet(k + mS[j])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private void search(final LongDynamicBooleanArray seen, final int m) {
+    if (m > mMax) {
+      mMax = m;
+    }
+    final int prev = mS[m - 1];
+    if (mN - prev < mMax - m) {
+      return; // insufficient remaining numbers
+    }
+    for (int k = prev + 1; k <= mN; ++k) {
+      if (isOk(k, m, seen)) {
+        mS[m] = k;
+        for (int j = 0; j < m; ++j) {
+          seen.set(k + mS[j]);
+        }
+        search(seen, m + 1);
+        for (int j = 0; j < m; ++j) {
+          seen.clear(k + mS[j]);
+        }
+      }
+    }
+  }
+
+  @Override
+  public Z next() {
+    ++mN;
+    for (int k = 1; k <= mN; ++k) {
+      mS[0] = k;
+      search(new LongDynamicBooleanArray(), 1);
+    }
+    return Z.valueOf(mMax);
+  }
+}

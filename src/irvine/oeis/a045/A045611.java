b3c284c187e3a1c2799b24878e16912a553e615f
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-03-16 16:59:41 +1300

    Sequences

--- src/irvine/oeis/a045/A045611.java
+++ src/irvine/oeis/a045/A045611.java
@@ -1,9 +1,9 @@
 package irvine.oeis.a045;
 
-import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import irvine.math.LongUtils;
-import irvine.math.q.Q;
 import irvine.math.z.Z;
 import irvine.oeis.Sequence;
 
@@ -20,33 +20,24 @@ public class A045611 implements Sequence {
     if (++mN < 2) {
       return Z.ONE;
     }
-    final TreeSet<Q> energy = new TreeSet<>();
-    final TreeSet<Long> distances = new TreeSet<>();
+    final HashSet<String> distances = new HashSet<>();
     final int charges = 2 * mN;
     final long limit = 1L << charges;
     for (long state = (1L << mN) - 1; state < limit; state = LongUtils.swizzle(state)) {
-      Q sum = Q.ZERO;
-      long d = 0;
+      final int[] cnts = new int[mN];
       for (int k = 0; k < charges; ++k) {
-        Q s = Q.ZERO;
-        for (int j = 0; j < charges; ++j) {
-          if (k != j) {
-            final int delta = Math.abs(k - j);
-            s = s.signedAdd((state & (1L << j)) == 0, new Q(1, Math.min(delta, charges - delta)));
-            if (((state & (1L << j))) == 0 && ((state & (1L << k)) == 0)) {
-              //d |= 1L << Math.min(delta, charges - delta);
-              d |= 1L << delta;
+        if (((state >>> k) & 1) != 0) {
+          for (int j = k + 1; j < charges; ++j) {
+            if (((state >>> j) & 1) != 0) {
+              final int delta = Math.abs(k - j);
+              final int shortest = Math.min(delta, charges - delta);
+              ++cnts[shortest - 1];
             }
           }
         }
-        sum = sum.signedAdd((state & (1L << k)) == 0, s);
       }
-      distances.add(d);
-//      final String s = "00000000000000000000" + Long.toBinaryString(state);
-//      System.out.println("state=" + s.substring(s.length() - charges) + " U=" + sum);
-      energy.add(sum);
+      distances.add(Arrays.toString(cnts));
     }
-    System.out.println("D=" + distances.size() + " " + distances);
-    return Z.valueOf(energy.size());
+    return Z.valueOf(distances.size());
   }
 }

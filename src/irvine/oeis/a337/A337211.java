5a7e91900e3e22d86c2099098e892b81e48dde3b
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-03-24 13:48:11 +1300

    Sequence

---
+++ src/irvine/oeis/a337/A337211.java
@@ -0,0 +1,51 @@
+package irvine.oeis.a337;
+
+import java.util.ArrayList;
+
+import irvine.math.cr.CR;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A337211.
+ * @author Sean A. Irvine
+ */
+public class A337211 implements Sequence {
+
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private CR mBest = CR.TWO;
+  private int mN = 0;
+  private final ArrayList<CR> mSqrtCache = new ArrayList<>();
+
+  private CR sqrt(final int n) {
+    while (n >= mSqrtCache.size()) {
+      mSqrtCache.add(CR.valueOf(mSqrtCache.size()).sqrt());
+    }
+    return mSqrtCache.get(n);
+  }
+
+  private CR search(CR s, final int c) {
+    if (c <= 0) {
+      return s;
+    }
+    final CR t = s.subtract(sqrt(c));
+    if (t.signum(-256) <= 0) {
+      return search(s, c - 1);
+    }
+    return search(s, c - 1).min(search(t, c - 1));
+  }
+
+  @Override
+  public Z next() {
+    while (true) {
+      final CR b = search(sqrt(++mN), mN - 1);
+      if (b.compareTo(mBest) < 0) {
+        mBest = b;
+        if (mVerbose) {
+          System.out.println(mN + " # b=" + mBest.toString(30));
+        }
+        return Z.valueOf(mN);
+      }
+    }
+  }
+}

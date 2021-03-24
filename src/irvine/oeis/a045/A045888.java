3828bfccf9e3a06796fd3f6dda64bf44ab7eeb5c
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-03-24 16:32:57 +1300

    Sequence

---
+++ src/irvine/oeis/a045/A045888.java
@@ -0,0 +1,34 @@
+package irvine.oeis.a045;
+
+import java.util.TreeSet;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A045888.
+ * @author Sean A. Irvine
+ */
+public class A045888 implements Sequence {
+
+  private long mN = 0;
+  private final TreeSet<Long> mA = new TreeSet<>();
+
+  private void search(final String s, final int k, final long v) {
+    if (k == s.length()) {
+      if (v != mN && (v & 1) == 1) {
+        mA.add(v);
+      }
+      return;
+    }
+    search(s, k + 1, v);
+    search(s, k + 1, v * 10 + s.charAt(k) - '0');
+  }
+
+  @Override
+  public Z next() {
+    mA.clear();
+    search(String.valueOf(++mN), 0, 0);
+    return Z.valueOf(mA.size());
+  }
+}

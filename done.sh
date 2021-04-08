0a01aabc914bd5b3f30903654e8e130c9046a301
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-04-09 09:28:07 +1200

    Adding OEIS history

--- done.sh
+++ done.sh
@@ -47,18 +47,22 @@ EOF
     done
 } | awk -vmax=${max} '{pix[$1,$2]=$3}END{print "P6 1000 "max" 1"; for (y=0;y<max;y++) for(x=0; x<1000;x++) {val=pix[y,x]; if (val=="r") {printf "%c%c%c", 1, 0, 0} else if (val=="g") {printf "%c%c%c", 0, 1, 0} else {printf "%c%c%c", 0, 0, 0 }}}' | pnmtopng >oeis-implemented.png
 
+# The oeis-by-month.dat file was computed in the following way from the oeis_created.txt
+# file constructed by Georg Fischer.  Afterward I manually trimmed the file to post-2009.
+#  sort -k2 oeis_created.txt | grep '^A' | grep -v 1971-12-31 | tr -d '\r' | sed 's/-[0-9][0-9]$//' | awk '{if ($2!=prev){print $2"-01",NR-1}; prev=$2}' >oeis-by-month.dat
+
 gnuplot <<EOF
         set term pngcairo
         set output "oeis-history.png"
         set xlabel "Year (20xx)"
-        set ylabel "Sequences implemented"
-        set title "History of jOEIS"
+        set ylabel "Sequences"
+        set title "History"
         set xdata time     
         #set xtics 63072000
         set timefmt "%Y-%m-%d"
         set format x "%y"
-        set key off
-        plot "history.dat" using 1:2 with lines
+        set key left
+        plot "oeis-by-month.dat" using 1:2 with lines title "OEIS sequences", "history.dat" using 1:2 with lines title "jOEIS implementations"
 EOF
 
 mv ./*.png doc

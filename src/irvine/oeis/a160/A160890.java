edf641096842a7c4bc75207d6ec0ae7fe56790cd
Sean A. Irvine <sairvin@gmail.com>
2021-04-24 14:23:32 +1200

    Contribution from Georg Fischer, dirichcon.2021-04-24.00.tgz

--- src/irvine/oeis/a160/A160890.java
+++ src/irvine/oeis/a160/A160890.java
@@ -1,19 +1,16 @@
 package irvine.oeis.a160;
 // manually
 
-import irvine.math.LongUtils;
 import irvine.math.z.Z;
-import irvine.oeis.a059.A059376;
 
 /**
  * A160890 a(n) = ((2^b-1)/phi(n))*Sum_{d|n} Moebius(n/d)*d^(b-1) for b = 3.
- * a(n) = J_2(n)/J_1(n)
+ * a(n) = J_3(n)/J_1(n)=J_3(n)/phi(n)=A059376(n)/A000010(n)
  * @author Georg Fischer
  */
-public class A160890 extends A059376 {
+public class A160890 extends A160889 {
 
   protected long mN = 0;
-  protected long mB21; // 2^b-1
 
   /** Construct the sequence. */
   public A160890() {
@@ -21,18 +18,16 @@ public class A160890 extends A059376 {
   }
 
   /**
-   * Generic constructor with parameter 
+   * Generic constructor with parameter
    * @param b exponent of Jordan function + 1
    */
-  public A160890(final int b) {
-    super(b - 1);
-    mB21 = (1 << b) - 1;
+  protected A160890(final int b) {
+    super(b);
   }
-  
-  
+
   @Override
   public Z next() {
     ++mN;
-    return super.next().multiply(mB21).divide(LongUtils.phi(mN));
+    return super.next().multiply(Z.ONE.shiftLeft(mExp + 1).subtract(1));
   }
 }

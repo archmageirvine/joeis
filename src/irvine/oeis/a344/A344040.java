5bb1801080280cba36ff908750ae0adab0904d48
Sean A. Irvine <sairvin@gmail.com>
2021-05-08 08:29:19 +1200

    A344040

---
+++ src/irvine/oeis/a344/A344040.java
@@ -0,0 +1,23 @@
+package irvine.oeis.a344;
+
+import irvine.math.lattice.Lattices;
+import irvine.math.lattice.NonadjacentWalker;
+import irvine.math.lattice.ParallelWalker;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A344040.
+ * @author Sean A. Irvine
+ */
+public class A344040 implements Sequence {
+
+  private final ParallelWalker mWalker = new ParallelWalker(8, () -> new NonadjacentWalker(Lattices.HYDROGEN_PEROXIDE));
+  private final long mC = Lattices.HYDROGEN_PEROXIDE.neighbour(Lattices.HYDROGEN_PEROXIDE.origin(), 0);
+  private int mN = -1;
+
+  @Override
+  public Z next() {
+    return ++mN == 0 ? Z.ONE : Z.valueOf(mWalker.count(mN, 6, 7, Lattices.HYDROGEN_PEROXIDE.origin(), mC));
+  }
+}

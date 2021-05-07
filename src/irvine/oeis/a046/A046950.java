4d8eb9c5abde1bf42fc1d86881726c117fb84906
Sean A. Irvine <sairvin@gmail.com>
2021-05-08 09:27:50 +1200

    A046950

---
+++ src/irvine/oeis/a046/A046950.java
@@ -0,0 +1,20 @@
+package irvine.oeis.a046;
+
+import irvine.math.z.Z;
+
+/**
+ * A046950.
+ * @author Sean A. Irvine
+ */
+public class A046950 extends A046949 {
+
+  @Override
+  public Z next() {
+    while (true) {
+      final Z t = super.next();
+      if (t.isSquare()) {
+        return t;
+      }
+    }
+  }
+}

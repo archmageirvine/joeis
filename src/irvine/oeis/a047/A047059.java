b4792071c8fe645f2da7c639b1e7d2ed7c8e10db
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-05-12 11:37:29 +1200

    Sequence

---
+++ src/irvine/oeis/a047/A047059.java
@@ -0,0 +1,17 @@
+package irvine.oeis.a047;
+
+import irvine.math.z.Z;
+
+/**
+ * A047059.
+ * @author Sean A. Irvine
+ */
+public class A047059 extends A047050 {
+
+  private long mN = -1;
+
+  @Override
+  public Z next() {
+    return get(++mN, 2 * mN);
+  }
+}
---
+++ src/irvine/oeis/a047/A047060.java
@@ -0,0 +1,39 @@
+package irvine.oeis.a047;
+
+import irvine.math.MemoryFunction2;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A047060.
+ * @author Sean A. Irvine
+ */
+public class A047060 extends MemoryFunction2<Long, Z> implements Sequence {
+
+  private long mN = -1;
+  private long mM = 0;
+
+  @Override
+  protected Z compute(final Long n, final Long m) {
+    if (n == 0 || m == 0) {
+      return Z.ONE;
+    }
+    Z s = Z.ZERO;
+    if (5 * n - 1 != 3 * m && 5 * n - 2 != 3 * m && 5 * n - 3 != 3 * m && 5 * n - 4 != 3 * m && 5 * n - 5 != 3 * m) {
+      s = s.add(get(n - 1, m));
+    }
+    if (5 * n != 3 * m - 3 && 5 * n != 3 * m - 2 && 5 * n != 3 * m - 1) {
+      s = s.add(get(n, m - 1));
+    }
+    return s;
+  }
+
+  @Override
+  public Z next() {
+    if (++mM > mN) {
+      ++mN;
+      mM = 0;
+    }
+    return get(mM, mN - mM);
+  }
+}

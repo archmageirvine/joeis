5b38553c57069ecadd30775f1edda216e8ee13bb
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-05-31 09:07:55 +1200

    Contribution from Georg Fischer, A004050.2021-05-30.20.tgz

--- src/irvine/oeis/a004/A004050.java
+++ src/irvine/oeis/a004/A004050.java
@@ -1,7 +1,5 @@
 package irvine.oeis.a004;
 
-import java.io.Serializable;
-import java.util.Comparator;
 import java.util.TreeSet;
 
 import irvine.math.z.Z;
@@ -14,27 +12,52 @@ import irvine.util.Pair;
  */
 public class A004050 implements Sequence {
 
-  private static class MyComparator implements Comparator<Pair<Z, Z>>, Serializable {
-
-    @Override
-    public int compare(final Pair<Z, Z> a, final Pair<Z, Z> b) {
-      return a.left().add(a.right()).compareTo(b.left().add(b.right()));
-    }
-  }
-
-  private final TreeSet<Pair<Z, Z>> mPriority = new TreeSet<>(new MyComparator());
-  {
-    mPriority.add(new Pair<>(Z.ONE, Z.ONE));
-  }
+  protected int mBase1; 
+  protected int mBase2;
   private Z mPrev = null;
+
+  private final TreeSet<Pair<Z, Z>> mPriority = new TreeSet<>((a, b) -> {
+    int result = a.left().add(a.right()).compareTo(b.left().add(b.right()));
+    if (result == 0) {
+      result = a.left().compareTo(b.left());
+      if (result == 0) {
+        result = a.right().compareTo(b.right());
+      }
+    }
+    return result;
+  });
+  
+  /** Construct the sequence */
+  public A004050() {
+    this(2, 3);
+  }
+  
+  /**
+   * Generic constructor with parameters
+   * @param parm1 first base
+   * @param parm2 second base
+   */
+  public A004050(final int parm1, final int parm2) {
+    mBase1 = parm1;
+    mBase2 = parm2;
+    mPriority.add(new Pair<>(Z.ONE, Z.ONE));
+    mPrev = Z.ZERO;
+  }
 
   @Override
   public Z next() {
     Z res;
     do {
       final Pair<Z, Z> e = mPriority.pollFirst();
-      mPriority.add(new Pair<>(e.left().multiply2(), e.right()));
-      mPriority.add(new Pair<>(e.left(), e.right().multiply(3)));
+      mPriority.add(new Pair<>(e.left().multiply(mBase1), e.right()));
+      mPriority.add(new Pair<>(e.left(), e.right().multiply(mBase2)));
+/*
+      if (false) {
+        System.out.println("\npoll (" + e.left() + "," + e.right() + "), add ("
+          + e.left().multiply(mBase1) + "," + e.right() + ") and ("
+          + e.left() + "," + e.right().multiply(mBase2) + ")");
+      } 
+*/
       res = e.left().add(e.right());
     } while (res.equals(mPrev));
     mPrev = res;

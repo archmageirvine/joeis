ab274d486c742c8203d7263e653a9b7cd8b1a045
Sean A. Irvine <sairvin@gmail.com>
2021-06-20 09:20:50 +1200

    Sequences

---
+++ src/irvine/oeis/a048/A048516.java
@@ -0,0 +1,39 @@
+package irvine.oeis.a048;
+
+import irvine.math.group.IntegerField;
+import irvine.math.group.PolynomialRingField;
+import irvine.math.polynomial.Polynomial;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A048516.
+ * @author Sean A. Irvine
+ */
+public class A048516 implements Sequence {
+
+  private static final PolynomialRingField<Z> RING = new PolynomialRingField<>(IntegerField.SINGLETON);
+  private static final Polynomial<Z> C1 = Polynomial.create(1, -2, 1);
+  private int mN = -1;
+  private int mM = 0;
+
+  private Z t(final int m, final int n) {
+    if (n == 0) {
+      return Z.ZERO;
+    }
+    final long[] coeffs = new long[n + 1];
+    coeffs[0] = 1;
+    coeffs[1] = -1;
+    coeffs[n] -= 1;
+    return RING.coeff(RING.one(), RING.multiply(C1, Polynomial.create(coeffs), m), m);
+  }
+
+  @Override
+  public Z next() {
+    if (++mM > mN) {
+      ++mN;
+      mM = 0;
+    }
+    return t(mM, mN - mM);
+  }
+}

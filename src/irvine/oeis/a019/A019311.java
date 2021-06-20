8b4ec059ae2f99721d31823d03741adbb693f9ec
Sean A. Irvine <sairvin@gmail.com>
2021-06-20 20:27:32 +1200

    Sequences

--- src/irvine/oeis/a019/A019311.java
+++ src/irvine/oeis/a019/A019311.java
@@ -11,26 +11,24 @@ public class A019311 implements Sequence {
 
   private long[] mCounts = new long[0];
   private long mMask = 0;
-  private int mM = 0;
 
   private int slide(final long t) {
-    final int n = mCounts.length;
+    int n = mCounts.length;
     long m = mMask;
     long u = t;
-    for (int slide = 1; slide < n; ++slide) {
+    while (true) {
       m >>>= 1;
       u >>>= 1;
+      --n;
       if (((t ^ u) & m) == 0) {
-        return n - slide;
+        return n;
       }
     }
-    return 0;
   }
 
   private void computeNextRow() {
     mCounts = new long[mCounts.length + 1];
     mMask = (1L << mCounts.length) - 1;
-    mM = 0;
     for (long t = 0; t <= mMask; ++t) {
       ++mCounts[slide(t)];
     }

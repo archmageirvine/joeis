efff04d6685294df428007a03f712d1cdb67f813
Sean A. Irvine <sairvin@gmail.com>
2021-06-21 10:58:07 +1200

    Sequence

---
+++ src/irvine/oeis/a345/A345530.java
@@ -0,0 +1,46 @@
+package irvine.oeis.a345;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A345530.
+ * @author Sean A. Irvine
+ */
+public class A345530 implements Sequence {
+
+  protected long[] mCounts = new long[0];
+  private long mMask = 0;
+  private int mM = 0;
+
+  private int slide(final long t) {
+    int n = mCounts.length;
+    long m = mMask;
+    long u = t;
+    while (true) {
+      m >>>= 1;
+      u >>>= 1;
+      --n;
+      if (((t ^ u) & m) == 0) {
+        return n;
+      }
+    }
+  }
+
+  protected void computeNextRow() {
+    mCounts = new long[mCounts.length + 1];
+    mMask = (1L << mCounts.length) - 1;
+    for (long t = 0; t <= mMask; ++t) {
+      ++mCounts[slide(t)];
+    }
+  }
+
+  @Override
+  public Z next() {
+    if (++mM >= mCounts.length) {
+      computeNextRow();
+      mM = 0;
+    }
+    return Z.valueOf(mCounts[mM]);
+  }
+}

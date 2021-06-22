d9f5189d326330b6b83e6bb581aa9607dea950bf
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-06-23 11:59:38 +1200

    Sequence

---
+++ src/irvine/oeis/a345/A345628.java
@@ -0,0 +1,27 @@
+package irvine.oeis.a345;
+
+import irvine.math.group.DicyclicGroup;
+import irvine.math.z.Z;
+import irvine.oeis.OrdersFiniteGroupSequence;
+import irvine.oeis.Sequence;
+
+/**
+ * A345628.
+ * @author Sean A. Irvine
+ */
+public class A345628 implements Sequence {
+
+  private int mN = 2;
+  private OrdersFiniteGroupSequence mSeq = new OrdersFiniteGroupSequence(new DicyclicGroup(2));
+
+  @Override
+  public Z next() {
+    final Z t = mSeq.next();
+    if (t != null) {
+      return t;
+    }
+    // Step to next row and group
+    mSeq = new OrdersFiniteGroupSequence(new DicyclicGroup(++mN));
+    return mSeq.next();
+  }
+}

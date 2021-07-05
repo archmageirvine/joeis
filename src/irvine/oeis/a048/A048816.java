c249bf3d6102e528631d4a121ebced1446bf1e02
Sean A. Irvine <sairvin@gmail.com>
2021-07-05 18:39:07 +1200

    Sequences

---
+++ src/irvine/oeis/a048/A048816.java
@@ -0,0 +1,23 @@
+package irvine.oeis.a048;
+
+import irvine.math.z.Z;
+import irvine.oeis.a244.A244925;
+
+/**
+ * A048816.
+ * @author Sean A. Irvine
+ */
+public class A048816 extends A244925 {
+
+  private long mN = 0;
+
+  @Override
+  public Z next() {
+    ++mN;
+    Z sum = Z.ZERO;
+    for (int k = 0; k < mN; ++k) {
+      sum = sum.add(super.next());
+    }
+    return sum;
+  }
+}
---
+++ src/irvine/oeis/a244/A244925.java
@@ -0,0 +1,49 @@
+package irvine.oeis.a244;
+
+import irvine.factor.factor.Cheetah;
+import irvine.math.MemoryFunction2;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A244925.
+ * @author Sean A. Irvine
+ */
+public class A244925 extends MemoryFunction2<Long, Z> implements Sequence {
+
+  // After Alois P. Heinz
+
+  private long mN = 0;
+  private long mM = 0;
+
+  @Override
+  protected Z compute(final Long n, final Long m) {
+    if (n == 1) {
+      return Z.ONE;
+    }
+    if (m == 0) {
+      return Z.ZERO;
+    }
+    Z sum = Z.ZERO;
+    for (long j = 1; j < n; ++j) {
+      Z s = Z.ZERO;
+      for (final Z dd : Cheetah.factor(j).divisors()) {
+        final long d = dd.longValue();
+        if (d >= m) {
+          s = s.add(get(d, m - 1).multiply(d));
+        }
+      }
+      sum = sum.add(s.multiply(get(n - j, m)));
+    }
+    return sum.divide(n - 1);
+  }
+
+  @Override
+  public Z next() {
+    if (++mM >= mN) {
+      ++mN;
+      mM = 0;
+    }
+    return get(mN, mM);
+  }
+}

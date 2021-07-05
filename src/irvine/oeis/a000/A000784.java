5bbf751f72c9ac03a1f2f65a9b61839f33ecb773
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-07-05 16:25:27 +1200

    Cache

---
+++ src/irvine/oeis/a000/A000784.java
@@ -0,0 +1,22 @@
+package irvine.oeis.a000;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+import irvine.oeis.SkipSequence;
+import irvine.oeis.a048.A048140;
+import irvine.oeis.a048.A048141;
+
+/**
+ * A000784.
+ * @author Sean A. Irvine
+ */
+public class A000784 extends A048140 {
+
+  private final Sequence mC3v = new A048141();
+  private final Sequence mA219 = new SkipSequence(new A000219(), 1);
+
+  @Override
+  public Z next() {
+    return super.next().multiply2().subtract(mC3v.next()).subtract(mA219.next());
+  }
+}
--- src/irvine/oeis/a048/A048141.java
+++ src/irvine/oeis/a048/A048141.java
@@ -1,6 +1,12 @@
 package irvine.oeis.a048;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import irvine.math.partitions.IntegerPartition;
 import irvine.math.z.Z;
@@ -13,6 +19,21 @@ import irvine.oeis.Sequence;
 public class A048141 implements Sequence {
 
   private int mN = 0;
+  private final List<Map<int[], Long>> mCache = new ArrayList<>();
+
+  private static final Comparator<int[]> ARRAY_COMPARATOR = (o1, o2) -> {
+    final int c = Integer.compare(o1.length, o2.length);
+    if (c != 0) {
+      return c;
+    }
+    for (int k = 0; k < o1.length; ++k) {
+      final int d = Integer.compare(o1[k], o2[k]);
+      if (d != 0) {
+        return d;
+      }
+    }
+    return 0;
+  };
 
   private boolean isOverMajored(final int[] outer, final int[] inner) {
     if (inner.length > outer.length) {
@@ -35,47 +56,37 @@ public class A048141 implements Sequence {
     return true;
   }
 
-  private long count(final int n, final int[] majors, final String prefix) {
-    //System.out.println(prefix + n + " remains, major=" + Arrays.toString(majors));
-    if (n == 0) {
-      return 1;
-    }
-    if (majors.length == 0) {
-      return 0;
-    }
-    long cnt = 0;
-    for (int arm = n; arm > 0; --arm) {
-      long c = 0;
-      final IntegerPartition part = new IntegerPartition(arm);
-      int[] p;
-      while ((p = part.next()) != null) {
-        //System.out.println(prefix + "Partition arm=" + arm + " " + Arrays.toString(p));
-        if (!isPerfectlyBalanced(p)) {
-          continue;
-        }
-        final int residue = n - 3 * (arm - p[0]) - 1;
-        if (residue < 0) {
-          continue;
-        }
-        //System.out.println(prefix + "Balanced: n=" + arm + " " + Arrays.toString(p) + " leaves " + residue);
-        if (isOverMajored(majors, p)) {
-          final long contrib = count(residue, Arrays.copyOfRange(p, 1, p.length), prefix + "  ");
-          //System.out.println(prefix + "n=" + arm + " r=" + residue + " " + Arrays.toString(p) + " contributes " + contrib);
-          c += contrib;
+  private long count(final int n, final int[] majors) {
+    long c = 0;
+    if (majors.length != 0) {
+      for (Map.Entry<int[], Long> e : mCache.get(n).entrySet()) {
+        if (isOverMajored(majors, e.getKey())) {
+          c += e.getValue();
         }
       }
-      cnt += c;
     }
-    return cnt;
+    return c;
   }
+
+//  private void printCache(final int n) {
+//    System.out.print("n=" + n + ": ");
+//    for (Map.Entry<int[], Long> e : mCache.get(n).entrySet()) {
+//      System.out.print(Arrays.toString(e.getKey()) + " -> " + e.getValue() + ", ");
+//    }
+//    System.out.println();
+//  }
 
   @Override
   public Z next() {
     if (++mN == 1) {
+      mCache.add(Collections.singletonMap(new int[] {}, 1L)); // n = 0
+      mCache.add(Collections.singletonMap(new int[] {1}, 1L));
       return Z.ONE;
     }
     //System.out.println("Trying for " + mN);
     long count = 0;
+    final TreeMap<int[], Long> map = new TreeMap<>(ARRAY_COMPARATOR);
+    mCache.add(map);
     for (int arm = mN; arm > 0; --arm) {
       long c = 0;
       final IntegerPartition part = new IntegerPartition(arm);
@@ -90,13 +101,16 @@ public class A048141 implements Sequence {
           continue;
         }
         //System.out.println("  Balanced: n=" + arm + " " + Arrays.toString(p) + " leaves " + residue);
-        final long pc = count(residue, Arrays.copyOfRange(p, 1, p.length), "    ");
+        final long pc = count(residue, Arrays.copyOfRange(p, 1, p.length));
         //System.out.println("  " + Arrays.toString(p) + " contributes " + pc);
+        if (pc != 0) {
+          map.put(p, pc);
+        }
         c += pc;
       }
       count += c;
     }
+    //printCache(mN);
     return Z.valueOf(count);
   }
-
 }

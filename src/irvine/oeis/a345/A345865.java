1182c5ee4d482dcfb9973121892f0463a62c7713
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-07-06 14:23:23 +1200

    Interim

---
+++ src/irvine/oeis/a345/A345865.java
@@ -0,0 +1,15 @@
+package irvine.oeis.a345;
+
+import irvine.oeis.LimitedSumOfLikePowersSequence;
+
+/**
+ * A345865.
+ * @author Sean A. Irvine
+ */
+public class A345865 extends LimitedSumOfLikePowersSequence {
+
+  /** Construct the sequence. */
+  public A345865() {
+    super(3, 2, 4, true, false);
+  }
+}

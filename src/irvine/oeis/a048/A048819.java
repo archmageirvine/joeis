e4c09f5d44903d3c4ea28760d3996734e6f8dab1
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-07-13 08:19:31 +1200

    Sequence

---
+++ src/irvine/oeis/a048/A048819.java
@@ -0,0 +1,28 @@
+package irvine.oeis.a048;
+
+import irvine.math.cr.CR;
+import irvine.math.cr.MinkowskiQuestionMark;
+import irvine.math.cr.UnaryCRFunction;
+import irvine.oeis.DecimalExpansionSequence;
+
+/**
+ * A048819.
+ * @author Sean A. Irvine
+ */
+public class A048819 extends DecimalExpansionSequence {
+
+  // todo perhaps this is not monotonic! -- it is supposedly continuous
+
+  private static final CR N = new UnaryCRFunction() {
+    @Override
+    public CR execute(final CR x) {
+      return new MinkowskiQuestionMark(x).subtract(x);
+    }
+  }.inverseMonotone(CR.valueOf("0.4203723", 10), CR.valueOf("0.4203724", 10)).execute(CR.ZERO);
+
+  @Override
+  protected CR getCR() {
+    return N;
+  }
+}
+

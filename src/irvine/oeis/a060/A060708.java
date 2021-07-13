1548568d55eeea2d955c33115d02988583a5d160
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-07-14 08:17:27 +1200

    Contribution from Georg Fischer, decexp.2021-07-13.good.tgz

---
+++ src/irvine/oeis/a060/A060708.java
@@ -0,0 +1,18 @@
+package irvine.oeis.a060;
+// manually (decexp) at 2021-07-13
+
+import irvine.math.cr.CR;
+import irvine.oeis.DecimalExpansionSequence;
+import irvine.oeis.PrependSequence;
+
+/**
+ * A060708 The Reuleaux Triangle constant.
+ * @author Georg Fischer
+ */
+public class A060708 extends PrependSequence {
+
+  /** Construct the sequence */
+  public A060708() {
+    super(new DecimalExpansionSequence(1, CR.PI.subtract(CR.THREE.sqrt()).divide(CR.TWO), 10), 0);
+  }
+}
---
+++ src/irvine/oeis/a164/A164629.java
@@ -0,0 +1,17 @@
+package irvine.oeis.a164;
+// manually (decexp) at 2021-07-13 17:23
+
+import irvine.math.cr.CR;
+import irvine.oeis.DecimalExpansionSequence;
+
+/**
+ * A164629 Expansion of phi (golden ratio) in base 5.
+ * @author Georg Fischer
+ */
+public class A164629 extends DecimalExpansionSequence {
+
+  /** Construct the sequence */
+  public A164629() {
+    super(1, CR.PHI, 5);
+  }
+}

9c86196f86a599fb599e4fe01362e23869e78012
Sean A. Irvine <sairvin@gmail.com>
2021-07-13 22:03:08 +1200

    Sequence

---
+++ src/irvine/oeis/a048/A048939.java
@@ -0,0 +1,32 @@
+package irvine.oeis.a048;
+
+import irvine.math.z.Z;
+import irvine.oeis.a014.A014575;
+
+/**
+ * A048939.
+ * @author Sean A. Irvine
+ */
+public class A048939 extends A014575 {
+
+  private Z mA = super.next();
+
+  @Override
+  protected long start() {
+    return 1000174288;
+  }
+
+  @Override
+  public Z next() {
+    if (mA == null) {
+      return null;
+    }
+    final Z res = mA;
+    mA = super.next();
+    if (mA.toString().length() > 10) {
+      mA = null;
+    }
+    return res;
+  }
+}
+

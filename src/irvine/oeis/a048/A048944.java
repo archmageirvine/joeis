e6b1164453ecfc97da9281c576b0beda0c994d8a
Sean A. Irvine <sairvin@gmail.com>
2021-07-15 06:59:04 +1200

    Sequences

---
+++ src/irvine/oeis/a048/A048944.java
@@ -0,0 +1,35 @@
+package irvine.oeis.a048;
+
+import irvine.factor.factor.Cheetah;
+import irvine.factor.util.FactorSequence;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A048944.
+ * @author Sean A. Irvine
+ */
+public class A048944 implements Sequence {
+
+  private long mN = 0;
+  private long mC = 1;
+  private long mNextCube = 1;
+
+  @Override
+  public Z next() {
+    while (true) {
+      if (++mN == mNextCube) {
+        mNextCube = ++mC * mC * mC;
+        return Z.valueOf(mN);
+      }
+      final FactorSequence fs = Cheetah.factor(mN);
+      for (final Z p : fs.toZArray()) {
+        final int e = fs.getExponent(p) % 3;
+        if (e == 2) {
+          return Z.valueOf(mN);
+        }
+      }
+    }
+  }
+}
+

0c5c4010d4054896385d4abdf72b688105db5b11
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-07-20 09:51:01 +1200

    Contribution from Georg Fischer, deseq.2021-07-19.22.tgz

---
+++ src/irvine/oeis/a070/A070912.java
@@ -0,0 +1,16 @@
+package irvine.oeis.a070;
+
+import irvine.math.cr.CR;
+import irvine.oeis.DecimalExpansionSequence;
+
+/**
+ * A070912 Binary expansion of BesselI(0, 2).
+ * @author Georg Fischer
+ */
+public class A070912 extends DecimalExpansionSequence {
+
+  /** Construct the sequence. */
+  public A070912() {
+    super(2, CR.TWO.besselI0(), 2);
+  }
+}

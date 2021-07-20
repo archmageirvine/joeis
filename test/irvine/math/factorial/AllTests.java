58a425a10bf1966ccf618a2e0f63d7ba408d33fe
Sean A. Irvine <sairvin@gmail.com>
2021-07-20 21:49:24 +1200

    Removed unused factorial implementations

--- test/irvine/math/factorial/AllTests.java
+++ test/irvine/math/factorial/AllTests.java
@@ -12,18 +12,9 @@ public class AllTests extends TestSuite {
 
   public static Test suite() {
     final TestSuite suite = new TestSuite();
-    suite.addTestSuite(AlternatingFactorialSumSieveTest.class);
     suite.addTestSuite(BinarySplitFactorialTest.class);
-    suite.addTestSuite(BorweinFactorialTest.class);
-    suite.addTestSuite(FactorialSieveTest.class);
-    suite.addTestSuite(FourElementPartitionFactorialTest.class);
-    suite.addTestSuite(FourElementPartitionShiftFactorialTest.class);
     suite.addTestSuite(MemoryFactorialTest.class);
-    suite.addTestSuite(MoessnerFactorialTest.class);
     suite.addTestSuite(NaiveFactorialTest.class);
-    suite.addTestSuite(ThreeElementPartitionFactorialTest.class);
-    suite.addTestSuite(TwoElementPartitionFactorialTest.class);
-    suite.addTestSuite(TwoElementPartitionShiftFactorialTest.class);
     return suite;
   }
 

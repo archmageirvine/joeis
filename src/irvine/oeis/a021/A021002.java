b1cc854cdc8c8468d955583397e41e8b30276039
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-07-21 14:06:19 +1200

    Names

---
+++ src/irvine/oeis/a021/A021002.java
@@ -0,0 +1,34 @@
+package irvine.oeis.a021;
+
+import irvine.math.cr.CR;
+import irvine.math.cr.Zeta;
+import irvine.math.z.Z;
+import irvine.oeis.DecimalExpansionSequence;
+
+/**
+ * A021002.
+ * @author Sean A. Irvine
+ */
+public class A021002 extends DecimalExpansionSequence {
+
+  /** Construct the sequence. */
+  public A021002() {
+    super(new CR() {
+      @Override
+      protected Z approximate(final int precision) {
+        final int opPrec = 5 - precision;
+        Z prod = Z.ONE.shiftLeft(opPrec);
+        int k = 1;
+        while (true) {
+          final Z zeta = Zeta.zeta(++k).getApprox(-opPrec);
+          final Z prev = prod;
+          prod = prod.multiply(zeta).shiftRight(opPrec);
+          if (prod.equals(prev)) {
+            break;
+          }
+        }
+        return prod.shiftRight(5);
+      }
+    });
+  }
+}

06e652d62acaf0b3076363268674ac84614e6b36
Sean A. Irvine <sairvin@gmail.com>
2021-07-27 20:48:55 +1200

    Contribution from Georg Fischer, decexp.2021-07-27.10.tgz

---
+++ src/irvine/oeis/a273/A273512.java
@@ -0,0 +1,19 @@
+package irvine.oeis.a273;
+// manually (decexp) at 2021-07-27
+
+import irvine.math.cr.CR;
+import irvine.math.q.Q;
+import irvine.oeis.DecimalExpansionSequence;
+
+/**
+ * A273512 Expansion of Lemniscate constant or Gauss's constant in base 2.
+ * Formula: Pi^(3/2)/Gamma[3/4]^2*Sqrt[2]/2
+ * @author Georg Fischer
+ */
+public class A273512 extends DecimalExpansionSequence {
+
+  /** Construct the sequence */
+  public A273512() {
+    super(2, CR.PI.pow(CR.valueOf(new Q(3, 2))).divide(CR.valueOf(new Q(3, 4)).gamma().pow(CR.TWO)).multiply(CR.SQRT2).divide(CR.TWO), 2);
+  }
+}

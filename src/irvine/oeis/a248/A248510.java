2308eab3a2a933dddec4be414e1556dc857d29b9
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-08-12 09:32:58 +1200

    Contribution from Georg Fischer, decsolv.2021-08-11.21.tgz, decexp.2021-08-11.22.tgz

---
+++ src/irvine/oeis/a248/A248510.java
@@ -0,0 +1,19 @@
+package irvine.oeis.a248;
+// manually decexp at 2021-08-11 22:14
+
+import irvine.math.cr.CR;
+import irvine.math.z.Z;
+import irvine.oeis.DecimalExpansionSequence;
+
+/**
+ * A248510 Decimal expansion of von Klitzing constant R(K) (ohms).
+ * Formula: 5521725125000000000000/213914163877964163
+ * @author Georg Fischer
+ */
+public class A248510 extends DecimalExpansionSequence {
+
+  /** Construct the sequence */
+  public A248510() {
+    super(5, CR.valueOf(new Z("5521725125000000000000")).divide(CR.valueOf(new Z("213914163877964163"))));
+  }
+}

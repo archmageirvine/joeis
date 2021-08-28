42b133226984d6a67cc071406d5529cbdefca2fd
Sean A. Irvine <sairvin@gmail.com>
2021-08-28 22:00:20 +1200

    Contribution from Georg Fischer, posins.2021-08-28.09.tgz

---
+++ src/irvine/oeis/a285/A285432.java
@@ -0,0 +1,17 @@
+package irvine.oeis.a285;
+// manually posins at 2021-08-27 09:06
+
+import irvine.oeis.PositionSequence;
+import irvine.oeis.a286.A286939;
+
+/**
+ * A285432 Positions of 0 in A286939; complement of A285564.
+ * @author Georg Fischer
+ */
+public class A285432 extends PositionSequence {
+
+  /** Construct the sequence. */
+  public A285432() {
+    super(1, new A286939(), 0);
+  }
+}
---
+++ src/irvine/oeis/a285/A285564.java
@@ -0,0 +1,17 @@
+package irvine.oeis.a285;
+// manually posins at 2021-08-27 09:06
+
+import irvine.oeis.PositionSequence;
+import irvine.oeis.a286.A286939;
+
+/**
+ * A285564 Positions of 1 in A286939; complement of A285432.
+ * @author Georg Fischer
+ */
+public class A285564 extends PositionSequence {
+
+  /** Construct the sequence. */
+  public A285564() {
+    super(1, new A286939(), 1);
+  }
+}

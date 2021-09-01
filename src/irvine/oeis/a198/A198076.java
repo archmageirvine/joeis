76615f750acb38d36a51b81ed4a0f17acf005e3a
Sean A. Irvine <sairvin@gmail.com>
2021-09-01 15:13:04 +1200

    Sequence

---
+++ src/irvine/oeis/a198/A198076.java
@@ -0,0 +1,20 @@
+package irvine.oeis.a198;
+
+import irvine.math.q.Q;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A198076.
+ * @author Sean A. Irvine
+ */
+public class A198076 implements Sequence {
+
+  private int mN = 0;
+
+  @Override
+  public Z next() {
+    //return CR.valueOf(++mN).add(CR.HALF).pow(mN).round();
+    return new Q(2L * ++mN + 1, 2).pow(mN).round();
+  }
+}

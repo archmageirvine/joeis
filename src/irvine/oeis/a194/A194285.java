9c97aa524880f484ff0ac4f0da01d5e6a96f61cf
Sean A. Irvine <sairvin@gmail.com>
2021-09-11 08:45:36 +1200

    Names

--- src/irvine/oeis/a194/A194285.java
+++ src/irvine/oeis/a194/A194285.java
@@ -6,7 +6,7 @@ import irvine.math.z.Z;
 import irvine.oeis.Sequence;
 
 /**
- * A194285 Triangular array: g(n,k)=number of fractional parts (i*sqrt(2))
+ * A194285 Triangular array:  g(n,k)=number of fractional parts (i*sqrt(2)) in interval [(k-1)/n, k/n], for 1&lt;=i&lt;=n, 1&lt;=k&lt;=n.
  * in interval [(k-1)/n, k/n], for 1<=i<=n, 1<=k<=n.
  * @author Georg Fischer
  */

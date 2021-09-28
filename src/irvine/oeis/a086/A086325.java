a9b50f34f56d3713a09c1eb0820137cc6899c4ff
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-09-28 14:12:35 +1300

    Remove shadow on mN

--- src/irvine/oeis/a086/A086325.java
+++ src/irvine/oeis/a086/A086325.java
@@ -10,16 +10,13 @@ import irvine.oeis.a000.A000166;
  */
 public class A086325 extends A000166 {
 
-  protected int mN;
-
   /** Construct the sequence. */
   public A086325() {
     super.next(); // skip A000166(0)
-    mN = 0;
   }
 
   @Override
   public Z next() {
-    return super.next().multiply(++mN);
+    return super.next().multiply(mN);
   }
 }

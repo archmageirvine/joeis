4df50db070c4d54c4fd070c0a356b28df075dd2c
Sean A. Irvine <sairvin@gmail.com>
2021-10-19 07:48:48 +1300

    Contribution from Georg Fischer, sumpown.2021-10-18.19.tgz

---
+++ src/irvine/oeis/a055/A055235.java
@@ -0,0 +1,32 @@
+package irvine.oeis.a055;
+// manually 2021-10-18
+
+import irvine.math.z.Z;
+import irvine.oeis.triangle.Triangle;
+
+/**
+ * A055235 Sums of two powers of 3. 
+ * @author Georg Fischer
+ */
+public class A055235 extends Triangle {
+
+  protected Z mBase;
+
+  /** Construct the sequence. */
+  public A055235() {
+    this(3);
+  }
+
+  /**
+   * Generic constructor with parameters
+   * @param base 
+   */
+  public A055235(final int base) {
+    mBase = Z.valueOf(base);
+  }
+
+  @Override
+  protected Z compute(final int n, final int k) {
+    return (k < 0 || k > n) ? Z.ZERO : mBase.pow(n).add(mBase.pow(k));
+  }
+}

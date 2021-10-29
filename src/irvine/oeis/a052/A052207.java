1f0e67d7610421171d5eae390d64da7932100000
Sean A. Irvine <sairvin@gmail.com>
2021-10-30 09:58:15 +1300

    Sequence

---
+++ src/irvine/oeis/a052/A052207.java
@@ -0,0 +1,49 @@
+package irvine.oeis.a052;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A052207.
+ * @author Sean A. Irvine
+ */
+public class A052207 implements Sequence {
+
+  private int mN = 0;
+  private long mCount = 0;
+
+  // WLOG can assume s1=+1 and multiply by 2 for symmetry
+
+  private void search(final int current, final int numZeros, final int remaining) {
+    if (remaining == 0) {
+      if (numZeros == 4) {
+        ++mCount;
+      }
+      return;
+    }
+    if (current == -1) {
+      if (numZeros < 4) {
+        search(0, numZeros + 1, remaining - 1);
+      }
+    } else {
+      search(current + 1, numZeros, remaining - 1);
+    }
+    if (current == 1) {
+      if (numZeros < 4) {
+        search(0, numZeros + 1, remaining - 1);
+      }
+    } else {
+      search(current - 1, numZeros, remaining - 1);
+    }
+  }
+
+  @Override
+  public Z next() {
+    if (++mN < 6) {
+      return Z.ZERO;
+    }
+    mCount = 0;
+    search(1, 1, mN - 1);
+    return Z.valueOf(mCount).multiply2();
+  }
+}

08f3e8d3048c73307a59e465cc095d3419750094
Sean A. Irvine <sairvin@gmail.com>
2021-11-09 10:31:18 +1300

    Contribution from Georg Fischer, trisimple.2021-11-08.22.tgz

---
+++ src/irvine/oeis/a059/A059036.java
@@ -0,0 +1,23 @@
+package irvine.oeis.a059;
+// manually trisimple at 2021-11-08
+
+import irvine.math.z.Z;
+import irvine.oeis.triangle.UpperLeftTriangle;
+
+/**
+ * A059036 In a triangle of numbers (such as that in A059032, A059033, A059034) how many entries lie above position (n,k)? Answer: T(n,k) = (n+1)*(k+1)-1 (n &gt;= 0, k &gt;= 0).
+ * T(n, k) = n + k + n*k.
+ * @author Georg Fischer
+ */
+public class A059036 extends UpperLeftTriangle {
+
+  /** Construct the sequence. */
+  public A059036() {
+    super(0, 0, -1);
+  }
+
+  @Override
+  public Z matrixElement(final int n, final int k) {
+    return Z.valueOf(n + k + n * k);
+  }
+}

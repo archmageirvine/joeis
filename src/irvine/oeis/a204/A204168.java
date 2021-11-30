51e198d79837ddf5f0da2444b2ee56f314c4df51
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2021-11-30 13:33:19 +1300

    Contribution from Georg Fischer, egfsim.2021-11-29.22.tgz

---
+++ src/irvine/oeis/a204/A204168.java
@@ -0,0 +1,16 @@
+package irvine.oeis.a204;
+
+import irvine.oeis.a003.A003057;
+import irvine.oeis.a203.A203991;
+
+/**
+ * A204168 Array: row n shows the coefficients of the characteristic polynomial of the n-th principal submatrix of (i+j), as in A003057.
+ * @author Georg Fischer
+ */
+public class A204168 extends A203991 {
+
+  /** Construct the sequence. */
+  public A204168() {
+    super(2, new A003057());
+  }
+}

8d1163ef4b897ff0074651b9c64cd7944ca27609
Sean A. Irvine <sairvin@gmail.com>
2022-01-01 07:46:08 +1300

    Graphs

---
+++ src/irvine/oeis/a053/A053578.java
@@ -0,0 +1,24 @@
+package irvine.oeis.a053;
+
+import irvine.math.LongUtils;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A053577.
+ * @author Sean A. Irvine
+ */
+public class A053578 implements Sequence {
+
+  private long mN = 1;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final long t = ++mN - LongUtils.phi(mN);
+      if ((t & (t - 1)) == 0) {
+        return Z.valueOf(t);
+      }
+    }
+  }
+}

4cf75bde3e74173c1d4bc8e02402baab67eb1a18
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2022-01-10 15:50:54 +1300

    general characteristic (i.e. works for GF(3))

---
+++ src/irvine/oeis/a053/A053658.java
@@ -0,0 +1,36 @@
+package irvine.oeis.a053;
+
+import java.util.Map;
+import java.util.TreeSet;
+
+import irvine.math.group.GeneralLinearGroup;
+import irvine.math.group.IntegersMod;
+import irvine.math.polynomial.CycleIndex;
+import irvine.math.polynomial.MultivariateMonomial;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+import irvine.util.Pair;
+
+/**
+ * A053651 Number of nonisomorphic cyclic subgroups of general linear group GL(n,2).
+ * @author Sean A. Irvine
+ */
+public class A053651 implements Sequence {
+
+  private int mN = 0;
+
+  @Override
+  public Z next() {
+    final GeneralLinearGroup<Z> g = new GeneralLinearGroup<>(++mN, new IntegersMod(2));
+    final CycleIndex ci = g.cycleIndex();
+    final TreeSet<Z> orders = new TreeSet<>();
+    for (final MultivariateMonomial m : ci.values()) {
+      Z order = Z.ONE;
+      for (final Map.Entry<Pair<String, Integer>, Z> e : m.entrySet()) {
+        order = order.lcm(Z.valueOf(e.getKey().right()));
+      }
+      orders.add(order);
+    }
+    return Z.valueOf(orders.size());
+  }
+}

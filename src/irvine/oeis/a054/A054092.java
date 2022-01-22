94cfc888a0fd462e56a6a19de148d694f1269c3f
Sean A. Irvine <sairvin@gmail.com>
2022-01-22 14:40:16 +1300

    Lambda

--- src/irvine/oeis/a054/A054092.java
+++ src/irvine/oeis/a054/A054092.java
@@ -1,6 +1,6 @@
 package irvine.oeis.a054;
 
-import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import irvine.math.z.Z;
 
@@ -13,19 +13,23 @@ public class A054092 extends A054090 {
   private int mN; // row index
   private int mTri; // current index of triangle element
   private int mStop; // sequential index of relevant triangle element
-  private final BiFunction<Integer, Integer, Integer> mStopFunction;
+  private final Function<Integer, Integer> mStopFunction;
 
-  /** Construct the sequence. */
-  public A054092(final BiFunction<Integer, Integer, Integer> stopFunction) {
+  /**
+   * Construct the sequence.
+   * @param firstStop index to first element of this sequence in the triangle
+   * @param stopFunction function giving distance to next element of sequence
+   */
+  protected A054092(final int firstStop, final Function<Integer, Integer> stopFunction) {
+    mStop = firstStop;
+    mStopFunction = stopFunction;
     mN = -1;
     mTri = 0;
-    mStop = 0;
-    mStopFunction = stopFunction;
   }
 
   /** Construct the sequence. */
   public A054092() {
-    this((tri, n) -> tri + n + 1);
+    this(0, (n) -> n + 1);
   }
 
   @Override
@@ -36,7 +40,7 @@ public class A054092 extends A054090 {
       result = super.next();
     }
     ++mN;
-    mStop = mStopFunction.apply(mTri, mN);
+    mStop += mStopFunction.apply(mN);
     return result;
   }
 
---
+++ src/irvine/oeis/a054/A054093.java
@@ -0,0 +1,13 @@
+package irvine.oeis.a054;
+
+/**
+ * A054092 T(n,n), array T as in A054090.
+ * @author Georg Fischer
+ */
+public class A054093 extends A054092 {
+
+  /** Construct the sequence. */
+  public A054093() {
+    super(1, (n) -> n + 2);
+  }
+}

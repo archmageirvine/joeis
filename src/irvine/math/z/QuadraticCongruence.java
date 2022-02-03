7610e1ed17b5a73fe5392630963c87b3b2c6d40e
Sean A. Irvine <sairvin@gmail.com>
2022-02-04 07:17:51 +1300

    wip

---
+++ src/irvine/math/z/QuadraticCongruence.java
@@ -0,0 +1,96 @@
+package irvine.math.z;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.TreeSet;
+
+/**
+ * Solve quadratic congruence equations.
+ * @author Sean A. Irvine
+ */
+public class QuadraticCongruence {
+
+  private static final Collection<Z> MOD4 = Arrays.asList(Z.ONE, Z.THREE);
+  private static final Collection<Z> MOD8 = Arrays.asList(Z.ONE, Z.THREE, Z.FIVE, Z.SEVEN);
+
+  /**
+   * Solve <code>x^2=a (mod p)</code>
+   * @param a constant
+   * @param p modulus
+   * @return solutions
+   */
+  public static Collection<Z> solve(Z a, final Z p) {
+    System.out.println("Trying to solve x^2=" + a + " (mod " + p + ")");
+//    if (a.signum() < 0) {
+//      a = a.add(p);
+//    }
+    final Z t = a.modSqrt(p);
+    if (t == null) {
+      return Collections.emptySet();
+    }
+    final TreeSet<Z> res = new TreeSet<>();
+    res.add(t);
+    res.add(p.subtract(t));
+    return res;
+  }
+
+  /**
+   * Solve <code>x^2=a (mod p)</code>
+   * @param a constant
+   * @param p modulus
+   * @return solutions
+   */
+  public static Collection<Z> solve(final Z a, final Z p, final int e) {
+    System.out.println("Trying to solve x^2=" + a + " (mod " + p + "^" +e + ")");
+    if (e == 1) {
+      return solve(a, p);
+    }
+    if (Z.TWO.equals(p)) {
+      if (e == 2) {
+        return a.mod(4) == 1 ? MOD4 : Collections.emptySet();
+      }
+      if (a.mod(8) != 1) {
+        return Collections.emptySet();
+      }
+      if (e == 3) {
+        return MOD8;
+      }
+      return null; //todo
+    }
+    return null;
+  }
+
+  /**
+   * Solve <code>a*x^2+b*x+c = 0 (mod p)</code>
+   * @param a coefficient
+   * @param b coefficient
+   * @param c coefficient
+   * @param p prime modulus
+   * @param e exponent on p
+   * @return list of solutions
+   */
+  public static Collection<Z> solve(final Z a, final Z b, final Z c, final Z p, final int e) {
+    final Z d = b.square().subtract(a.multiply(c).multiply(4));
+    final Z m = p.pow(e);
+    final TreeSet<Z> res = new TreeSet<>();
+    final Z inv = a.multiply2().modInverse(m);
+    for (final Z s : solve(d, p, e)) {
+      res.add(s.subtract(b).modMultiply(inv, m));
+    }
+    return res;
+  }
+
+  /**
+   * Noddy.
+   * @param args equation.
+   */
+  public static void main(final String... args) {
+    final Z a = new Z(args[0]);
+    final Z b = new Z(args[1]);
+    final Z c = new Z(args[2]);
+    final Z p = new Z(args[3]);
+    final int e = Integer.parseInt(args[4]);
+    System.out.println(solve(a, b, c, p, e));
+  }
+}

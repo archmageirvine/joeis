df62d4b9edeb23819f43513f89fd513c2193f22d
Sean A. Irvine <sairvin@gmail.com>
2022-02-23 13:50:37 +1300

    A054762

---
+++ src/irvine/oeis/a054/A054762.java
@@ -0,0 +1,46 @@
+package irvine.oeis.a054;
+
+import irvine.math.LongUtils;
+import irvine.math.z.Z;
+import irvine.oeis.MemorySequence;
+
+/**
+ * A054762.
+ * @author Sean A. Irvine
+ */
+public class A054762 extends MemorySequence {
+
+  private  boolean test(final long u, final long m) {
+    for (long k = 0; k < m; ++k) {
+      if (LongUtils.modPow(k, 3, m) == u) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isCubes(final Z n) {
+    final long t = n.longValueExact();
+    for (final Z a : this) {
+      final long m = a.longValueExact();
+      if (!test(t % m, m)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  protected Z computeNext() {
+    if (isEmpty()) {
+      return Z.ONE;
+    }
+    Z t = get(size() - 1);
+    while (true) {
+      t = t.add(1);
+      if (isCubes(t)) {
+        return t;
+      }
+    }
+  }
+}

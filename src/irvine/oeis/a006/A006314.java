48b83e1920790765467f2d18e468ed644d23986e
Sean A. Irvine <sairvin@gmail.com>
2022-04-07 08:37:42 +1200

    Contribution from Georg Fischer, A055494.2022-04-06.19.tgz

--- src/irvine/oeis/a006/A006314.java
+++ src/irvine/oeis/a006/A006314.java
@@ -6,16 +6,32 @@ import irvine.oeis.Sequence;
 /**
  * A006314 Numbers n such that n^8 + 1 is prime.
  * @author Sean A. Irvine
+ * @author Georg Fischer
  */
 public class A006314 implements Sequence {
 
-  private Z mN = Z.ZERO;
+  private Z mN;
+  private final int mExpon;
+
+  /** Construct the sequence. */
+  public A006314() {
+    this(16);
+  }
+
+  /**
+   * Generic constructor with parameters
+   * @param cyclo number of cyclotomic polynomial
+   */
+  public A006314(final int cyclo) {
+    mN = Z.ZERO;
+    mExpon = cyclo / 2;
+  }
 
   @Override
   public Z next() {
     while (true) {
       mN = mN.add(1);
-      if (mN.pow(8).add(1).isProbablePrime()) {
+      if (mN.pow(mExpon).add(1).isProbablePrime()) {
         return mN;
       }
     }
---
+++ src/irvine/oeis/a109/A109555.java
@@ -0,0 +1,53 @@
+package irvine.oeis.a109;
+
+import irvine.math.q.Q;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+import irvine.oeis.a000.A000040;
+import irvine.oeis.a000.A000720;
+
+/**
+ * A109555 a(n) = If[Floor[2*Mod[(Prime[n + 1] - Prime[n])*PrimePi[n]/n, 8]] == 0, Prime[n]]
+ * @author Georg Fischer
+ */
+public class A109555 extends A000040 {
+
+  private int mN; // current index
+  private Z mResidue;
+  private Z mPrimeN1;
+  private Sequence mSeq;
+
+  /** Construct the sequence. */
+  public A109555() {
+    this(0);
+  }
+
+  /**
+   * Generic constructor with parameter
+   * @param residue desired residue
+   */
+  public A109555(final int residue) {
+    mN = 0;
+    mResidue = Z.valueOf(residue);
+    mSeq = new A000720();
+    mPrimeN1 = super.next();
+  }
+
+  // floor(2*(((prime(k + 1) - prime(k))*PrimePi(k)) mod (8*k)) / k) = m
+  // Flatten[Table[If[Floor[2*Mod[(Prime[n + 1] - Prime[n])*PrimePi[n], 8*n]/n] == 0, Prime[n], {}], {n, 1, 400}]]
+  @Override
+  public Z next() {
+    ++mN;
+    Z primeN = mPrimeN1;
+    mPrimeN1 = super.next();
+    Z primePi = mSeq.next();
+    while (!new Q(mPrimeN1.subtract(primeN).multiply(primePi).mod(Z.EIGHT.multiply(mN)).multiply(2), Z.valueOf(mN)).floor().equals(mResidue)) {
+      // System.out.println("mN=" + mN + ", mPrimeN1=" + mPrimeN1  + ", primeN=" + primeN + ", primePi=" + primePi + ", mod=" + mPrimeN1.subtract(primeN).multiply(primePi).mod(Z.EIGHT.multiply(mN)));
+      ++mN;
+      primeN = mPrimeN1;
+      mPrimeN1 = super.next();
+      primePi = mSeq.next();
+    }
+    return primeN;
+  }
+}

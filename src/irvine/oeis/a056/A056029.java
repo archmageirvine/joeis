160fbe597f7fe809f0f9d066eec8c7fc837399c2
Sean A. Irvine <sairvin@gmail.com>
2022-04-13 09:16:23 +1200

    Sequence

---
+++ src/irvine/oeis/a056/A056029.java
@@ -0,0 +1,31 @@
+package irvine.oeis.a056;
+
+import irvine.math.cr.CR;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+import irvine.oeis.a033.A033307;
+import irvine.oeis.cons.DecimalExpansionSequence;
+
+/**
+ * A056029.
+ * @author Sean A. Irvine
+ */
+public class A056029 extends DecimalExpansionSequence {
+
+  public A056029() {
+    super(0, new CR() {
+      private final Sequence mChamperknowne = new A033307();
+      private Z mA = Z.ZERO;
+      private Z mB = Z.ONE;
+
+      @Override
+      protected Z approximate(final int precision) {
+        while (mA.bitLength() <= -precision) {
+          mA = mA.multiply(10).add(mChamperknowne.next());
+          mB = mB.multiply(10);
+        }
+        return scale(mA, -precision).divide(mB);
+      }
+    }, 2);
+  }
+}

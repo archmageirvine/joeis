6a4358ac1ea20c8a105be9358ba180e49ac64389
Sean A. Irvine <sairvin@gmail.com>
2022-04-29 15:22:39 +1200

    Bug fix

--- src/irvine/oeis/a047/A047704.java
+++ src/irvine/oeis/a047/A047704.java
@@ -2,10 +2,9 @@ package irvine.oeis.a047;
 
 import irvine.oeis.SetDifferenceSequence;
 import irvine.oeis.a000.A000578;
+import irvine.oeis.a003.A003072;
+import irvine.oeis.a003.A003325;
 import irvine.oeis.a003.A003328;
-import irvine.oeis.a025.A025395;
-import irvine.oeis.a025.A025403;
-import irvine.oeis.a338.A338667;
 
 /**
  * A047704 Numbers that are the sum of 5 but no fewer positive cubes.
@@ -15,6 +14,6 @@ public class A047704 extends SetDifferenceSequence {
 
   /** Construct the sequence. */
   public A047704() {
-    super(new SetDifferenceSequence(new SetDifferenceSequence(new SetDifferenceSequence(new A003328(), new A025403()), new A025395()), new A338667()), new A000578());
+    super(new SetDifferenceSequence(new SetDifferenceSequence(new SetDifferenceSequence(new A003328(), new A047703()), new A003072()), new A003325()), new A000578());
   }
 }

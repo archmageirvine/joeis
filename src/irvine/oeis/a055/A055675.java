f6c866a2de8ef3c96c373b2eedeea45c99be4f0b
Sean A. Irvine <sairvin@gmail.com>
2022-04-30 11:00:06 +1200

    A055674

---
+++ src/irvine/oeis/a055/A055675.java
@@ -0,0 +1,16 @@
+package irvine.oeis.a055;
+
+import irvine.math.z.Z;
+
+/**
+ * A055675.
+ * @author Sean A. Irvine
+ */
+public class A055675 extends A055674 {
+
+  @Override
+  public Z next() {
+    super.next();
+    return Z.valueOf(mB.get(mB.size() - 1));
+  }
+}

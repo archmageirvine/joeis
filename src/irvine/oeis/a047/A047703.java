a0e492b3f5861360fd867e71ad900bfd367ddc49
Sean A. Irvine <sairvin@gmail.com>
2022-04-29 14:20:01 +1200

    Bug fix

--- src/irvine/oeis/a047/A047703.java
+++ src/irvine/oeis/a047/A047703.java
@@ -2,9 +2,9 @@ package irvine.oeis.a047;
 
 import irvine.oeis.SetDifferenceSequence;
 import irvine.oeis.a000.A000578;
+import irvine.oeis.a003.A003072;
+import irvine.oeis.a003.A003325;
 import irvine.oeis.a003.A003327;
-import irvine.oeis.a025.A025395;
-import irvine.oeis.a338.A338667;
 
 /**
  * A047703 Numbers that are the sum of 4 but no fewer positive cubes.
@@ -14,6 +14,6 @@ public class A047703 extends SetDifferenceSequence {
 
   /** Construct the sequence. */
   public A047703() {
-    super(new SetDifferenceSequence(new SetDifferenceSequence(new A003327(), new A025395()), new A338667()), new A000578());
+    super(new SetDifferenceSequence(new SetDifferenceSequence(new A003327(), new A003072()), new A003325()), new A000578());
   }
 }

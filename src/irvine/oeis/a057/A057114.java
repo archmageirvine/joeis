fec790319824189b5995cf277992300458d76e51
Sean A. Irvine <sairvin@gmail.com>
2022-05-22 16:52:06 +1200

    Sequences

---
+++ src/irvine/oeis/a057/A057114.java
@@ -0,0 +1,59 @@
+package irvine.oeis.a057;
+
+import java.util.LinkedList;
+import java.util.TreeMap;
+
+import irvine.math.q.Q;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A057114 Permutation of N induced by the order-preserving permutation of the rational numbers (x -&gt; x+1); positions in Stern-Brocot tree.
+ * @author Sean A. Irvine
+ */
+public class A057114 implements Sequence {
+
+  // Full (-infinity,infinity) Stern-Brocot tree
+  private final LinkedList<Q> mBreadthFirst = new LinkedList<>();
+  private final TreeMap<Q, Long> mOrdering = new TreeMap<>();
+  private long mOrderingIndex = 0;
+  private Z[] mFullNum = {Z.NEG_ONE, Z.ZERO, Z.ONE};
+  private Z[] mFullDen = {Z.ZERO, Z.ONE, Z.ZERO};
+  {
+    mBreadthFirst.add(Q.ZERO);
+    mOrdering.put(Q.ZERO, ++mOrderingIndex);
+  }
+
+  private void stepSternBrocot() {
+    // Compute entire next row of the tree
+    final int len1 = 2 * mFullNum.length - 1;
+    final Z[] fullNum = new Z[len1];
+    final Z[] fullDen = new Z[len1];
+    fullNum[0] = mFullNum[0];
+    fullDen[0] = mFullDen[0];
+    for (int k = 0, j = 1; k < mFullNum.length - 1; ++k) {
+      final Z n = mFullNum[k + 1];
+      final Z d = mFullDen[k + 1];
+      fullNum[j] = mFullNum[k].add(n);
+      fullDen[j] = mFullDen[k].add(d);
+      final Q t = new Q(fullNum[j], fullDen[j]);
+      mBreadthFirst.add(t);
+      mOrdering.put(t, ++mOrderingIndex);
+      ++j;
+      fullNum[j] = n;
+      fullDen[j] = d;
+      ++j;
+    }
+    mFullNum = fullNum;
+    mFullDen = fullDen;
+  }
+
+  @Override
+  public Z next() {
+    final Q t = mBreadthFirst.pollFirst().add(1);
+    while (mOrdering.get(t) == null) {
+      stepSternBrocot();
+    }
+    return Z.valueOf(mOrdering.get(t));
+  }
+}
---
+++ src/irvine/oeis/a057/A057115.java
@@ -0,0 +1,15 @@
+package irvine.oeis.a057;
+
+import irvine.oeis.PositionOfNSequence;
+
+/**
+ * A057115.
+ * @author Sean A. Irvine
+ */
+public class A057115 extends PositionOfNSequence {
+
+  /** Construct the sequence. */
+  public A057115() {
+    super(new A057114());
+  }
+}

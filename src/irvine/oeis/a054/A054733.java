834658f262d6913ea5e59a5c029b42982577ddae
Sean A. Irvine <sairvin@gmail.com>
2022-05-30 08:55:07 +1200

    Sequences

--- src/irvine/oeis/a054/A054733.java
+++ src/irvine/oeis/a054/A054733.java
@@ -1,7 +1,5 @@
 package irvine.oeis.a054;
 
-import irvine.math.Mobius;
-import irvine.math.group.DegreeLimitedPolynomialRingField;
 import irvine.math.group.PolynomialRingField;
 import irvine.math.polynomial.Polynomial;
 import irvine.math.polynomial.PolynomialUtils;
@@ -24,24 +22,11 @@ public class A054733 extends A052283 {
     ++mK;
   }
 
-  /**
-   * Inverse Euler transform.
-   * @param p bivariate polynomial
-   * @param n maximum degree
-   * @param m maximum inner degree
-   * @return inverse Euler transform
-   */
-  public static Polynomial<Polynomial<Q>> inverseEuler(final Polynomial<Polynomial<Q>> p, final int n, final int m) {
-    final PolynomialRingField<Polynomial<Q>> r = new PolynomialRingField<>(new DegreeLimitedPolynomialRingField<>("y", Rationals.SINGLETON, m));
-    final Polynomial<Polynomial<Q>> q = r.log(p, n); // This is where Q is needed
-    return r.sum(1, n, i -> r.divide(r.multiply(PolynomialUtils.innerSubstitute(r, q.substitutePower(i, n), i, m), Polynomial.create(new Q(Mobius.mobius(i)))), Polynomial.create(new Q(i))));
-  }
-
   protected Polynomial<Z> getRow(final int n) {
     while (mP.size() <= n) {
       mP.add(PolynomialUtils.zToQ(g(mP.size())));
     }
-    return PolynomialUtils.qToZ(inverseEuler(mP, n, n * (n - 1)).coeff(n).shift(-1));
+    return PolynomialUtils.qToZ(PolynomialUtils.inverseEuler(mP, n, n * (n - 1)).coeff(n).shift(-1));
   }
 
   @Override
--- src/irvine/oeis/a057/A057276.java
+++ src/irvine/oeis/a057/A057276.java
@@ -5,10 +5,10 @@ import java.util.List;
 import irvine.math.graph.Edges;
 import irvine.math.group.DegreeLimitedPolynomialRingField;
 import irvine.math.polynomial.Polynomial;
+import irvine.math.polynomial.PolynomialUtils;
 import irvine.math.q.Q;
 import irvine.math.q.Rationals;
 import irvine.math.z.Z;
-import irvine.oeis.a054.A054733;
 import irvine.oeis.a054.A054951;
 
 /**
@@ -27,9 +27,9 @@ public class A057276 extends A054951 {
       ++mN;
       mM = 0;
       final DegreeLimitedPolynomialRingField<Q> fld = new DegreeLimitedPolynomialRingField<>("y", Rationals.SINGLETON, mN * (mN - 1));
-      final List<List<Polynomial<Q>>> gcid = graphCycleIndexData(mN, Edges.DIGRAPH_EDGES, fld, e -> fld.onePlusXToTheN(e));
-      final List<List<Polynomial<Q>>> inv = invGgfCiData(gcid, fld, e -> fld.onePlusXToTheN(e));
-      final Polynomial<Polynomial<Q>> res = A054733.inverseEuler(unlabeledOgf(fld, inv), mN, mN * (mN - 1));
+      final List<List<Polynomial<Q>>> graphs = graphCycleIndexData(mN, Edges.DIGRAPH_EDGES, fld, e -> fld.onePlusXToTheN(e));
+      final List<List<Polynomial<Q>>> inv = invGgfCiData(graphs, fld, e -> fld.onePlusXToTheN(e));
+      final Polynomial<Polynomial<Q>> res = PolynomialUtils.inverseEuler(unlabeledOgf(fld, inv), mN, mN * (mN - 1));
       mRow = res.coeff(mN);
     }
     return mRow.coeff(mM).toZ().negate();

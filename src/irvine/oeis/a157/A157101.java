92e77b92cd32dca03ed6b746162464029a5c250d
Sean A. Irvine <sairvin@gmail.com>
2022-06-01 21:13:59 +1200

    Fix the way initial terms are handled in MemorySequence, fix consequences in GeneralRecurrence so that offset works correctly

--- src/irvine/oeis/a157/A157101.java
+++ src/irvine/oeis/a157/A157101.java
@@ -11,5 +11,6 @@ public class A157101 extends GeneralRecurrence {
   /** Construct the sequence. */
   public A157101() {
     super(0, 1, -1, -5, -4);
+    mLambda.add(n -> a(n - 1).multiply(a(n - 3)).add(a(n - 2).square()).divide(a(n - 4)));
   }
 }

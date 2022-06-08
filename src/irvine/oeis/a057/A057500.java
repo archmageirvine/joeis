ed9f20a062ec7b112d692a9602e0232b742b48bb
Sean A. Irvine <sairvin@gmail.com>
2022-06-09 08:16:12 +1200

    Sequence

---
+++ src/irvine/oeis/a057/A057500.java
@@ -0,0 +1,23 @@
+package irvine.oeis.a057;
+
+import irvine.math.z.Z;
+import irvine.oeis.a052.A052121;
+
+/**
+ * A057500.
+ * @author Sean A. Irvine
+ */
+public class A057500 extends A052121 {
+
+  private long mN = -1;
+
+  @Override
+  public Z next() {
+    ++mN;
+    Z sum = Z.ZERO;
+    for (long k = 0; k <= mN * (mN - 1) / 2; ++k) {
+      sum = sum.add(super.next().multiply(k));
+    }
+    return sum;
+  }
+}

a867b46f21ff8bced72e0226d1a947a1a54d3e4e
Sean A. Irvine <sairvin@gmail.com>
2022-06-13 18:04:16 +1200

    Faster

--- src/irvine/oeis/a057/A057542.java
+++ src/irvine/oeis/a057/A057542.java
@@ -5,6 +5,7 @@ import irvine.oeis.MemorySequence;
 import irvine.oeis.PrependSequence;
 import irvine.oeis.a000.A000108;
 import irvine.oeis.a038.A038776;
+import irvine.util.array.LongDynamicBooleanArray;
 
 /**
  * A057542 Maximum cycle length in each permutation between A038776[1] and A038776[A000108[n]].
@@ -13,24 +14,26 @@ import irvine.oeis.a038.A038776;
 public class A057542 extends A000108 {
 
   private final MemorySequence mA = MemorySequence.cachedSequence(new PrependSequence(new A038776(), 0));
+  private final LongDynamicBooleanArray mSeen = new LongDynamicBooleanArray();
   private int mN = 0;
   private int mLongest = 1;
-  {
-    super.next();
-  }
 
   @Override
   public Z next() {
     final int lim = super.next().intValueExact();
     while (++mN < lim) {
-      int m = mN;
-      int cnt = 0;
-      do {
-        m = mA.a(m).intValueExact();
-        ++cnt;
-      } while (m != mN);
-      if (cnt > mLongest) {
-        mLongest = cnt;
+      if (!mSeen.isSet(mN)) {
+        int m = mN;
+        mSeen.set(m);
+        int cnt = 0;
+        do {
+          m = mA.a(m).intValueExact();
+          mSeen.set(m);
+          ++cnt;
+        } while (m != mN);
+        if (cnt > mLongest) {
+          mLongest = cnt;
+        }
       }
     }
     return Z.valueOf(mLongest);

f1bbb7109a1420b34d3adbb95f931b8750726cc3
Sean A. Irvine <sairvin@gmail.com>
2022-06-15 15:20:23 +1200

    Contribution from Georg Fischer, build.2022-06-14.21.tgz

--- src/irvine/oeis/a054/A054055.java
+++ src/irvine/oeis/a054/A054055.java
@@ -10,14 +10,28 @@ import irvine.oeis.Sequence;
 public class A054055 implements Sequence {
 
   private long mN = -1;
+  private final int mBase;
+
+  /** Construct the sequence. */
+  public A054055() {
+    this(10);
+  }
+
+  /**
+   * Generic constructor with parameter.
+   * @param base number base
+   */
+  public A054055(final int base) {
+    mBase = base;
+  }
 
   @Override
   public Z next() {
     long m = ++mN;
     long max = 0;
-    while (m != 0 && max != 9) {
-      final long r = m % 10;
-      m /= 10;
+    while (m != 0 && max != mBase - 1) {
+      final long r = m % mBase;
+      m /= mBase;
       if (r > max) {
         max = r;
       }
---
+++ src/irvine/oeis/a120/A120602.java
@@ -0,0 +1,63 @@
+package irvine.oeis.a120;
+
+import irvine.math.group.PolynomialRingField;
+import irvine.math.polynomial.Polynomial;
+import irvine.math.q.Q;
+import irvine.math.q.Rationals;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A120602 G.f. satisfies: 31*A(x) = 30 + 125*x + A(x)^6, starting with [1,5,15].
+ * @author Georg Fischer
+ */
+public class A120602 implements Sequence {
+
+  private int mN;
+  private int mFa;
+  private int mDiv;
+  private int mPow;
+  private Q[] mInits;
+  private int mInLen;
+  private Polynomial<Q> mA;
+  private Polynomial<Q> mX;
+  private static final PolynomialRingField<Q> RING = new PolynomialRingField<>(Rationals.SINGLETON);
+
+  /** Construct the sequence. */
+  public A120602() {
+    this(31, 25, 125, 6, 1, 5, 15);
+  }
+
+  /**
+   * Generic constructor with parameters. The equation is <ode>fa*A(x) = x0 + x1*x + A(x)^pow.
+   * @param fa
+   * @param div divisor
+   * @param x1
+   * @param pow
+   * @param inits initial terms
+   */
+  public A120602(final int fa, final int div, final int x1, final int pow, final long... inits) {
+    mN = -1;
+    mFa = fa;
+    mDiv = div;
+    mPow = pow;
+    mInLen = inits.length;
+    mInits = new Q[mInLen];
+    for (int i = 0; i < mInLen; ++i) {
+      mInits[i] = new Q(inits[i]);
+    }
+    mA = Polynomial.create(mInits);
+    mX = Polynomial.create(new Q(fa - 1), new Q(x1));
+  }
+
+  /* (PARI) {a(n)=local(A=1+x+6*x^2+x*O(x^n)); for(i=0, n, A=A-5*A+4+x+A^4); polcoeff(A, n)} */
+  @Override
+  public Z next() {
+    ++mN;
+    if (mN < mInLen) {
+      return mInits[mN].num();
+    }
+    mA = RING.add(mA, RING.divide(RING.add(RING.add(RING.multiply(mA, new Q(-mFa)), mX), RING.pow(mA, mPow, mN)), new Q(mDiv)));
+    return mA.coeff(mN).num();
+  }
+}

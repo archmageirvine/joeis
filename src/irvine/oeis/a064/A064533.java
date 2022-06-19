602df96c11b9c407666dfe6afc56154f152c9844
Sean A. Irvine <sairvin@gmail.com>
2022-06-19 21:54:22 +1200

    Sequences

--- src/irvine/oeis/a064/A064533.java
+++ src/irvine/oeis/a064/A064533.java
@@ -7,15 +7,21 @@ import irvine.math.z.Z;
 import irvine.oeis.cons.DecimalExpansionSequence;
 
 /**
- * A057574.
+ * A064533 Decimal expansion of Landau-Ramanujan constant.
  * @author Sean A. Irvine
  */
 public class A064533 extends DecimalExpansionSequence {
 
-  // todo not quite accurate enough
+  // todo not quite accurate enough -- found problems in my zeta
+
+  /*
+  (* Victor Adamchik calculated 5100 digits of the Landau-Ramanujan constant using Mathematica (from Mathematica 4 demos): *) LandauRamanujan[n_] := With[{K = Ceiling[Log[2, n*Log[3, 10]]]}, N[Product[(((1 - 2^(-2^k))*4^2^k*Zeta[2^k])/(Zeta[2^k, 1/4] - Zeta[2^k, 3/4]))^2^(-k - 1), {k, 1, K}]/Sqrt[2], n]];
+   */
 
   private static final CR ONE_QUARTER = CR.valueOf(Q.ONE_QUARTER);
-  private static final CR THREE_QUARTERS = CR.valueOf(new Q(3, 4));
+  //private static final CR THREE_QUARTERS = CR.valueOf(new Q(3, 4));
+  private static final Q THREE_QUARTERS = new Q(3, 4);
+  private static final int EXTRA_PRECISION = 0;
 
   /** Construct the sequence. */
   public A064533() {
@@ -25,26 +31,33 @@ public class A064533 extends DecimalExpansionSequence {
         final int j = 1 << k;
         final Q q = Q.ONE.subtract(new Q(Z.ONE, Z.ONE.shiftLeft(j)));
         final CR top = Zeta.zeta(j).multiply(q).multiply(Z.ONE.shiftLeft(2 * j));
-        final CR bot = Zeta.zetaHurwitz(j, ONE_QUARTER).subtract(Zeta.zetaHurwitz(j, THREE_QUARTERS));
+        final CR bot = Zeta.zetaHurwitz(j, Q.ONE_QUARTER).subtract(Zeta.zetaHurwitz(j, THREE_QUARTERS));
         return top.divide(bot).pow(new Q(1, 2L * j));
       }
 
       @Override
       protected Z approximate(final int precision) {
+        final int workingPrecision = precision - EXTRA_PRECISION;
+        System.out.println("prec=" + precision);
         int k = 0;
-        final Z scaledOne = CR.ONE.getApprox(precision);
+        final Z scaledOne = CR.ONE.getApprox(workingPrecision);
         Z prod = scaledOne;
         while (true) {
           final CR t = term(++k);
-          System.out.println("k=" + k + " t=" + t);
-          final Z term = t.getApprox(precision);
+          System.out.println("k=" + k + " t=" + t.toString());
+          final Z term = t.getApprox(workingPrecision);
+          prod = CR.scale(prod.multiply(term), workingPrecision);
           if (scaledOne.compareTo(term) >= 0) {
             break;
           }
-          prod = CR.scale(prod.multiply(term), precision);
         }
-        return prod;
+        return prod.shiftRight(EXTRA_PRECISION);
       }
     }.divide(CR.SQRT2));
+
+//    System.out.println(Zeta.zetaHurwitz(1 << 6, THREE_QUARTERS).toString(50));
+//    System.out.println(Zeta.zeta((1 << 6) + 1).toString(50));
+//    System.out.println(Zeta.zeta((1 << 6) + 63).toString(50));
+//    System.out.println(Zeta.zeta((1 << 6) + 101).toString(50));
   }
 }

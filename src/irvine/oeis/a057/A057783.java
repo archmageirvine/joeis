2112ea9a8ced891a0dede1ee5a3b726c355f555c
Sean A. Irvine <sairvin@gmail.com>
2022-07-02 11:11:35 +1200

    docs

--- src/irvine/oeis/a057/A057783.java
+++ src/irvine/oeis/a057/A057783.java
@@ -23,14 +23,17 @@ public class A057783 implements Sequence {
   @Override
   public Z next() {
     if (mCanons.isEmpty()) {
-      mCanons.add(new Animal(H.origin(), H.toPoint(2, 0)));
+      mCanons.add(new Animal(H.origin(), H.toPoint(2, 0))); // two adjacent hexes
     } else {
       final HashSet<Animal> newCanons = new HashSet<>();
       final HashSet<Pair<Long, Long>> tried = new HashSet<>(); // for efficiency only
       for (final Animal a : mCanons) {
         tried.clear();
         for (final long pt : a.points()) {
-          // Choose two new adjacent points for the dohex
+          // Choose two new adjacent points for the dohex. Check that these points
+          // are not already present in the animal and have not already been
+          // considered.  Form the free canonical form of the result and add it
+          // the set of new results.
           for (int k = 0; k < NEIGHBOURS; ++k) {
             final long q = H.neighbour(pt, k);
             if (!a.contains(q)) {

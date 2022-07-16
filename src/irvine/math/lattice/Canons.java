1d4533c19b8bac0ca90eeaea09a784487fe12792
Sean A. Irvine <sairvin@gmail.com>
2022-07-16 15:51:35 +1200

    Remove unused

--- src/irvine/math/lattice/Canons.java
+++ src/irvine/math/lattice/Canons.java
@@ -339,36 +339,4 @@ public final class Canons {
     }
     return canon;
   }
-
-  /**
-   * Return the canonical form of the given animal.
-   */
-  public static Animal dihedralCanon(final Animal animal) {
-    // todo something is broken here
-    final Animal a = translate(Lattices.Z2, animal);
-    //System.out.println("a=" + a.toString(Lattices.Z2));
-    Animal canon = a;
-    long extentX = a.extent(Lattices.Z2, 0);
-    long extentY = a.extent(Lattices.Z2, 1);
-    final Animal reflect = translate(Lattices.Z2, reflectHorizontal(a, extentX));
-    //System.out.println("reflect=" + reflect.toString(Lattices.Z2));
-    if (reflect.compareTo(canon) < 0) {
-      canon = reflect;
-      //System.out.println("Now canon");
-    }
-    final Animal vertical = translate(Lattices.Z2, reflectVertical(a, extentY));
-    //System.out.println("vertical=" + vertical.toString(Lattices.Z2));
-    if (vertical.compareTo(canon) < 0) {
-      canon = vertical;
-      //System.out.println("Now canon");
-    }
-    final Animal r2 = translate(Lattices.Z2, reflectHorizontal(vertical, extentX));
-    //System.out.println("rot180=" + r2.toString(Lattices.Z2));
-    if (r2.compareTo(canon) < 0) {
-      canon = r2;
-      //System.out.println("Now canon");
-    }
-//    System.out.println("canon=" + canon.toString(Lattices.Z2));
-    return canon;
-  }
 }

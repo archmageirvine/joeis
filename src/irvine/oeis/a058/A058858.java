5410ebe09ff9a14411b7c66d09b39888e945d344
Sean A. Irvine <sairvin@gmail.com>
2022-08-30 14:43:38 +1200

    A058858

---
+++ src/irvine/oeis/a058/A058858.java
@@ -0,0 +1,28 @@
+package irvine.oeis.a058;
+
+import irvine.math.z.Z;
+import irvine.oeis.a026.A026906;
+import irvine.util.string.StringUtils;
+
+/**
+ * A058845.
+ * @author Sean A. Irvine
+ */
+public class A058858 extends A026906 {
+
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private long mN = 0;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final Z t = super.next();
+      if (t.mod(++mN) == 0) {
+        return Z.valueOf(mN);
+      }
+      if (mVerbose && mN % 100000 == 0) {
+        StringUtils.message("Search complete to " + mN);
+      }
+    }
+  }
+}

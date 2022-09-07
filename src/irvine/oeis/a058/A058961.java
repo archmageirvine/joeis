71046fe89fd7f924c2e34c514e95fcd8ba445c66
Sean A. Irvine <sairvin@gmail.com>
2022-09-07 14:54:19 +1200

    A058961

---
+++ src/irvine/oeis/a058/A058961.java
@@ -0,0 +1,51 @@
+package irvine.oeis.a058;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence;
+
+/**
+ * A058961.
+ * @author Sean A. Irvine
+ */
+public class A058961 implements Sequence {
+
+  private final Set<Z> mSubsetSums = new HashSet<>();
+  private int mN = 0;
+
+  private boolean bump(final int[] set) {
+    for (int k = set.length - 1; k >= 0; --k) {
+      if (++set[k] < mN) {
+        for (int j = k + 1; j < set.length; ++j) {
+          set[j] = set[k];
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private Z search(final int[] multiset, final int pos, final int sum) {
+    if (pos >= multiset.length) {
+      return Z.ONE.shiftLeft(sum);
+    }
+    Z res = search(multiset, pos + 1, sum);
+    if (multiset[pos] != 0) {
+      final int s = (sum + multiset[pos]) % mN;
+      res = res.or(search(multiset, pos + 1, s));
+    }
+    return res;
+  }
+
+  @Override
+  public Z next() {
+    final int[] multiset = new int[mN++];
+    mSubsetSums.clear();
+    do {
+      mSubsetSums.add(search(multiset, 0, 0));
+    } while (bump(multiset));
+    return Z.valueOf(mSubsetSums.size());
+  }
+}

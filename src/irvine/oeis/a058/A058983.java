12cdb144c6a76530f49f9ed04af967628f06d19e
Sean A. Irvine <sairvin@gmail.com>
2022-09-09 11:43:27 +1200

    Sequences

---
+++ src/irvine/oeis/a058/A058983.java
@@ -0,0 +1,15 @@
+package irvine.oeis.a058;
+
+import irvine.oeis.RecordPositionSequence;
+
+/**
+ * A058978.
+ * @author Sean A. Irvine
+ */
+public class A058983 extends RecordPositionSequence {
+
+  /** Construct the sequence. */
+  public A058983() {
+    super(new A058981(), 0);
+  }
+}

fefb5cbaa71fc7a0142a69c155301d61fbb318a9
Sean A. Irvine <sairvin@gmail.com>
2022-10-22 07:43:43 +1300

    Fix offset

--- src/irvine/oeis/a060/A060034.java
+++ src/irvine/oeis/a060/A060034.java
@@ -14,6 +14,6 @@ public class A060034 extends Combiner {
 
   /** Construct the sequence. */
   public A060034() {
-    super(new SkipSequence(new A000041(), 1), new Combiner(new SkipSequence(new A000837(), 1), new A024994(), ADD), SUBTRACT);
+    super(1, new SkipSequence(new A000041(), 1), new Combiner(new SkipSequence(new A000837(), 1), new A024994(), ADD), SUBTRACT);
   }
 }

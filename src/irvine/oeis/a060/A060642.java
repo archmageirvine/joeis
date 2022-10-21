1e3f6f1c26a3cfd714a8ecc808cb56f3eecd4d5e
Georg Fischer <dr.georg.fischer@gmail.com>
2022-10-21 20:57:16 +0200

    PartitionTransformTriangle inheritance problems

--- src/irvine/oeis/a060/A060642.java
+++ src/irvine/oeis/a060/A060642.java
@@ -1,17 +1,51 @@
 package irvine.oeis.a060;
 
-import irvine.oeis.SkipSequence;
-import irvine.oeis.a000.A000041;
-import irvine.oeis.triangle.PartitionTransformTriangle;
+import irvine.factor.factor.Jaguar;
+import irvine.math.MemoryFunction2;
+import irvine.math.z.Binomial;
+import irvine.math.z.Z;
+import irvine.oeis.triangle.Triangle;
 
 /**
  * A060642 Triangle read by rows: row n lists number of ordered partitions into k parts of partitions of n.
  * @author Georg Fischer
  */
-public class A060642 extends PartitionTransformTriangle {
+public class A060642 extends Triangle {
 
   /** Construct the sequence. */
   public A060642() {
-    super(1, new SkipSequence(new A000041(), 1));
+    hasRAM(true);
+  }
+
+  /* Maple:
+    A:= proc(n, k) option remember; `if`(n=0, 1, k*add(
+          A(n-j, k)*numtheory[sigma](j), j=1..n)/n)
+        end:
+    T:= (n, k)-> add(A(n, k-i)*(-1)^i*binomial(k, i), i=0..k):
+    seq(seq(T(n, k), k=1..n), n=1..12);  # _Alois P. Heinz_, Mar 12 2015
+  */
+  private final MemoryFunction2<Integer, Z> mA = new MemoryFunction2<Integer, Z>() {
+    @Override
+    protected Z compute(final Integer n, final Integer k) {
+      if (n == 0) {
+        return Z.ONE;
+      }
+      Z sum = Z.ZERO;
+      for (int j = 1; j <= n; ++j) {
+        sum = sum.add(get(n - j, k).multiply(Jaguar.factor(j).sigma()));
+      }
+      return sum.multiply(k).divide(n);
+    }
+  };
+
+  @Override
+  protected Z compute(int n, int k) {
+    ++n;
+    ++k;
+    Z sum = Z.ZERO;
+    for (int i = 0; i <= k; ++i) {
+      sum = sum.add(mA.get(n, k - i).multiply(Binomial.binomial(k, i)).multiply(((i & 1) == 0) ? 1 : -1));
+    }
+    return sum;
   }
 }

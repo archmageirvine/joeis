e4f76d147ccf195caf4b56820c2981875b58afe0
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2022-10-27 08:55:58 +1300

    Unix line ending style

--- src/irvine/oeis/a291/A291292.java
+++ src/irvine/oeis/a291/A291292.java
@@ -4,8 +4,8 @@ import irvine.oeis.recur.HolonomicRecurrence;
 
 /**
  * A291292 Necklace Catalan numbers.
- * FindRE((3/2) + (x - sqrt(1 - 4*x))*(2*x - 1)/(6*x - 2),x,a(n));
- * -6*n*(11*n-9)*a(n)+12*(2*n-7)*(n-4)*a(n-3)+(-158*n^2+744*n-862)*a(n-2)+2*(n-1)*(79*n-143)*a(n-1)+(n+1)*(13*n+2)*a(n+1)-(n+1)*(n+2)*a(n+2) = 0
+ * <code>FindRE((3/2) + (x - sqrt(1 - 4*x))*(2*x - 1)/(6*x - 2),x,a(n));
+ * -6*n*(11*n-9)*a(n)+12*(2*n-7)*(n-4)*a(n-3)+(-158*n^2+744*n-862)*a(n-2)+2*(n-1)*(79*n-143)*a(n-1)+(n+1)*(13*n+2)*a(n+1)-(n+1)*(n+2)*a(n+2) = 0</code>
  * @author Georg Fischer
  */
 public class A291292 extends HolonomicRecurrence {

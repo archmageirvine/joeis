117f502123041e3cad6de120f80c261e5f4ccb0a
Sean A. Irvine <sairvin@gmail.com>
2022-11-08 16:35:10 +1300

    A060289

---
+++ src/irvine/oeis/a060/A060289.java
@@ -0,0 +1,32 @@
+package irvine.oeis.a060;
+
+import irvine.math.z.Z;
+import irvine.oeis.a005.A005349;
+
+/**
+ * A060286.
+ * @author Sean A. Irvine
+ */
+public class A060289 extends A005349 {
+
+  private Z mA = super.next();
+  private Z mLim = Z.ONE;
+  private long mCount = 0;
+
+  @Override
+  public Z next() {
+    mLim = mLim.multiply(10);
+    while (mA.compareTo(mLim) < 0) {
+      final Z t = mA;
+      mA = super.next();
+      if (mA.subtract(t).equals(Z.ONE)) {
+        final Z u = mA.add(t);
+        mA = super.next(); // Avoid overlaps
+        if (u.isProbablePrime()) {
+          ++mCount;
+        }
+      }
+    }
+    return Z.valueOf(mCount);
+  }
+}

2615c45e4584603817e8e94391ffc0aee61a7956
Sean A. Irvine <sairvin@gmail.com>
2022-12-03 10:12:59 +1300

    Sequence

---
+++ src/irvine/oeis/a358/A358349.java
@@ -0,0 +1,27 @@
+package irvine.oeis.a358;
+
+import irvine.math.MemoryFunctionInt2;
+import irvine.math.z.Z;
+import irvine.oeis.triangle.AntidiagonalSequence;
+
+/**
+ * A060589.
+ * @author Sean A. Irvine
+ */
+public class A358349 extends AntidiagonalSequence {
+
+  public A358349() {
+    super(1, new MemoryFunctionInt2<>() {
+      @Override
+      protected Z compute(final int n, final int m) {
+        if (n == 1) {
+          return Z.valueOf(m);
+        }
+        if (m == 1) {
+          return Z.ONE;
+        }
+        return get(n - 1, m - 1).multiply(get(n - 1, m)).add(get(n, m - 1));
+      }
+    });
+  }
+}

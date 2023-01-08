7355eaa57120107d03cc8809d3b9111a3a4d2de0
Sean A. Irvine <sairvin@gmail.com>
2023-01-09 08:01:31 +1300

    Easier range searching

--- src/irvine/oeis/a359/A359069.java
+++ src/irvine/oeis/a359/A359069.java
@@ -24,25 +24,30 @@ public class A359069 extends Sequence1 {
     return n.mod(4) != 0 && n.mod(9) != 0 && n.mod(25) != 0 && n.mod(49) != 0 && n.mod(121) != 0;
   }
 
-  @Override
-  public Z next() {
-    mN += 2;
-    Z p = Z.ONE;
-    Z reportAt = Z.valueOf(REPORT);
-    while (true) {
+  private Z search(final int n, final Z minP, final Z maxP) {
+    Z p = minP;
+    Z reportAt = minP.add(REPORT);
+    while (maxP == null || p.compareTo(maxP) <= 0) {
       p = mPrime.nextPrime(p);
-      final Z t = p.pow(mN).subtract(1);
+      final Z t = p.pow(n).subtract(1);
       if (precheck(t)) {
         final FactorSequence fs = TILLMAN.factorize(t);
-        if (fs.omega() == mN && fs.isSquareFree()) {
+        if (fs.omega() == n && fs.isSquareFree()) {
           return p;
         }
       }
       if (mVerbose && p.compareTo(reportAt) > 0) {
-        StringUtils.message("exponent " + mN + " no solution up to p=" + p);
+        StringUtils.message("exponent " + n + " no solution up to p=" + p);
         reportAt = reportAt.add(REPORT);
       }
     }
+    return null;
+  }
+
+  @Override
+  public Z next() {
+    mN += 2;
+    return search(mN, Z.ONE, null);
   }
 
   /**
@@ -51,7 +56,6 @@ public class A359069 extends Sequence1 {
    */
   public static void main(final String... args) {
     final A359069 seq = new A359069();
-    seq.mN = Integer.parseInt(args[0]) * 2 - 3;
-    System.out.println(seq.next());
+    System.out.println(seq.search(Integer.parseInt(args[0]) * 2 - 1, args.length > 1 ? new Z(args[1]) : Z.ONE, args.length > 2 ? new Z(args[2]) : null));
   }
 }

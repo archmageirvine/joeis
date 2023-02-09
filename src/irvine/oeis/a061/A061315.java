238368db7f80cb4f81337ed6e48b9d55dd15da11
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2023-02-09 16:48:27 +1300

    Sequence

---
+++ src/irvine/oeis/a061/A061315.java
@@ -0,0 +1,31 @@
+package irvine.oeis.a061;
+
+import irvine.math.MemoryFunctionInt2;
+import irvine.math.z.Z;
+import irvine.oeis.triangle.AntidiagonalSequence;
+
+/**
+ * A061314 Table by antidiagonals where T(n,k)=T(n,k-1)+T(n,k-1)^2/k^2 and T(n,0)=n.
+ * @author Sean A. Irvine
+ */
+public class A061315 extends AntidiagonalSequence {
+
+  /** Construct the sequence. */
+  public A061315() {
+    super(1, new MemoryFunctionInt2<>() {
+      @Override
+      protected Z compute(final int n, final int m) {
+        if (m == 1) {
+          return Z.valueOf(n);
+        }
+        final Z b = get(n, m - 1);
+        final Z c = b.add(m - 1).multiply(b);
+        if (c.mod(m) != 0) {
+          throw new UnsupportedOperationException("non-integer");
+        }
+        return c.divide(m);
+      }
+    });
+  }
+}
+

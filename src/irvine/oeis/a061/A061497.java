be0b4acd01e033f41c05e45a63d18979d35ee862
Sean A. Irvine <sairvin@gmail.com>
2023-02-19 09:42:43 +1300

    A061497

---
+++ src/irvine/oeis/a061/A061497.java
@@ -0,0 +1,34 @@
+package irvine.oeis.a061;
+
+import java.util.HashSet;
+
+import irvine.math.z.Z;
+import irvine.math.z.ZUtils;
+import irvine.oeis.Sequence1;
+import irvine.util.Permutation;
+
+/**
+ * A061497.
+ * @author Sean A. Irvine
+ */
+public class A061497 extends Sequence1 {
+
+  private long mN = 0;
+
+  @Override
+  public Z next() {
+    final Permutation perm = Permutation.permuter(Z.valueOf(++mN));
+    final HashSet<Z> set = new HashSet<>();
+    int[] p;
+    while ((p = perm.next()) != null) {
+      Z res = Z.ZERO;
+      for (final int d : p) {
+        res = res.multiply(10).add(d);
+        set.add(res);
+      }
+    }
+    set.remove(Z.ZERO);
+    return ZUtils.product(set);
+  }
+}
+

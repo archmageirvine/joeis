4b62db2cbe1020022086d13d46b1d4e062fee114
Sean A. Irvine <sairvin@gmail.com>
2023-03-08 21:49:33 +1300

    Factors

--- src/irvine/oeis/a056/A056187.java
+++ src/irvine/oeis/a056/A056187.java
@@ -1,6 +1,6 @@
 package irvine.oeis.a056;
 
-import irvine.factor.factor.Jaguar;
+import irvine.factor.factor.LeastPrimeFactorizer;
 import irvine.math.z.Z;
 
 /**
@@ -11,6 +11,6 @@ public class A056187 extends A056788 {
 
   @Override
   public Z next() {
-    return Jaguar.factor(super.next()).leastPrimeFactor();
+    return LeastPrimeFactorizer.lpf(super.next());
   }
 }

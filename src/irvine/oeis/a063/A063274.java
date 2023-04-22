f40e47d1e91f029d542da3bc43a7fde77e90ac56
Sean A. Irvine <sairvin@gmail.com>
2023-04-23 07:56:40 +1200

    Sequence

---
+++ src/irvine/oeis/a063/A063274.java
@@ -0,0 +1,40 @@
+package irvine.oeis.a063;
+
+import irvine.math.MemoryFunction1;
+import irvine.math.z.Z;
+import irvine.oeis.a001.A001694;
+import irvine.util.array.DynamicIntArray;
+
+/**
+ * A063274.
+ * @author Sean A. Irvine
+ */
+public class A063274 extends A001694 {
+
+  private final DynamicIntArray mA = new DynamicIntArray();
+  {
+    mA.set(0, super.next().intValueExact()); // 1
+  }
+  private int mN = 0;
+  private final MemoryFunction1<Integer> mB = new MemoryFunction1<>() {
+    @Override
+    protected Integer compute(final int n) {
+      if (n == 0) {
+        return 0;
+      }
+      while (n > mA.get(mA.length() - 1)) {
+        mA.set(mA.length(), A063274.super.next().intValueExact());
+      }
+      int res = Integer.MAX_VALUE;
+      for (int k = 0; k < mA.length() && mA.get(k) <= n; ++k) {
+        res = Math.min(res, get(n - mA.get(k)) + 1);
+      }
+      return res;
+    }
+  };
+
+  @Override
+  public Z next() {
+    return Z.valueOf(mB.get(++mN));
+  }
+}

18a729fd629c2b0f7f29f2f6add54d3ab39f8839
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2023-05-05 17:29:27 +1200

    Sequence

---
+++ src/irvine/oeis/a063/A063663.java
@@ -0,0 +1,30 @@
+package irvine.oeis.a063;
+
+import java.util.TreeSet;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A063177.
+ * @author Sean A. Irvine
+ */
+public class A063663 extends Sequence1 {
+
+  private final TreeSet<Z> mA = new TreeSet<>();
+  private long mN = 1;
+  private Z mS = Z.ONE;
+
+  @Override
+  public Z next() {
+    while (mA.isEmpty() || mA.first().compareTo(mS) <= 0) {
+      final Z n2 = Z.valueOf(mN).square();
+      for (long k = 1; k <= mN; ++k) {
+        final Z k2 = Z.valueOf(k).square();
+        mA.add(n2.multiply(k2).multiply(n2.add(k2)));
+      }
+      mS = Z.valueOf(++mN).pow(4);
+    }
+    return mA.pollFirst();
+  }
+}

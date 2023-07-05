da94d101465a98248f5a33b15aa6472f86ddc811
Sean A. Irvine <sairvin@gmail.com>
2023-07-05 13:21:22 +1200

    Tracking changes in OEIS, limit some testing

--- src/irvine/oeis/a015/A015909.java
+++ src/irvine/oeis/a015/A015909.java
@@ -1,22 +1,22 @@
 package irvine.oeis.a015;
 
 import irvine.math.z.Z;
-import irvine.oeis.Sequence3;
+import irvine.oeis.Sequence1;
 
 /**
  * A015909 Inverse of A015908, that is, k is the first occurrence for which n | k^n + k.
  * @author Sean A. Irvine
  */
-public class A015909 extends Sequence3 {
+public class A015909 extends Sequence1 {
 
-  private int mN = 2;
+  private int mN = 0;
 
   @Override
   public Z next() {
     final Z n = Z.valueOf(++mN);
-    long k = 1;
+    long k = 0;
     while (true) {
-      k += 2;
+      ++k;
       if ((Z.valueOf(k).modPow(n, n).longValueExact() + k) % mN == 0) {
         return Z.valueOf(k);
       }

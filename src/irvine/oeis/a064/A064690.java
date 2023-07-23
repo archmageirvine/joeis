defff7ad4e60140d980992b957a49ae93b5e0135
Sean A. Irvine <sairvin@gmail.com>
2023-07-23 18:09:21 +1200

    Add progress

--- src/irvine/oeis/a064/A064690.java
+++ src/irvine/oeis/a064/A064690.java
@@ -3,6 +3,7 @@ package irvine.oeis.a064;
 import irvine.math.cr.CR;
 import irvine.math.z.Z;
 import irvine.oeis.Sequence1;
+import irvine.util.string.StringUtils;
 
 /**
  * A064690 Define a sequence of fractions by x(m+1) = (x(m)^2+x(m)-1)/(x(m)+1) with x(1)=1; sequences gives values of n such that sgn(x(n))&lt;&gt;sgn(x(n-1)).
@@ -10,28 +11,21 @@ import irvine.oeis.Sequence1;
  */
 public class A064690 extends Sequence1 {
 
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
   private long mN = 0;
   private CR mX = CR.ONE;
-
-//  private Z mP = Z.ONE;
-//  private Z mQ = Z.ONE;
 
   @Override
   public Z next() {
     while (true) {
       ++mN;
-//      Z p = mP;
-//      Z q = mQ;
-//      mP = p.square().add(p.multiply(q)).subtract(q.square());
-//      mQ = q.multiply(p.add(q));
-//      if ((p.signum() * q.signum()) != (mP.signum() * mQ.signum())) {
-//        return Z.valueOf(mN);
-//      }
-
       final CR y = mX;
       mX = mX.square().add(mX).subtract(CR.ONE).divide(mX.add(CR.ONE));
       if (y.signum() != mX.signum()) {
         return Z.valueOf(mN);
+      }
+      if (mVerbose && mN % 1000 == 0) {
+        StringUtils.message("Search completed to " + mN);
       }
     }
   }

704afb36665cd95a8917f4ead574c9c6fd2bc97b
Sean A. Irvine <sairvin@gmail.com>
2023-09-21 08:26:15 +1200

    Sequences

---
+++ src/irvine/oeis/a065/A065927.java
@@ -0,0 +1,15 @@
+package irvine.oeis.a065;
+
+import irvine.oeis.RecordSequence;
+
+/**
+ * A065926 Index values for new maxima in A065925.
+ * @author Sean A. Irvine
+ */
+public class A065927 extends RecordSequence {
+
+  /** Construct the sequence. */
+  public A065927() {
+    super(1, new A065925());
+  }
+}

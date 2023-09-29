af2c53cfb1cb1dbd138ea8f85e89bc05a0f036ec
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2023-09-29 16:54:40 +1300

    Sequences

---
+++ src/irvine/oeis/a028/A028444.java
@@ -0,0 +1,15 @@
+package irvine.oeis.a028;
+
+import irvine.oeis.NoncomputableSequence;
+
+/**
+ * A002844.
+ * @author Sean A. Irvine
+ */
+public class A028444 extends NoncomputableSequence {
+
+  /** Construct the sequence. */
+  public A028444() {
+    super(0, NONCOMPUTABLE, 0, 1, 4, 6, 13);
+  }
+}
---
+++ src/irvine/oeis/a040/A040173.java
@@ -0,0 +1,42 @@
+package irvine.oeis.a040;
+
+import irvine.math.group.SymmetricGroup;
+import irvine.math.q.Q;
+import irvine.math.set.FiniteSet;
+import irvine.math.set.Permutation;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A002844.
+ * @author Sean A. Irvine
+ */
+public class A040173 extends Sequence1 {
+
+  protected int mN = 0;
+
+  protected Z select(final Q n) {
+    return n.num();
+  }
+
+  @Override
+  public Z next() {
+    final SymmetricGroup<Integer> g = SymmetricGroup.create(++mN);
+    final Z size = g.size();
+    long cnt = 0;
+    long total = 0;
+    for (final Permutation<Integer> a : g) {
+      for (final Permutation<Integer> b : g) {
+        final int inc = a.equals(b) ? 1 : 2;
+        total += inc;
+        if (g.generateSubgroup(new FiniteSet<>(a, b)).size().equals(size)) {
+          cnt += inc;
+        }
+        if (inc == 1) {
+          break;
+        }
+      }
+    }
+    return select(new Q(cnt, total));
+  }
+}

fc6ea31babbc77a8e53a20773f764b0ff352f4a7
Sean A. Irvine <sairvin@gmail.com>
2023-10-03 08:12:45 +1300

    A301315

--- src/irvine/oeis/a301/A301315.java
+++ src/irvine/oeis/a301/A301315.java
@@ -1,31 +1,24 @@
 package irvine.oeis.a301;
 
-import java.util.function.BiFunction;
-
 import irvine.factor.prime.Puma;
 import irvine.math.z.Z;
 import irvine.oeis.MultiplicativeSequence;
-import irvine.oeis.Sequence1;
 import irvine.oeis.memory.MemorySequence;
 
 /**
  * A301315.
  * @author Sean A. Irvine
  */
-public class A301315 extends Sequence1 {
+public class A301315 extends MemorySequence {
 
-  private final MemorySequence mSeq = MemorySequence.cachedSequence(new MultiplicativeSequence(1, new BiFunction<>() {
-    @Override
-    public Z apply(final Z p, final Integer e) {
-      return Puma.primeZ(p).pow(mSeq.a(e));
-    }
-  }));
   {
-    mSeq.add(null); // 0 unused
+    add(null); // 0 unused
+    setOffset(1);
   }
+  private final MultiplicativeSequence mB = new MultiplicativeSequence(1, (p, e) -> Puma.primeZ(p).pow(a(e)));
 
   @Override
-  public Z next() {
-    return mSeq.next();
+  protected Z computeNext() {
+    return mB.next();
   }
 }

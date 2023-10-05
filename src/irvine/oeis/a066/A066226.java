471c1f60d115991a3066be773040fa2cb9dbe84e
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2023-10-06 09:17:03 +1300

    A066226

---
+++ src/irvine/oeis/a066/A066226.java
@@ -0,0 +1,42 @@
+package irvine.oeis.a066;
+
+import irvine.factor.factor.Jaguar;
+import irvine.math.MemoryFunction1;
+import irvine.math.z.Euler;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A066207.
+ * @author Sean A. Irvine
+ */
+public class A066226 extends Sequence1 {
+
+  private int mN = 1;
+  private final MemoryFunction1<Long> mF = new MemoryFunction1<>() {
+    @Override
+    protected Long compute(final int n) {
+      return Jaguar.factor(Euler.phi((long) n)).sigma().longValueExact();
+    }
+  };
+
+  private boolean is(final int n) {
+    long sum = 0;
+    for (final Z d : Jaguar.factor(n).divisors()) {
+      sum += mF.get(d.intValueExact());
+    }
+    return sum == 2 * mF.get(n);
+  }
+
+  @Override
+  public Z next() {
+    while (true) {
+      if (++mN < 0) {
+        throw new UnsupportedOperationException();
+      }
+      if (is(mN)) {
+        return Z.valueOf(mN);
+      }
+    }
+  }
+}

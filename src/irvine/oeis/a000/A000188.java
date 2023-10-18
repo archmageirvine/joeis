8f70c8862f330205a035ce41b303f3f967bbda83
Sean A. Irvine <sairvin@gmail.com>
2023-10-18 16:35:56 +1300

    A000188 with DirectSequence

--- src/irvine/oeis/a000/A000188.java
+++ src/irvine/oeis/a000/A000188.java
@@ -3,13 +3,14 @@ package irvine.oeis.a000;
 import irvine.factor.factor.Jaguar;
 import irvine.factor.util.FactorSequence;
 import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.Sequence1;
 
 /**
  * A000188 (1) Number of solutions to x^2 == 0 (mod n). (2) Also square root of largest square dividing n. (3) Also max_{ d divides n } gcd(d, n/d).
  * @author Sean A. Irvine
  */
-public class A000188 extends Sequence1 {
+public class A000188 extends Sequence1 implements DirectSequence {
 
   private int mN = 0;
   private final int mA, mB;
@@ -34,5 +35,22 @@ public class A000188 extends Sequence1 {
     }
     return r;
   }
+
+  @Override
+  public Z a(final int n) {
+    return a(Z.valueOf(n));
+  }
+
+  @Override
+  public Z a(final Z n) {
+    final FactorSequence fs = Jaguar.factor(n);
+    Z r = Z.ONE;
+    for (final Z p : fs.toZArray()) {
+      final int e = fs.getExponent(p);
+      r = r.multiply(p.pow((long) e / 2)); // a=1, b=2
+    }
+    return r;
+  }
+
 }
 

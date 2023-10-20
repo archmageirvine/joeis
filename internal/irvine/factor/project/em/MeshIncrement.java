42cfcf6211810cbc477923c36e9311c356cd3f7a
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2023-10-20 14:18:46 +1300

    EulerMesh

--- internal/irvine/factor/project/em/MeshIncrement.java
+++ internal/irvine/factor/project/em/MeshIncrement.java
@@ -7,6 +7,7 @@ import java.io.PrintStream;
 import java.util.Arrays;
 
 import irvine.factor.factor.Factorizer;
+import irvine.factor.factor.Jaguar;
 import irvine.factor.factor.TillmanFactorizer;
 import irvine.factor.util.FactorSequence;
 import irvine.math.z.Z;
@@ -27,6 +28,8 @@ public final class MeshIncrement {
   static {
     Arrays.fill(MIN_LENGTH_COMPOSITE, Integer.MAX_VALUE);
   }
+  private static long NEW_TILLMAN = 0;
+  private static long NEW_JAGUAR = 0;
 
   private static void writeFactors(final String line, final int openParen, final FactorSequence fs, final PrintStream out) {
     for (final Z f : fs.toZArray()) {
@@ -36,12 +39,12 @@ public final class MeshIncrement {
         System.err.println("-> Found prime factor " + f);
         out.println(line.substring(0, openParen) + f + (exp > 1 ? "^" + exp : ""));
       } else {
-        out.println(line.substring(0, openParen + 1) + f + ")2");
+        out.println(line.substring(0, openParen + 1) + f + ")");
       }
     }
   }
 
-  static void process(final String line, final int maxLevel, final int maxCompositeSize) {
+  static void process(final String line, final int[] tillman, final int[] jaguar, final boolean testPrimes) {
     if (line.startsWith("#")) {
       System.out.println(line);
       return;
@@ -64,34 +67,52 @@ public final class MeshIncrement {
         MIN_LENGTH_COMPOSITE[level] = length;
       }
     }
-    if (level > maxLevel) {
-      // Too far down the mesh to care about right now
-      System.out.println(line);
-      return;
-    }
 
-    // todo maybe checking all these prime is too slow
-    final Z n = new Z(line.substring(openParen + 1, closeParen));
-    final FactorSequence fs = new FactorSequence(n);
-    if (false && n.isProbablePrime()) {
-      System.err.println("Claimed composite " + n + " is actually prime (correcting)");
-      fs.remove(n);
-      fs.add(n, FactorSequence.PRIME);
-    } else {
-      //final int status = Integer.parseInt(line.substring(closeParen + 1));
-      if (length > maxCompositeSize) {
-        System.out.println(line);
+    if (testPrimes) {
+      final Z n = new Z(line.substring(openParen + 1, closeParen));
+      if (n.isProbablePrime()) {
+        System.err.println("Claimed composite " + n + " is actually prime (correcting)");
+        final FactorSequence fs = new FactorSequence();
+        fs.add(n, FactorSequence.PRIME);
+        writeFactors(line, openParen, fs, System.out);
         return;
       }
-      System.err.println("Attempting to factor level " + level + " n=" + n);
-      TILLMAN.factor(fs);
     }
-    // Spew out any factors found
-    writeFactors(line, openParen, fs, System.out);
+    if (level <= tillman[0] && length <= tillman[1]) {
+      final Z n = new Z(line.substring(openParen + 1, closeParen));
+      System.err.println("Attempting to Tillman factor level " + level + " n=" + n);
+      ++NEW_TILLMAN;
+      writeFactors(line, openParen, TILLMAN.factorize(n), System.out);
+    } else if (level <= jaguar[0] && length <= jaguar[1]) {
+      final Z n = new Z(line.substring(openParen + 1, closeParen));
+      System.err.println("Attempting to Jaguar factor level " + level + " n=" + n);
+      try {
+        final FactorSequence fs = Jaguar.factor(n);
+        ++NEW_JAGUAR;
+        writeFactors(line, openParen, fs, System.out);
+      } catch(final UnsupportedOperationException e) {
+        // No it was not factored
+        System.out.println(line);
+      }
+    } else {
+      System.out.println(line);
+    }
   }
 
-  private static final String MAX_COMPOSITE_FLAG = "max";
-  private static final String MAX_LEVEL_FLAG = "level";
+  private static final String TILLMAN_FLAG = "tillman";
+  private static final String JAGUAR_FLAG = "jaguar";
+  private static final String PRIME_FLAG = "prime";
+
+  private static int[] getFactorSpec(final String s) {
+    final int colon = s.indexOf(':');
+    if (colon < 0) {
+      return new int[] {Integer.parseInt(s), Integer.MAX_VALUE};
+    }
+    if (colon == 0) {
+      return new int[] {Integer.MAX_VALUE, Integer.parseInt(s.substring(1))};
+    }
+    return new int[] {Integer.parseInt(s.substring(0, colon)), Integer.parseInt(s.substring(colon + 1))};
+  }
 
   /**
    * Try and factor nodes meeting certain conditions.
@@ -100,13 +121,16 @@ public final class MeshIncrement {
    */
   public static void main(final String[] args) throws IOException {
     final CliFlags flags = new CliFlags("Mesh", "Compute and check the Euclid-Mullin mesh");
-    flags.registerOptional('m', MAX_LEVEL_FLAG, Integer.class, "level", "Maximum level at which to attempt factorization", 16);
-    flags.registerOptional('d', MAX_COMPOSITE_FLAG, Integer.class, "size", "Maximum length of composite to apply Tillman to", 80);
+    flags.registerOptional('t', TILLMAN_FLAG, String.class, "level:size", "Constraints for using the Tillman factorization engine", "0:0");
+    flags.registerOptional('j', JAGUAR_FLAG, String.class, "level:size", "Constraints for using the Jaguar factorization engine", "0:0");
+    flags.registerOptional('p', PRIME_FLAG, "Explicitly check if claimed composites are actually prime");
     flags.setFlags(args);
+    final int[] tillman = getFactorSpec((String) flags.getValue(TILLMAN_FLAG));
+    final int[] jaguar = getFactorSpec((String) flags.getValue(JAGUAR_FLAG));
     try (final BufferedReader mesh = new BufferedReader(new InputStreamReader(System.in))) {
       String line;
       while ((line = mesh.readLine()) != null) {
-        process(line, (Integer) flags.getValue(MAX_LEVEL_FLAG), (Integer) flags.getValue(MAX_COMPOSITE_FLAG));
+        process(line, tillman, jaguar, flags.isSet(PRIME_FLAG));
       }
     }
     System.err.println("Summary of number and smallest composite size by level");
@@ -114,6 +138,12 @@ public final class MeshIncrement {
       if (NUM_COMPOSITES[k] != 0) {
         System.err.println(k + " " + NUM_COMPOSITES[k] + " C" + MIN_LENGTH_COMPOSITE[k]);
       }
+    }
+    if (NEW_TILLMAN > 0) {
+      System.err.println("New Tillan factorizations: " + NEW_TILLMAN);
+    }
+    if (NEW_JAGUAR > 0) {
+      System.err.println("New Jaguar factorizations: " + NEW_JAGUAR);
     }
   }
 }

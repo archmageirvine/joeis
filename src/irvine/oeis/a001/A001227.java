60a934056507a5419c21d812dc60b1e6e97cd3e9
Sean A. Irvine <sairvin@gmail.com>
2023-10-29 15:32:59 +1300

    Contribution from Georg Fischer, 2023-10-17.2023-10-28.00.tgz

--- src/irvine/oeis/a001/A001227.java
+++ src/irvine/oeis/a001/A001227.java
@@ -2,24 +2,35 @@ package irvine.oeis.a001;
 
 import irvine.factor.factor.Jaguar;
 import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.Sequence1;
 
 /**
  * A001227 Number of odd divisors of n.
  * @author Sean A. Irvine
  */
-public class A001227 extends Sequence1 {
+public class A001227 extends Sequence1 implements DirectSequence {
 
   protected long mN = 0;
 
   @Override
-  public Z next() {
+  public Z a(final Z n) {
     long c = 0;
-    for (final Z d : Jaguar.factor(++mN).divisors()) {
+    for (final Z d : Jaguar.factor(n).divisors()) {
       if (d.isOdd()) {
         ++c;
       }
     }
     return Z.valueOf(c);
+  }
+
+  @Override
+  public Z a(final int n) {
+    return a(Z.valueOf(n));
+  }
+
+  @Override
+  public Z next() {
+    return a(Z.valueOf(++mN));
   }
 }
--- src/irvine/oeis/a007/A007732.java
+++ src/irvine/oeis/a007/A007732.java
@@ -2,19 +2,32 @@ package irvine.oeis.a007;
 
 import irvine.math.group.IntegersModMul;
 import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.Sequence1;
 
 /**
  * A007732 Period of decimal representation of 1/n.
  * @author Sean A. Irvine
  */
-public class A007732 extends Sequence1 {
+public class A007732 extends Sequence1 implements DirectSequence {
 
-  private long mN = 0;
+  private int mN = 0;
 
   @Override
-  public Z next() {
-    long n = ++mN;
+  public Z a(final Z k) {
+    Z n = k;
+    while (!n.testBit(0)) {
+      n = n.shiftRight(1);
+    }
+    while (n.mod(5) == 0) {
+      n = n.divide(5);
+    }
+    return n.equals(Z.ONE) ? Z.ONE : new IntegersModMul(n).order(Z.TEN);
+  }
+
+  @Override
+  public Z a(final int k) {
+    long n = k;
     while ((n & 1) == 0) {
       n >>>= 1;
     }
@@ -22,5 +35,10 @@ public class A007732 extends Sequence1 {
       n /= 5;
     }
     return n == 1 ? Z.ONE : new IntegersModMul(n).order(Z.TEN);
+  }
+
+  @Override
+  public Z next() {
+    return a(Z.valueOf(++mN));
   }
 }

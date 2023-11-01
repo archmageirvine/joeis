96f8fc111f33cb6c1639edc133cf226c595668ea
Sean A. Irvine <sairvin@gmail.com>
2023-11-01 14:11:11 +1300

    A366397

---
+++ src/irvine/oeis/a366/A366397.java
@@ -0,0 +1,18 @@
+package irvine.oeis.a366;
+
+import irvine.math.z.Z;
+import irvine.oeis.a001.A001203;
+import irvine.oeis.cons.DecimalExpansionFromContinuedFractionSequence;
+import irvine.oeis.transform.SimpleTransformSequence;
+
+/**
+ * A366397.
+ * @author Sean A. Irvine
+ */
+public class A366397 extends DecimalExpansionFromContinuedFractionSequence {
+
+  /** Construct the sequence. */
+  public A366397() {
+    super(1, new SimpleTransformSequence(new A001203(), Z.ONE::add));
+  }
+}

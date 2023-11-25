111743050756f04fa11d8660b64faba7dc34413e
Sean A. Irvine <sairvin@gmail.com>
2023-11-26 08:55:38 +1300

    Move isPractical into ZUtils

--- src/irvine/math/z/ZUtils.java
+++ src/irvine/math/z/ZUtils.java
@@ -1348,4 +1348,29 @@ public final class ZUtils {
     return s[0].add(k - 4).mod(2 * (k - 2)) == 0;
   }
 
+  /**
+   * Test if a number is a practical number.
+   * @param n number to test
+   * @return true iff the number is practical
+   */
+  public static boolean isPractical(final long n) {
+    if ((n & (n - 1)) == 0) {
+      return true; // Powers of 2 are members
+    }
+    if ((n & 1) == 1) {
+      return false; // All other terms are even
+    }
+    final FactorSequence fs = Jaguar.factor(n);
+    final Z[] fn = fs.toZArray();
+    Arrays.sort(fn);
+    Z p = Z.ONE;
+    for (int k = 1; k < fn.length; ++k) {
+      final Z sigma = Jaguar.factor(fn[k - 1].pow(fs.getExponent(fn[k - 1]))).sigma();
+      p = p.multiply(sigma);
+      if (fn[k].compareTo(p.add(1)) > 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
--- src/irvine/oeis/a005/A005153.java
+++ src/irvine/oeis/a005/A005153.java
@@ -1,10 +1,7 @@
 package irvine.oeis.a005;
 
-import java.util.Arrays;
-
-import irvine.factor.factor.Jaguar;
-import irvine.factor.util.FactorSequence;
 import irvine.math.z.Z;
+import irvine.math.z.ZUtils;
 import irvine.oeis.Sequence1;
 
 /**
@@ -15,31 +12,10 @@ public class A005153 extends Sequence1 {
 
   private long mN = 0;
 
-  private boolean isPractical(final long n) {
-    if ((n & (n - 1)) == 0) {
-      return true; // Powers of 2 are members
-    }
-    if ((n & 1) == 1) {
-      return false; // All other terms are even
-    }
-    final FactorSequence fs = Jaguar.factor(n);
-    final Z[] fn = fs.toZArray();
-    Arrays.sort(fn);
-    Z p = Z.ONE;
-    for (int k = 1; k < fn.length; ++k) {
-      final Z sigma = Jaguar.factor(fn[k - 1].pow(fs.getExponent(fn[k - 1]))).sigma();
-      p = p.multiply(sigma);
-      if (fn[k].compareTo(p.add(1)) > 0) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   @Override
   public Z next() {
     while (true) {
-      if (isPractical(++mN)) {
+      if (ZUtils.isPractical(++mN)) {
         return Z.valueOf(mN);
       }
     }

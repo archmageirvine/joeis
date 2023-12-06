5d5f079f80dfa034bdedf4b20ef2c4144e2f9357
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2023-12-07 09:58:12 +1300

    A067207

---
+++ src/irvine/oeis/a067/A067207.java
@@ -0,0 +1,30 @@
+package irvine.oeis.a067;
+
+import irvine.factor.factor.Jaguar;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+import irvine.util.string.StringUtils;
+
+/**
+ * A067207.
+ * @author Sean A. Irvine
+ */
+public class A067207 extends Sequence1 {
+
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private long mN = 0;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final String s = String.valueOf(++mN);
+      final String p = Jaguar.factor(mN).sigma2().toString();
+      if (p.endsWith(s)) {
+        return Z.valueOf(mN);
+      }
+      if (mVerbose && mN % 1000000 == 0) {
+        StringUtils.message("Search completed to " + mN);
+      }
+    }
+  }
+}

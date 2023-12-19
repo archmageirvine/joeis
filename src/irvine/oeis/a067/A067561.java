9eca368523cba2ad453cfcd5978976f2d5befb62
Sean A. Irvine <sairvin@gmail.com>
2023-12-20 11:47:43 +1300

    A067561

---
+++ src/irvine/oeis/a067/A067561.java
@@ -0,0 +1,29 @@
+package irvine.oeis.a067;
+
+import irvine.math.cr.CR;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+import irvine.util.string.StringUtils;
+
+/**
+ * A067180.
+ * @author Sean A. Irvine
+ */
+public class A067561 extends Sequence1 {
+
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private long mN = 0;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final CR m = CR.PI.multiply(Z.valueOf(++mN).square());
+      if (m.floor().isSquare() || m.ceil().isSquare()) {
+        return Z.valueOf(mN);
+      }
+      if (mVerbose && mN % 10000000 == 0) {
+        StringUtils.message("Search completed to " + mN);
+      }
+    }
+  }
+}

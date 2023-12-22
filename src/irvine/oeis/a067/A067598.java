5360b3082f1a130969dacc967e79e5ace18fd49e
Sean A. Irvine <sairvin@gmail.com>
2023-12-23 12:00:21 +1300

    A067598

---
+++ src/irvine/oeis/a067/A067598.java
@@ -0,0 +1,25 @@
+package irvine.oeis.a067;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+import irvine.oeis.prime.HomePrimeSequence;
+
+/**
+ * A067598.
+ * @author Sean A. Irvine
+ */
+public class A067598 extends Sequence1 {
+
+  private long mN = 20;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final Z t = new HomePrimeSequence(++mN, 10, false, HomePrimeSequence.HomePrimeType.ALONSO_WITH_ONE).skip().next();
+      if (t.mod(mN) == 0) {
+        return Z.valueOf(mN);
+      }
+    }
+  }
+}
+

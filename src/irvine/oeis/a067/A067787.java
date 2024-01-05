9a5bb9e5dea21ff9032fe8807b146efd9d4c1c89
Sean A. Irvine <sairvin@gmail.com>
2024-01-06 09:21:36 +1300

    A067787

---
+++ src/irvine/oeis/a067/A067787.java
@@ -0,0 +1,31 @@
+package irvine.oeis.a067;
+
+import irvine.factor.factor.Jaguar;
+import irvine.factor.prime.Puma;
+import irvine.factor.util.FactorSequence;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+import irvine.util.string.StringUtils;
+
+/**
+ * A001168.
+ * @author Sean A. Irvine
+ */
+public class A067787 extends Sequence1 {
+
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private long mN = 0;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final FactorSequence fs = Jaguar.factor(++mN);
+      if (fs.phi().equals(Puma.primePiZ(fs.sigma()))) {
+        return Z.valueOf(mN);
+      }
+      if (mVerbose && mN % 1000000 == 0) {
+        StringUtils.message("Search complete to " + mN);
+      }
+    }
+  }
+}

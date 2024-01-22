4d8d3ccc50a57e0bacfc77e5a72a19b557629bb0
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2024-01-22 15:25:22 +1300

    Contribution from Georg Fischer, man.2024-01-21.23.tgz

--- src/irvine/oeis/a055/A055881.java
+++ src/irvine/oeis/a055/A055881.java
@@ -1,30 +1,45 @@
 package irvine.oeis.a055;
 
 import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.Sequence1;
 
 /**
  * A055881 a(n) = largest m such that m! divides n.
  * @author Sean A. Irvine
  */
-public class A055881 extends Sequence1 {
+public class A055881 extends Sequence1 implements DirectSequence {
 
-  private long mN = 0;
-  private final long[] mFactorial = new long[20];
-  {
-    mFactorial[0] = 1;
-    for (int k = 1; k < mFactorial.length; ++k) {
-      mFactorial[k] = mFactorial[k - 1] * (k + 1);
+  private int mN = 0;
+  private static final long[] FACTORIAL = new long[]{1, 1, 2, 6, 24,
+    120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800L, 87178291200L, 1307674368000L,
+    20922789888000L, 355687428096000L, 6402373705728000L, 121645100408832000L, 2432902008176640000L // 20!
+  };
+
+  @Override
+  public Z a(final Z n) {
+    int k = 0;
+    final int flen = FACTORIAL.length;
+    while (k < flen && n.modZ(FACTORIAL[k]).isZero()) {
+      ++k;
     }
+    if (k >= flen) {
+      Z fk = Z.valueOf(FACTORIAL[flen - 1]).multiply(k);
+      while (n.mod(fk).isZero()) {
+        ++k;
+        fk = fk.multiply(k);
+      }
+    }
+    return Z.valueOf(k - 1);
+  }
+
+  @Override
+  public Z a(final int n) {
+    return a(Z.valueOf(n));
   }
 
   @Override
   public Z next() {
-    ++mN;
-    int k = 0;
-    while (mN % mFactorial[k] == 0) {
-      ++k;
-    }
-    return Z.valueOf(k);
+    return a(++mN);
   }
 }
--- src/irvine/oeis/a269/A269993.java
+++ src/irvine/oeis/a269/A269993.java
@@ -8,8 +8,8 @@ import irvine.math.z.Z;
 import irvine.oeis.AbstractSequence;
 
 /**
- * A269993 Denominators of r-Egyptian fraction expansion for sqrt(1/2), where r = (1,1/2,1/3,1/4,...)
- * Suppose that r is a sequence of rational numbers r(k) <= 1 for k >= 1, and that x is an irrational number in (0,1).
+ * A269993 Denominators of r-Egyptian fraction expansion for sqrt(1/2), where r = (1, 1/2, 1/3, 1/4,...)
+ * Suppose that r is a sequence of rational numbers r(k) <= 1 for k >= 1, and that x is an irrational number in (0, 1).
  * Let f(0) = x, n(k) = floor(r(k)/f(k-1)), and f(k) = f(k-1) - r(k)/n(k).
  * Then x = r(1)/n(1) + r(2)/n(2) + r(3)/n(3) + ... , the r-Egyptian fraction for x.
  * @author Georg Fischer
@@ -28,7 +28,7 @@ public class A269993 extends AbstractSequence {
   /**
    * Generic constructor with parameters
    * @param offset first index
-   * @param x start with this constant
+   * @param x start with this constant 
    * @param lambda expression for the running index <em>k</em>
    */
   public A269993(final int offset, final CR x, final Function<Integer, Z> lambda) {
@@ -50,7 +50,7 @@ public class A269993 extends AbstractSequence {
   }
 
   private Z n(final int k) {
-    return CR.valueOf(new Q(Z.ONE, mLambda.apply(k))).divide(f(k - 1)).ceil();
+   return CR.valueOf(new Q(Z.ONE, mLambda.apply(k))).divide(f(k - 1)).ceil();
   }
 
   @Override

94aeb9292b936e9b9554b03eb816b4fc9fe7a415
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2024-01-26 10:21:24 +1300

    A068075

---
+++ src/irvine/oeis/a068/A068075.java
@@ -0,0 +1,33 @@
+package irvine.oeis.a068;
+
+import irvine.math.cr.CR;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A068072.
+ * @author Sean A. Irvine
+ */
+public class A068075 extends Sequence1 {
+
+  private static final CR LOG10 = CR.TEN.log();
+  private Z mN = Z.ZERO;
+
+  @Override
+  public Z next() {
+    while (true) {
+      mN = mN.add(1);
+      if (Z.ONE.equals(mN)) {
+        return Z.ONE;
+      }
+      final Z n2 = mN.square();
+      if (n2.mod(10) != 0) {
+        final CR log = CR.valueOf(n2).log().divide(LOG10);
+        if (n2.compareTo(Z.TEN.pow(log.floor()).multiply(5)) <= 0
+          && Z.TEN.pow(log.add(1).floor()).subtract(n2).isSquare()) {
+          return n2;
+        }
+      }
+    }
+  }
+}

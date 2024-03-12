f8e1fae7a604a45cd48a991ee108fe018f908b31
Sean A. Irvine <sairvin@gmail.com>
2024-03-12 21:31:44 +1300

    A068744 (interim)

--- src/irvine/oeis/a068/A068744.java
+++ src/irvine/oeis/a068/A068744.java
@@ -36,71 +36,25 @@ public class A068744 extends Sequence0 {
   // todo rather than -n..n can we do 0..2n instead?
   // todo would it be better to pick e first
 
-  @Override
-  public Z next() {
-    ++mN;
-    // abc
-    // def
-    // ghi
-    long count = 0;
-    for (int a = -mN; a <= mN; ++a) {
-      for (int b = Math.max(-mN, a - mN); b <= Math.min(mN, a + mN); ++b) {
-        for (int c = Math.max(-mN, b - mN); c <= Math.min(mN, b + mN); ++c) {
-          for (int d = Math.max(-mN, a - mN); d <= Math.min(mN, a + mN); ++d) {
-            for (int e = Math.max(Math.max(-mN, b - mN), d - mN); e <= Math.min(Math.min(mN, b + mN), d + mN); ++e) {
-              for (int f = Math.max(Math.max(-mN, c - mN), e - mN); f <= Math.min(Math.min(mN, c + mN), e + mN); ++f) {
-                for (int g = Math.max(-mN, d - mN); g <= Math.min(mN, d + mN); ++g) {
-                  for (int h = Math.max(Math.max(-mN, e - mN), g - mN); h <= Math.min(Math.min(mN, e + mN), g + mN); ++h) {
-                    for (int i = Math.max(Math.max(-mN, f - mN), h - mN); i <= Math.min(Math.min(mN, f + mN), h + mN); ++i) {
-                      if (a == mN || b == mN || c == mN || d == mN || e == mN || f == mN || g == mN || h == mN || i == mN) {
-                        //System.out.println(mN + " [" + a + "," + b + "," + c + "," + d + "," + e + "," + f + "," + g + "," + h + "," + i + "]");
-                        ++count;
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    return Z.valueOf(count);
-  }
-
 //  @Override
 //  public Z next() {
 //    ++mN;
-//    // abcd
-//    // efgh
-//    // ijkl
-//    // mnop
+//    // abc
+//    // def
+//    // ghi
 //    long count = 0;
 //    for (int a = -mN; a <= mN; ++a) {
 //      for (int b = Math.max(-mN, a - mN); b <= Math.min(mN, a + mN); ++b) {
 //        for (int c = Math.max(-mN, b - mN); c <= Math.min(mN, b + mN); ++c) {
-//          for (int d = Math.max(-mN, c - mN); d <= Math.min(mN, c + mN); ++d) {
-//            for (int e = Math.max(-mN, a - mN); e <= Math.min(mN, a + mN); ++e) {
-//              for (int f = Math.max(Math.max(-mN, e - mN), b - mN); f <= Math.min(Math.min(mN, e + mN), b + mN); ++f) {
-//                for (int g = Math.max(Math.max(-mN, f - mN), c - mN); g <= Math.min(Math.min(mN, f + mN), c + mN); ++g) {
-//                  for (int h = Math.max(Math.max(-mN, g - mN), d - mN); h <= Math.min(Math.min(mN, g + mN), d + mN); ++h) {
-//                    for (int i = Math.max(-mN, e - mN); i <= Math.min(mN, e + mN); ++i) {
-//                      for (int j = Math.max(Math.max(-mN, f - mN), i - mN); j <= Math.min(Math.min(mN, f + mN), i + mN); ++j) {
-//                        for (int k = Math.max(Math.max(-mN, j - mN), g - mN); k <= Math.min(Math.min(mN, j + mN), g + mN); ++k) {
-//                          for (int l = Math.max(Math.max(-mN, k - mN), h - mN); l <= Math.min(Math.min(mN, k + mN), h + mN); ++l) {
-//                            for (int m = Math.max(-mN, i - mN); m <= Math.min(mN, i + mN); ++m) {
-//                              for (int n = Math.max(Math.max(-mN, m - mN), j - mN); n <= Math.min(Math.min(mN, m + mN), j + mN); ++n) {
-//                                for (int o = Math.max(Math.max(-mN, n - mN), k - mN); o <= Math.min(Math.min(mN, n + mN), k + mN); ++o) {
-//                                  for (int p = Math.max(Math.max(-mN, o - mN), l - mN); p <= Math.min(Math.min(mN, o + mN), l + mN); ++p) {
-//                                    if (a == mN || b == mN || c == mN || d == mN || e == mN || f == mN || g == mN || h == mN || i == mN || j == mN || k == mN || l == mN || m == mN || n == mN || o == mN || p == mN) {
-//                                      ++count;
-//                                    }
-//                                  }
-//                                }
-//                              }
-//                            }
-//                          }
-//                        }
+//          for (int d = Math.max(-mN, a - mN); d <= Math.min(mN, a + mN); ++d) {
+//            for (int e = Math.max(Math.max(-mN, b - mN), d - mN); e <= Math.min(Math.min(mN, b + mN), d + mN); ++e) {
+//              for (int f = Math.max(Math.max(-mN, c - mN), e - mN); f <= Math.min(Math.min(mN, c + mN), e + mN); ++f) {
+//                for (int g = Math.max(-mN, d - mN); g <= Math.min(mN, d + mN); ++g) {
+//                  for (int h = Math.max(Math.max(-mN, e - mN), g - mN); h <= Math.min(Math.min(mN, e + mN), g + mN); ++h) {
+//                    for (int i = Math.max(Math.max(-mN, f - mN), h - mN); i <= Math.min(Math.min(mN, f + mN), h + mN); ++i) {
+//                      if (a == mN || b == mN || c == mN || d == mN || e == mN || f == mN || g == mN || h == mN || i == mN) {
+//                        System.out.println(mN + " [" + a + "," + b + "," + c + "," + d + "," + e + "," + f + "," + g + "," + h + "," + i + "]");
+//                        ++count;
 //                      }
 //                    }
 //                  }
@@ -113,4 +67,43 @@ public class A068744 extends Sequence0 {
 //    }
 //    return Z.valueOf(count);
 //  }
+
+  @Override
+  public Z next() {
+    ++mN;
+    // abc
+    // def
+    // ghi
+    long count = 0;
+    for (int ab = -mN; ab <= mN; ++ab) {
+      for (int be = -mN; be <= mN; ++be) {
+        for (int de = -mN; de <= mN; ++de) {
+          final int ad = ab - be - de;
+          if (ad >= -mN && ad <= mN) {
+            for (int bc = -mN; bc <= mN; ++bc) {
+              for (int cf = -mN; cf <= mN; ++cf) {
+                final int ef = cf - be + bc;
+                if (ef >= -mN && ef <= mN) {
+                  for (int fi = -mN; fi <= mN; ++fi) {
+                    for (int hi = -mN; hi <= mN; ++hi) {
+                      final int eh = ef - fi - hi;
+                      if (eh >= -mN && eh <= mN) {
+                        for (int gh = -mN; gh <= mN; ++gh) {
+                          final int dg = de - eh - gh;
+                          if (dg >= -mN && dg <= mN) {
+                            ++count;
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return Z.valueOf(count);
+  }
 }

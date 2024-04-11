fb0d20c3e019ed359b4e3d50974daadb711610a0
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2024-04-12 11:10:40 +1200

    A010357

---
+++ src/irvine/oeis/a010/A010357.java
@@ -0,0 +1,45 @@
+package irvine.oeis.a010;
+
+import irvine.math.graph.Graph;
+import irvine.math.nauty.GenerateGraphs;
+import irvine.math.nauty.GraphProcessor;
+import irvine.math.nauty.Multigraph;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A010357.
+ * @author Sean A. Irvine
+ */
+public class A010357 extends Sequence1 implements GraphProcessor {
+
+  private int mEdges = 0;
+  private Multigraph mMultigraph = null;
+
+  @Override
+  public void process(final Graph graph) {
+    mMultigraph.multi(graph, 0, mEdges, mEdges, Multigraph.NOLIMIT, Multigraph.NOLIMIT, false);
+  }
+
+  @Override
+  public Z next() {
+    if (++mEdges == 1) {
+      return Z.ONE;
+    }
+    Z count = Z.ONE;
+    for (int k = 0; k <= mEdges; ++k) {
+      final GenerateGraphs gg = new GenerateGraphs(1);
+      gg.setConnectionLevel(2);
+      gg.setVertices(k);
+      gg.setMaxEdges(mEdges);
+      gg.setMaxDeg(Multigraph.NOLIMIT);
+      gg.setProcessor(this);
+      gg.sanitizeParams();
+      mMultigraph = new Multigraph(null);
+      gg.run(false, false, false, 0, 0);
+      count = count.add(mMultigraph.getGraphsOutputCount());
+    }
+    return count;
+  }
+}
+

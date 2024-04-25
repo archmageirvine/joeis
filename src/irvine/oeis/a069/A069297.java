72a62befbd7c5e068de018582d147405adc9be5c
Sean A. Irvine <sairvin@gmail.com>
2024-04-25 13:26:14 +1200

    Sequences

--- src/irvine/oeis/a069/A069297.java
+++ src/irvine/oeis/a069/A069297.java
@@ -6,12 +6,12 @@ import irvine.math.z.Z;
  * A069295.
  * @author Sean A. Irvine
  */
-public class A069296 extends A069295 {
+public class A069297 extends A069295 {
 
   private int mN = 1;
 
   @Override
   public Z next() {
-    return count(++mN, 5);
+    return count(++mN, 6);
   }
 }

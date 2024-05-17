2b9d7f339da5fa2e45c7357619a73c77c6bb4ab4
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2024-05-17 17:18:37 +1200

    Sequences

---
+++ src/irvine/oeis/a002/A002810.java
@@ -0,0 +1,32 @@
+package irvine.oeis.a002;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A002810.
+ * @author Sean A. Irvine
+ */
+public class A002810 extends Sequence1 {
+
+  // After M. F. Hasler
+
+  private static final long[] SMALL = {0, 1, 7, 11, 27, 77, 107, 111, 127, 177, 777, 1127, 1177};
+  private int mN = 0;
+
+  private Z a(final long n) {
+    if (n > 58) {
+      throw new UnsupportedOperationException();
+    }
+    if (n < SMALL.length) {
+      return Z.valueOf(SMALL[(int) n]);
+    }
+    return a(n - 12).multiply(1000).add(777);
+  }
+
+  @Override
+  public Z next() {
+    return a(++mN);
+  }
+}
+
---
+++ src/irvine/oeis/a038/A038034.java
@@ -0,0 +1,41 @@
+package irvine.oeis.a038;
+
+import irvine.math.q.Q;
+import irvine.math.z.Binomial;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A002810.
+ * @author Sean A. Irvine
+ */
+public class A038034 extends Sequence1 {
+
+  private int mN = 0;
+  private int[] mCounts = null;
+  private Z mNumCompositions = Z.ZERO;
+
+  private void search(final Q remaining, final int den, final int parts) {
+    if (remaining.isZero()) {
+      mNumCompositions = mNumCompositions.add(Binomial.multinomial(parts, mCounts));
+      return;
+    }
+    for (int d = den; d <= mN; ++d) {
+      final Q v = remaining.subtract(new Q(1, d));
+      if (v.signum() >= 0) {
+        ++mCounts[d - 1];
+        search(v, d, parts + 1);
+        --mCounts[d - 1];
+      }
+    }
+  }
+
+  @Override
+  public Z next() {
+    mCounts = new int[++mN];
+    mNumCompositions = Z.ZERO;
+    search(Q.ONE, 1, 0);
+    return mNumCompositions;
+  }
+}
+

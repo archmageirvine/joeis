8813bb8c2ba733120b092247a9eb6826e36a7940
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2024-05-23 09:44:26 +1200

    Sequence

---
+++ src/irvine/oeis/a069/A069786.java
@@ -0,0 +1,25 @@
+package irvine.oeis.a069;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A068142.
+ * @author Sean A. Irvine
+ */
+public class A069786 extends Sequence1 {
+
+  private long mN = 0;
+
+  @Override
+  public Z next() {
+    final Z t = Z.valueOf(++mN).multiply(mN + 1).multiply(mN + 2).multiply(mN + 3);
+    long k = mN;
+    while (true) {
+      final Z u = Z.valueOf(++k).multiply(k + 1).multiply(k + 2).multiply(k + 3);
+      if (u.mod(t).isZero()) {
+        return Z.valueOf(k);
+      }
+    }
+  }
+}

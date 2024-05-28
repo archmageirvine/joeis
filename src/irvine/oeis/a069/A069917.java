e44e2c163796c2e86e84b1d5d1608ce2b8b4bb4e
Sean A. Irvine <sairvin@gmail.com>
2024-05-28 13:13:45 +1200

    Sequences

---
+++ src/irvine/oeis/a069/A069917.java
@@ -0,0 +1,29 @@
+package irvine.oeis.a069;
+
+import irvine.factor.factor.Jaguar;
+import irvine.math.function.Functions;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A069917.
+ * @author Sean A. Irvine
+ */
+public class A069917 extends Sequence1 {
+
+  private long mN = 27;
+
+  @Override
+  public Z next() {
+    while (true) {
+      final long rev = Functions.REVERSE.l(6, ++mN);
+      long sum = 0;
+      for (final Z d : Jaguar.factor(mN).divisors()) {
+        sum += Functions.REVERSE.l(6, d);
+      }
+      if (sum == 2 * rev) {
+        return Z.valueOf(mN);
+      }
+    }
+  }
+}

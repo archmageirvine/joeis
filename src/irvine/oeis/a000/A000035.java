f3920e4aad9ad04267927e51734397daafc6ea41
Sean A. Irvine <sairvin@gmail.com>
2024-06-18 15:42:58 +1200

    Contribution from Georg Fischer, 2024-06-17.2024-06-17.23.tgz

--- src/irvine/oeis/a000/A000035.java
+++ src/irvine/oeis/a000/A000035.java
@@ -1,13 +1,14 @@
 package irvine.oeis.a000;
 
 import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.Sequence0;
 
 /**
  * A000035 Period 2: repeat [0, 1]; a(n) = n mod 2; parity of n.
  * @author Sean A. Irvine
  */
-public class A000035 extends Sequence0 {
+public class A000035 extends Sequence0 implements DirectSequence {
 
   private long mN = -1;
 
@@ -15,5 +16,16 @@ public class A000035 extends Sequence0 {
   public Z next() {
     return (++mN & 1) == 0 ? Z.ZERO : Z.ONE;
   }
+
+  @Override
+  public Z a(final Z n) {
+    return n.testBit(0) ? Z.ONE : Z.ZERO;
+  }
+
+  @Override
+  public Z a(final int n) {
+    return (n & 1) == 0 ? Z.ZERO : Z.ONE;
+  }
+
 }
 

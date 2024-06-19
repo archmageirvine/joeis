8fb85f670323a925d6b59f7bbad63dfcd1f893b6
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2024-06-20 07:59:57 +1200

    Sequences

---
+++ src/irvine/oeis/a070/A070903.java
@@ -0,0 +1,37 @@
+package irvine.oeis.a070;
+
+import irvine.math.ContinuedFractionUtils;
+import irvine.math.function.Functions;
+import irvine.math.q.Q;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A070899.
+ * @author Sean A. Irvine
+ */
+public class A070903 extends Sequence1 {
+
+  private Q mSum = null;
+  private Z mM = Z.ONE;
+  private Z mN = Z.ZERO;
+
+  @Override
+  public Z next() {
+    mN = mN.add(1);
+    if (mSum == null) {
+      mSum = Q.ONE;
+      return Z.ONE;
+    }
+    final Z n3 = mN.pow(3);
+    while (true) {
+      mM = mM.add(1);
+      final Q t = mSum.add(new Q(Z.ONE, mM));
+      final Z max = Functions.MAX.z(ContinuedFractionUtils.continuedFraction(t));
+      if (max.equals(n3)) {
+        mSum = t;
+        return mM;
+      }
+    }
+  }
+}

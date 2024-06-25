9c0eaad698580c4b0bd7ea8a82af3430148ee3f8
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2024-06-26 10:50:28 +1200

    Contribution from Georg Fischer, 2024-06-25.2024-06-25.23.tgz

--- src/irvine/oeis/LambdaSequence.java
+++ src/irvine/oeis/LambdaSequence.java
@@ -3,6 +3,7 @@ package irvine.oeis;
 import java.util.function.Function;
 
 import irvine.math.z.Z;
+import irvine.math.z.ZUtils;
 
 /**
  * This class builds an infinite sequence from a lambda expression.
@@ -23,7 +24,9 @@ public class LambdaSequence extends AbstractSequence implements DirectSequence {
 
   private int mN; // current index
   private final Function<Integer, Z> mLambda; // lambda expression n -> f(n)
-
+  private Z[] mInits; // initial terms
+  private int mInitNo; // mInits.length
+  private int mIn; // index for mInits
   /**
    * Construct the sequence with offset 0 and no initial terms.
    * @param lambda lambda expression for an index variable starting at <code>offset</code>.
@@ -41,11 +44,50 @@ public class LambdaSequence extends AbstractSequence implements DirectSequence {
     super(offset);
     mN = offset - 1;
     mLambda = lambda;
+    mInitNo = 0;
+    mInits = new Z[0];
+    mIn = -1;
+  }
+
+  /**
+   * Construct the sequence with offset and a list of initial terms.
+   * @param offset first index
+   * @param lambda lambda expression for an index variable starting at <code>offset</code>.
+   * @param initTerms list of initial values for a(offset), a(offset + 1) etc.
+   */
+  public LambdaSequence(final int offset, final Function<Integer, Z> lambda, final long... initTerms) {
+    super(offset);
+    mN = offset - 1;
+    mLambda = lambda;
+    mIn = -1;
+    mInits = ZUtils.toZ(initTerms);
+    mInitNo = mInits.length;
+  }
+
+  /**
+   * Construct the sequence with offset and a list of initial terms given as a String.
+   * @param offset first index
+   * @param lambda lambda expression for an index variable starting at <code>offset</code>.
+   * @param initTerms list of initial values as a String
+   */
+  public LambdaSequence(final int offset, final Function<Integer, Z> lambda, final String initTerms) {
+    super(offset);
+    mN = offset - 1;
+    mLambda = lambda;
+    mIn = -1;
+    mInits = (initTerms.isEmpty() || "[]".equals(initTerms)) ? new Z[0] : ZUtils.toZ(initTerms);
+    mInitNo = mInits.length;
   }
 
   @Override
   public Z next() {
-    return a(++mN);
+    ++mN;
+    ++mIn;
+    if (mIn < mInitNo) {
+      return mInits[mIn];
+    } else {
+      return mLambda.apply(mN);
+    }
   }
 
   @Override
@@ -55,6 +97,10 @@ public class LambdaSequence extends AbstractSequence implements DirectSequence {
 
   @Override
   public Z a(final int n) {
-    return mLambda.apply(n);
+    if (n < mInitNo) {
+      return mInits[n];
+    } else {
+      return mLambda.apply(n);
+    }
   }
 }

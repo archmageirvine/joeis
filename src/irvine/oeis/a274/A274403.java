97ed9e927381d5760e785adcf9f3c5a1af0b2606
Sean A. Irvine <sairvin@gmail.com>
2024-07-04 18:31:00 +1200

    Sequences

---
+++ src/irvine/oeis/a274/A274403.java
@@ -0,0 +1,25 @@
+package irvine.oeis.a274;
+
+import irvine.math.z.Z;
+import irvine.oeis.a006.A006991;
+
+/**
+ * A071150.
+ * @author Sean A. Irvine
+ */
+public class A274403 extends A006991 {
+
+  private Z mT = Z.ONE;
+  private Z mA = super.next();
+  private long mCount = 0;
+
+  @Override
+  public Z next() {
+    mT = mT.multiply(10);
+    while (mA.compareTo(mT) <= 0) {
+      ++mCount;
+      mA = super.next();
+    }
+    return Z.valueOf(mCount);
+  }
+}

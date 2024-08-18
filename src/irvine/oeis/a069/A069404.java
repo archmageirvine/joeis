29c8ba2850f3b206b74b644c26ea8e2dcf6380f5
Sean A. Irvine <sairvin@gmail.com>
2024-08-19 08:39:37 +1200

    A069404, WIP

--- src/irvine/oeis/a069/A069404.java
+++ src/irvine/oeis/a069/A069404.java
@@ -1,6 +1,9 @@
 package irvine.oeis.a069;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 import irvine.math.function.Functions;
 import irvine.math.z.Z;
@@ -12,6 +15,85 @@ import irvine.oeis.Sequence1;
  * @author Sean A. Irvine
  */
 public class A069404 extends Sequence1 {
+
+  private boolean is(final int side, final int[] v, final int row, final int col, final HashSet<String> tried) {
+    if (row == v.length - 1) {
+      return true;
+    }
+    final String key = row + "," + (col + 1);
+    if (!tried.add(key)) {
+      return false;
+    }
+    assert (v[row] & (1 << col)) == (side << col);
+    if ((v[row + 1] & (1 << col)) == (side << col) && is(side, v, row + 1, col, tried)) {
+      return true;
+    }
+    if (col < 3 && (v[row] & (1 << (col + 1))) == (side << (col + 1))) {
+      if (is(side, v, row, col + 1, tried)) {
+        return true;
+      }
+    }
+    if (col > 0 && (v[row] & (1 << (col - 1))) == (side << (col - 1))) {
+      if (is(side, v, row, col - 1, tried)) {
+        return true;
+      }
+    }
+    tried.remove(key);
+    return false;
+  }
+
+  private boolean is(final int side, final int[] v) {
+    for (int k = 0; k < 4; ++k) {
+      if ((v[0] & (1 << k)) == (side << k) && is(side, v, 0, k, new HashSet<>())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private String bin(final int v) {
+    return String.valueOf((v >> 3) & 1) + ((v >> 2) & 1) + ((v >> 1) & 1) + (v & 1);
+  }
+
+  private void exhaust2() {
+    final HashMap<Integer, Integer> cnts = new HashMap<>();
+    long cnt = 0;
+    final int[] v = new int[2];
+    for (v[0] = 1; v[0] < 15; ++v[0]) {
+      for (v[1] = 1; v[1] < 15; ++v[1]) {
+        if (is(0, v) && is(1, v)) {
+          ++cnt;
+          final int u = Math.min(15 - v[1], v[1]);
+          cnts.merge(u, 1, Integer::sum);
+        }
+      }
+    }
+    System.out.println("Exhaust direct count of 2x4 gives: " + cnt + " -> " + cnt / 2);
+    for (final Map.Entry<Integer, Integer> e : cnts.entrySet()) {
+      System.out.println(bin(e.getKey()) + " " + e.getValue() / 2);
+    }
+  }
+
+  private void exhaust3() {
+    final HashMap<Integer, Integer> cnts = new HashMap<>();
+    long cnt = 0;
+    final int[] v = new int[3];
+    for (v[0] = 1; v[0] < 15; ++v[0]) {
+      for (v[1] = 1; v[1] < 15; ++v[1]) {
+        for (v[2] = 1; v[2] < 15; ++v[2]) {
+          if (is(0, v) && is(1, v)) {
+            ++cnt;
+            final int u = Math.min(15 - v[2], v[2]);
+            cnts.merge(u, 1, Integer::sum);
+          }
+        }
+      }
+    }
+    System.out.println("Direct count of 3x4 gives: " + cnt + " -> " + cnt / 2);
+    for (final Map.Entry<Integer, Integer> e : cnts.entrySet()) {
+      System.out.println(bin(e.getKey()) + " " + e.getValue() / 2);
+    }
+  }
 
   // Note 0101 is a special case, cannot transition into it (effectively union of 01AB and AB01
 
@@ -26,15 +108,15 @@ public class A069404 extends Sequence1 {
       "#.#...####.#####" + // 0
       ".##..##..##....." + // 1
       "###..##..#######" + // 2
-      "...####......#.." + // 3
+      "...####......#.#" + // 3
       "...###.........." + // 4
       ".######......##." + // 5
-      "#######....#####" + // 6
+      "#######.....####" + // 6
       ".......##..#...." + // 7
       "#......##..##.#." + // 8
       "###......###...." + // 9
       ".##......##....." + // 10
-      "#.#...####.##.#." + // 11
+      "#.#....###.##.#." + // 11
       "#...........#..." + // 12
       "......#......#.." + // 13
       "..............#." + // 14
@@ -55,8 +137,8 @@ public class A069404 extends Sequence1 {
     for (int j = 0; j < NODES.length; ++j) {
       System.out.println("  \"" + NODES[j] + "\" [label=\"" + NODES[j] + "\\n" + j + "\"];");
     }
-    for (int j = 0; j < NODES.length; ++j) { // Next animal
-      for (int i = 0; i < NODES.length; ++i) { // Previous animal
+    for (int j = 0; j < NODES.length; ++j) { // Next state
+      for (int i = 0; i < NODES.length; ++i) { // Previous state
         if (i == j) {
           continue; // Don't bother showing the loops
         }
@@ -82,6 +164,8 @@ public class A069404 extends Sequence1 {
 
   @Override
   public Z next() {
+    exhaust2();
+    exhaust3();
     ++mN;
     Z[] v = ZUtils.toZ(START);
     for (int k = 1; k < mN; ++k) {
@@ -89,11 +173,8 @@ public class A069404 extends Sequence1 {
       Arrays.fill(next, Z.ZERO);
       for (int j = 0; j < v.length; ++j) { // Next animal
         for (int i = 0; i < v.length; ++i) { // Previous animal
-          final int pos = i * v.length + j;
+          final int pos = j * v.length + i; // todo is this really right
           if (TRANSITIONS.charAt(pos) == '#') {
-//            if (j == 1) {
-//              System.out.println("Allowing " + j + " to follow " + i + " with count " + v[i]);
-//            }
             next[j] = next[j].add(v[i]);
           }
         }
@@ -101,6 +182,19 @@ public class A069404 extends Sequence1 {
       v = next;
       System.out.println(k + " " + Arrays.toString(v));
     }
+
+    final HashMap<Integer, Integer> cnts = new HashMap<>();
+    for (int k = 0; k < NODES.length; ++k) {
+      //System.out.println(NODES[k] + " " + v[k]);
+      final int u = Integer.parseInt(NODES[k].replace('A', '0').replace('B', '1'), 2);
+      final int w = Math.min(15 - u, u);
+      cnts.merge(w, v[k].intValueExact(), Integer::sum);
+    }
+    System.out.println("State counts");
+    for (final Map.Entry<Integer, Integer> e : cnts.entrySet()) {
+      System.out.println(bin(e.getKey()) + " " + e.getValue());
+    }
+
     return Functions.SUM.z(v);
   }
 }

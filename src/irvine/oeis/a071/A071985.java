4eb5fc78981886b5c7378fa72c03c491ef561c29
Sean A. Irvine <sairvin@gmail.com>
2024-08-25 15:28:29 +1200

    Speedup

--- src/irvine/oeis/a071/A071985.java
+++ src/irvine/oeis/a071/A071985.java
@@ -1,13 +1,11 @@
 package irvine.oeis.a071;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 
 import irvine.math.z.Z;
 import irvine.oeis.Sequence1;
@@ -18,29 +16,26 @@ import irvine.oeis.Sequence1;
  */
 public class A071985 extends Sequence1 {
 
-  // After Michael S. Branicky
-
   private int mN = 0;
 
   private Z a(final int n, final List<Z> v) {
-    final List<Map<Set<Z>, Set<Z>>> r = new ArrayList<>();
+    final List<Map<Long, Set<Z>>> r = new ArrayList<>();
     for (int k = 0; k < n; ++k) {
       r.add(new HashMap<>());
     }
-    for (final Z t : v) {
+    for (int k = 1; k <= mN; ++k) {
       final HashSet<Z> s = new HashSet<>();
-      s.add(t);
-      r.get(0).put(s, new HashSet<>(s));
+      s.add(Z.valueOf(k));
+      r.get(0).put(1L << (k - 1), new HashSet<>(s));
     }
     final HashSet<Z> reach = new HashSet<>(v);
     for (int j = 1; j < n; ++j) {
       for (int i = 0; i < (j + 1) / 2; ++i) {
-        for (final Set<Z> s1 : r.get(i).keySet()) {
-          for (final Set<Z> s2 : r.get(j - 1 - i).keySet()) {
-            if (Collections.disjoint(s1, s2)) {
-              final TreeSet<Z> s12 = new TreeSet<>(s1);
-              s12.addAll(s2);
-              final Set<Z> allowed = r.get(s12.size() - 1).computeIfAbsent(s12, s -> new HashSet<>());
+        for (final long s1 : r.get(i).keySet()) {
+          for (final long s2 : r.get(j - 1 - i).keySet()) {
+            if ((s1 & s2) == 0) { // Disjoint sets
+              final long s12 = s1 | s2;
+              final Set<Z> allowed = r.get(Long.bitCount(s12) - 1).computeIfAbsent(s12, s -> new HashSet<>());
               for (final Z a : r.get(i).get(s1)) {
                 for (final Z b : r.get(j - 1 - i).get(s2)) {
                   final Z t1 = a.add(b);

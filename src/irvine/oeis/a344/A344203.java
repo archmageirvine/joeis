720ab964c61c7394e45ab4fa7d19cdc459fee9ff
Sean A. Irvine <sairvin@gmail.com>
2024-11-25 13:37:45 +1300

    Contribution from Georg Fischer, 2024-11-24.ack.2024-11-24.22.tgz

---
+++ src/irvine/oeis/a344/A344203.java
@@ -0,0 +1,26 @@
+package irvine.oeis.a344;
+
+import irvine.math.z.DirichletSeries;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A344203 Number of ways to factor n into 2 kinds of 2, 3 kinds of 3, ...
+ * @author Sean A. Irvine
+ */
+public class A344203 extends Sequence1 {
+
+  // %N A344203 Dirichlet g.f.: Product_{k>=2} 1 / (1 - k^(-s))^binomial(k+2,3).
+  private int mN = 0;
+
+  @Override
+  public Z next() {
+    ++mN;
+    DirichletSeries series = DirichletSeries.ONE;
+    for (int k = 2; k <= mN; ++k) {
+      series = series.multiply(DirichletSeries.zeta(k, mN, Z.ONE).pow(k * (k + 1) * (k + 2) / 6, mN), mN);
+    }
+    return series.coeff(mN);
+  }
+}
+

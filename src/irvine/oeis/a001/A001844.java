fe3175276532b3dd4a03a673b7878dc14f99e978
Sean A. Irvine <sairvin@gmail.com>
2024-12-09 14:05:12 +1300

    Contribution from Georg Fischer, 2024-12-08.ack.2024-12-08.22.tgz

--- src/irvine/oeis/a001/A001844.java
+++ src/irvine/oeis/a001/A001844.java
@@ -1,13 +1,14 @@
 package irvine.oeis.a001;
 
 import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.Sequence0;
 
 /**
  * A001844 Centered square numbers: a(n) = 2*n*(n+1)+1. Sums of two consecutive squares. Also, consider all Pythagorean triples (X, Y, Z=Y+1) ordered by increasing Z; then sequence gives Z values.
  * @author Sean A. Irvine
  */
-public class A001844 extends Sequence0 {
+public class A001844 extends Sequence0 implements DirectSequence {
 
   private long mN = -4;
   private Z mA = Z.ONE;
@@ -18,4 +19,15 @@ public class A001844 extends Sequence0 {
     mA = mA.add(mN);
     return mA;
   }
+
+  @Override
+  public Z a(final Z n) {
+    return n.multiply2().multiply(n.add(1)).add(1);
+  }
+
+  @Override
+  public Z a(final int n) {
+    return Z.valueOf(2 * n).multiply(n + 1).add(1);
+  }
+
 }
--- src/irvine/oeis/a025/A025480.java
+++ src/irvine/oeis/a025/A025480.java
@@ -1,13 +1,15 @@
 package irvine.oeis.a025;
 
+import irvine.math.function.Functions;
 import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.a000.A000265;
 
 /**
  * A025480 a(2n) = n, a(2n+1) = a(n).
  * @author Sean A. Irvine
  */
-public class A025480 extends A000265 {
+public class A025480 extends A000265 implements DirectSequence {
 
   /** Construct the sequence. */
   public A025480() {
@@ -18,5 +20,17 @@ public class A025480 extends A000265 {
   public Z next() {
     return super.next().subtract(1).divide2();
   }
+
+  @Override
+  public Z a(final Z n) {
+    // a(n) = (A000265(n+1)-1)/2
+    return Functions.MAKE_ODD.z(n.add(1)).subtract(1).divide2();
+  }
+
+  @Override
+  public Z a(final int n) {
+    return Functions.MAKE_ODD.z(n + 1).subtract(1).divide2();
+  }
+
 }
 

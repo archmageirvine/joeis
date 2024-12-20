e545f06ddbb8b70e6d36fe8443db7752ecc7f129
Sean A. Irvine <sairvin@gmail.com>
2024-12-21 07:35:25 +1300

    Contribution from Georg Fischer, 2024-12-20.ack.2024-12-20.14.tgz

--- src/irvine/oeis/a065/A065091.java
+++ src/irvine/oeis/a065/A065091.java
@@ -1,14 +1,28 @@
 package irvine.oeis.a065;
 
+import irvine.math.function.Functions;
+import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.a000.A000040;
 
 /**
  * A065091 Odd primes.
  * @author Georg Fischer
  */
-public class A065091 extends A000040 {
+public class A065091 extends A000040 implements DirectSequence {
 
   {
     super.next();
   }
+
+  @Override
+  public Z a(final Z n) {
+    return Functions.PRIME.z(n.add(1));
+  }
+
+  @Override
+  public Z a(final int n) {
+    return Functions.PRIME.z(n + 1);
+  }
+
 }

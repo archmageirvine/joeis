ad0fb577aaf2ab89e2409285b394fe8fa3df1bba
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2025-01-16 16:08:18 +1300

    Sequences

---
+++ src/irvine/oeis/a074/A074235.java
@@ -0,0 +1,17 @@
+package irvine.oeis.a074;
+
+import irvine.math.z.Z;
+import irvine.oeis.ComplementSequence;
+import irvine.oeis.a009.A009012;
+
+/**
+ * A074196.
+ * @author Sean A. Irvine
+ */
+public class A074235 extends ComplementSequence {
+
+  /** Construct the sequence. */
+  public A074235() {
+    super(1, new A009012(), Z.ONE);
+  }
+}
---
+++ src/irvine/oeis/a074/A074237.java
@@ -0,0 +1,55 @@
+package irvine.oeis.a074;
+
+import irvine.factor.factor.Jaguar;
+import irvine.factor.util.FactorSequence;
+import irvine.math.z.Z;
+import irvine.math.z.ZUtils;
+import irvine.oeis.Sequence1;
+
+/**
+ * A074211 Composites which use less than all of their digits in their prime factorization.
+ * @author Sean A. Irvine
+ */
+public class A074237 extends Sequence1 {
+
+  private long mN = 94;
+
+  private void add(final int[] s, final int[] a) {
+    for (int k = 0; k < s.length; ++k) {
+      s[k] += a[k];
+    }
+  }
+
+  private boolean is(final long n) {
+    final int[] syn = ZUtils.digitCounts(n);
+    final int[] s = new int[syn.length];
+    final FactorSequence fs = Jaguar.factor(n);
+    for (final Z p : fs.toZArray()) {
+      add(s, ZUtils.digitCounts(p));
+      final int e = fs.getExponent(p);
+      if (e > 1) {
+        add(s, ZUtils.digitCounts(e));
+      }
+    }
+    for (int k = 0; k < s.length; ++k) {
+      if (syn[k] > s[k]) {
+        return false;
+      }
+    }
+    for (int k = 0; k < s.length; ++k) {
+      if (syn[k] < s[k]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public Z next() {
+    while (true) {
+      if (is(++mN)) {
+        return Z.valueOf(mN);
+      }
+    }
+  }
+}

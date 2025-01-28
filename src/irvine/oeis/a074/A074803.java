5309287ed996b830f884bae5cef5a8783c2754c0
Sean A. Irvine <sairvin@gmail.com>
2025-01-28 17:04:23 +1300

    Sequences

---
+++ src/irvine/oeis/a074/A074803.java
@@ -0,0 +1,47 @@
+package irvine.oeis.a074;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+import irvine.util.array.LongDynamicBooleanArray;
+
+/**
+ * A064353.
+ * @author Sean A. Irvine
+ */
+public class A074803 extends Sequence1 {
+
+  private final LongDynamicBooleanArray mA = new LongDynamicBooleanArray();
+  {
+    mA.set(0);
+    mA.set(1);
+    mA.set(2);
+    mA.set(3);
+  }
+  private long mL = 4; // Length of computed region of mA
+  private long mN = -1;
+  private long mM = 0; // Position of last run length expanded in mA
+  private boolean mNext = true;
+
+  @Override
+  public Z next() {
+    if (++mN >= mL) {
+      mNext = !mNext;
+      if (mA.isSet(++mM)) {
+        if (mNext) {
+          mA.set(mL++);
+          mA.set(mL++);
+          mA.set(mL++);
+          mA.set(mL++);
+        } else {
+          mL += 4;
+        }
+      } else if (mNext) {
+        mA.set(mL++);
+        mA.set(mL++);
+      } else {
+        mL += 2;
+      }
+    }
+    return mA.isSet(mN) ? Z.FOUR : Z.TWO;
+  }
+}

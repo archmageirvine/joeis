ecf05cf3ac1cf89e4e86e24978b0902337f52d7a
Sean A. Irvine <sairvin@gmail.com>
2025-02-03 10:56:22 +1300

    Sequences

---
+++ src/irvine/oeis/a074/A074923.java
@@ -0,0 +1,32 @@
+package irvine.oeis.a074;
+
+import irvine.math.q.Q;
+import irvine.math.z.Z;
+import irvine.oeis.a000.A000040;
+
+/**
+ * A073524.
+ * @author Sean A. Irvine
+ */
+public class A074923 extends A000040 {
+
+  @Override
+  public Z next() {
+    final Z p = super.next();
+    long e = 250;
+    while (true) {
+      Q x = new Q(mPrime.nextPrime(p), p);
+      final Z mod = p.pow(e);
+      long cnt = 0;
+      while (cnt <= e - 3 && !x.isInteger()) {
+        x = x.multiply(x.ceiling());
+        x = new Q(x.num().mod(mod), x.den().mod(mod));
+        ++cnt;
+      }
+      if (cnt < e - 3) {
+        return Z.valueOf(cnt);
+      }
+      e *= 2;
+    }
+  }
+}

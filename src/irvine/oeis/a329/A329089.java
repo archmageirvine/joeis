c5f6d073450744c9755eca9547a1fe950e6083c9
Sean A. Irvine <sairvin@gmail.com>
2025-02-09 07:28:29 +1300

    Contribution from Georg Fischer, 2025-02-08.ack.2025-02-08.10.tgz

---
+++ src/irvine/oeis/a329/A329089.java
@@ -0,0 +1,22 @@
+package irvine.oeis.a329;
+// manually 2025-02-03/decexpr at 2025-02-08 09:10
+
+import irvine.math.cr.CR;
+import irvine.math.cr.ComputableReals;
+import irvine.oeis.cons.DecimalExpansionSequence;
+
+/**
+ * A329089 Decimal expansion of Sum_{k&gt;=1} 1/(k^2-2), negated.
+ * Formula:
+ * @author Georg Fischer
+ */
+public class A329089 extends DecimalExpansionSequence {
+
+  private static final ComputableReals REALS = ComputableReals.SINGLETON;
+
+  /** Construct the sequence */
+  public A329089() {
+    super(-1, CR.NEG_ONE.add(CR.SQRT2.multiply(CR.PI).multiply(REALS.cot(CR.SQRT2.multiply(CR.PI)))).divide(CR.valueOf(-4)));
+    next();
+  }
+}

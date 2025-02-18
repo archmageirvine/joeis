b1c990e711434c1c192febc2885b29d5181c7b01
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2025-02-19 11:54:01 +1300

    Contribution from Georg Fischer, 2025-02-18.ack.2025-02-18.23.tgz

---
+++ src/irvine/oeis/a117/A117940.java
@@ -0,0 +1,35 @@
+package irvine.oeis.a117;
+// manually 2025-02-18
+
+import irvine.math.z.Z;
+import irvine.oeis.transform.GeneralizedEulerTransform;
+
+/**
+ * A117940 G.f.: Product_{k&gt;=0} (1 + 3*x^(3^k)).
+ * G.f.: <code>Product_{k&gt;=0} ((1+3*x^(3^k)))</code>
+ * @author Georg Fischer
+ */
+public class A117940 extends GeneralizedEulerTransform {
+
+  /** Construct the sequence. */
+  public A117940() {
+    super(0, 0);
+    mNextH = advanceH(mKh);
+  }
+
+  @Override
+  protected Z[] advanceF(final long k) {
+    return new Z[]{Z.NEG_ONE};
+  }
+
+  @Override
+  protected Z advanceG(final long k) {
+    return Z.THREE.negate();
+  }
+
+  @Override
+  protected Z advanceH(final long k) {
+    return Z.THREE.pow(k);
+  }
+
+}

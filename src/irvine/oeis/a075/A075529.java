8275ce70e2979a6266dabe205f74cd45154f3170
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2025-02-21 13:12:04 +1300

    A075529

---
+++ src/irvine/oeis/a075/A075529.java
@@ -0,0 +1,42 @@
+package irvine.oeis.a075;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence0;
+
+/**
+ * A075522.
+ * @author Sean A. Irvine
+ */
+public class A075529 extends Sequence0 {
+
+  private final Set<Integer> mSeen = new HashSet<>();
+  private int mN = -1;
+  private final int[] mWorkspace = new int[Integer.SIZE]; // Way more than we can hope to do with this approach
+
+  private void search(final int pos) {
+    if (pos - 1 == mN) {
+      mSeen.add(mWorkspace[pos - 1]);
+      return;
+    }
+    final int v = mWorkspace[pos];
+    for (int k = 0; k <= pos; ++k) {
+      mWorkspace[pos + 1] = mWorkspace[k] + v;
+      search(pos + 1);
+    }
+  }
+
+  @Override
+  public Z next() {
+    if (++mN <= 1) {
+      return Z.ONE;
+    }
+    mSeen.clear();
+    mWorkspace[0] = 1;
+    search(0);
+    return Z.valueOf(mSeen.size());
+  }
+}
+

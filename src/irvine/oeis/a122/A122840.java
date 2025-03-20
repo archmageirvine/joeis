cad8aaa3843f4ca3b0477926b377a9dee77af96c
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2025-03-20 14:52:32 +1300

    Contribution from Georg Fischer, 2025-03-19.ack.2025-03-19.23.tgz

--- src/irvine/oeis/a122/A122840.java
+++ src/irvine/oeis/a122/A122840.java
@@ -1,25 +1,37 @@
 package irvine.oeis.a122;
 
 import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
 import irvine.oeis.Sequence1;
 
 /**
  * A122840 a(n) is the number of 0's at the end of n when n is written in base 10.
  * @author Georg Fischer
  */
-public class A122840 extends Sequence1 {
+public class A122840 extends Sequence1 implements DirectSequence {
 
   private Z mN = Z.ZERO;
 
   @Override
   public Z next() {
     mN = mN.add(1);
+    return a(mN);
+  }
+
+  @Override
+  public Z a(final Z n) {
     int k = 0;
     Z pow10 = Z.TEN;
-    while (mN.mod(pow10).isZero()) {
+    while (n.mod(pow10).isZero()) {
       ++k;
       pow10 = pow10.multiply(10);
     }
     return Z.valueOf(k);
   }
+
+  @Override
+  public Z a(final int n) {
+    return a(Z.valueOf(n));
+  }
+
 }

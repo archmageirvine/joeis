cf36d45fadd96507f25c74c3e2ddcc90f2062139
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2025-03-27 15:43:06 +1300

    Generalize to multiple generators

--- src/irvine/oeis/a076/A076261.java
+++ src/irvine/oeis/a076/A076261.java
@@ -18,6 +18,16 @@ public class A076261 extends Sequence2 {
   private int mM = 0;
   private Set<Set<String>> mSets = null;
   private final Set<String> mSeen = new HashSet<>();
+  private final String[] mGenerators;
+
+  protected A076261(final String... generators) {
+    mGenerators = generators;
+  }
+
+  /** Construct the sequence. */
+  public A076261() {
+    this("a", "b");
+  }
 
   @Override
   public Z next() {
@@ -26,14 +36,14 @@ public class A076261 extends Sequence2 {
       mM = 1;
       mSeen.clear();
       mSets = new HashSet<>();
-      mSets.add(Set.of("a", "b")); // Start with a single set containing elements needing no multiplication
+      mSets.add(Set.of(mGenerators)); // Start with a single set containing elements needing no multiplication
     }
     // For each existing set, consider each possible multiplication of 2 elements within that set.
     // Retain if the multiplied word is not already in the set and not longer than mN
     // Keep track of the first time we see words of length mN
     int cnt = 0;
     // If we've already seen every word of length n, there is no point in searching further
-    if (mSeen.size() < (1 << mN)) {
+    if (mSeen.size() < Z.valueOf(mGenerators.length).pow(mN).intValueExact()) {
       final Set<Set<String>> next = new HashSet<>();
       for (final Set<String> set : mSets) {
         for (final String s : set) {
---
+++ src/irvine/oeis/a076/A076262.java
@@ -0,0 +1,13 @@
+package irvine.oeis.a076;
+
+/**
+ * A076261 Triangle T(n,k) (n &gt;= 2, 1 &lt;= k &lt;= n-1) read by rows, where T(n,k) is the number of words of length n in the free group on two generators that require exactly k multiplications for their formation.
+ * @author Sean A. Irvine
+ */
+public class A076262 extends A076261 {
+
+  /** Construct the sequence. */
+  public A076262() {
+    super("a", "b", "c");
+  }
+}

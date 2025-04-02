948ff8a2b6e4838d053d48aa1ebfc6e8fd8e973c
Sean A. Irvine <sairvin@gmail.com>
2025-04-03 09:16:36 +1300

    Fixes for discrepancies discovered by Jon Maiga

--- src/irvine/oeis/a074/A074112.java
+++ src/irvine/oeis/a074/A074112.java
@@ -1,52 +1,64 @@
 package irvine.oeis.a074;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
 
 import irvine.factor.prime.Fast;
 import irvine.math.z.Z;
-import irvine.oeis.Conjectural;
 import irvine.oeis.Sequence1;
 
 /**
  * A074112 Let omega(m) be the number of distinct prime divisors of m. Then a(n) is the largest n-digit squarefree number such that omega(n) &gt; omega(j) for all j &lt; n.
  * @author Sean A. Irvine
  */
-public class A074112 extends Sequence1 implements Conjectural {
+public class A074112 extends Sequence1 {
 
-  // There must be a better way to do this
+  // After Charlie Neder
 
-  private static final int HEURISTIC_PRIMES = 50;
-  private static final int HEURISTIC_TREE = 5000;
   private final Fast mPrime = new Fast();
-  private Z mMax = Z.ONE;
-  private TreeMap<Z, Long> mCandidates = new TreeMap<>();
+  private int mN = 0;
+  private final List<Z> mPrimes = new ArrayList<>();
+  private Z mP = Z.FIVE;
   {
-    mCandidates.put(Z.SIX, 3L);
+    mPrimes.add(Z.TWO);
+    mPrimes.add(Z.THREE);
   }
 
   @Override
   public Z next() {
-    mMax = mMax.multiply(10);
-    final Map.Entry<Z, Long> first = mCandidates.firstEntry();
-    final Z t = first.getKey().multiply(mPrime.nextPrime(first.getValue()));
-    if (t.compareTo(mMax) < 0) {
-      // We can now increase the number of prime factors
-      final TreeMap<Z, Long> next = new TreeMap<>();
-      int k = 0;
-      for (final Map.Entry<Z, Long> e : mCandidates.entrySet()) {
-        final Z n = e.getKey();
-        long p = e.getValue();
-        for (int j = 0; j < HEURISTIC_PRIMES; ++j) {
-          p = mPrime.nextPrime(p);
-          next.put(n.multiply(p), p);
-        }
-        if (++k == HEURISTIC_TREE) {
-          break;
+    ++mN;
+    final Z b = Z.TEN.pow(mN);
+    int leng = 0;
+    Z primorial = Z.ONE;
+    long q = 2;
+    while (primorial.multiply(q).compareTo(b) <= 0) {
+      primorial = primorial.multiply(q);
+      ++leng;
+      q = mPrime.nextPrime(q);
+    }
+    final int upper = b.multiply(mPrimes.get(leng - 1)).divide(primorial).intValueExact();
+    while (upper >= mPrimes.size()) {
+      mPrimes.add(mP);
+      mP = mPrime.nextPrime(mP);
+    }
+    TreeSet<Z> nums = new TreeSet<>();
+    nums.add(Z.ONE);
+    for (int k = 0; k < leng; ++k) {
+      final TreeSet<Z> next = new TreeSet<>();
+      for (final Z p : mPrimes) {
+        for (final Z m : nums) {
+          if (m.multiply(p.pow(leng - k)).compareTo(b) >= 0) {
+            break;
+          }
+          if (m.mod(p).isZero()) {
+            continue;
+          }
+          next.add(m.multiply(p));
         }
       }
-      mCandidates = next;
+      nums = next;
     }
-    return mCandidates.lowerKey(mMax);
+    return nums.last();
   }
 }

b9b9da814e0d71ab4d95e17dcff750e3d451f65f
Sean A. Irvine <sairvin@gmail.com>
2025-04-14 08:42:57 +1200

    Make A338211 parallel

--- src/irvine/oeis/a338/A338211.java
+++ src/irvine/oeis/a338/A338211.java
@@ -1,8 +1,11 @@
 package irvine.oeis.a338;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import irvine.math.lattice.Canons;
 import irvine.math.lattice.Hunter;
 import irvine.math.lattice.Lattices;
+import irvine.math.lattice.ParallelHunter;
 import irvine.math.z.Z;
 import irvine.oeis.Sequence0;
 
@@ -14,7 +17,7 @@ public class A338211 extends Sequence0 {
 
   private int mN = -1;
   private int mM = 0;
-  private long[] mPerimeterCounts = new long[0];
+  private AtomicLong[] mPerimeterCounts = new AtomicLong[0];
 
   @Override
   public Z next() {
@@ -23,18 +26,23 @@ public class A338211 extends Sequence0 {
         return Z.ONE; // exactly 1 polyomino with no perimeter
       }
       mM = 0;
-      mPerimeterCounts = new long[2 * mN + 3];
-      final Hunter h = new Hunter(Lattices.Z2, true) {
-        {
-          setKeeper((animal, forbidden) -> {
-            if (Canons.Z2_FREE.isFreeCanonical(animal)) {
-              ++mPerimeterCounts[animal.perimeterSize(Lattices.Z2)];
-            }
-          });
-        }
-      };
+      mPerimeterCounts = new AtomicLong[2 * mN + 3];
+      for (int k = 0; k < mPerimeterCounts.length; ++k) {
+        mPerimeterCounts[k] = new AtomicLong();
+      }
+      final ParallelHunter h = new ParallelHunter(6,
+        () -> new Hunter(Lattices.Z2, true),
+        () -> new Hunter(Lattices.Z2, true) {
+          {
+            setKeeper((animal, forbidden) -> {
+              if (Canons.Z2_FREE.isFreeCanonical(animal)) {
+                mPerimeterCounts[animal.perimeterSize(Lattices.Z2)].incrementAndGet();
+              }
+            });
+          }
+        });
       h.count(mN);
     }
-    return Z.valueOf(mPerimeterCounts[mM]);
+    return Z.valueOf(mPerimeterCounts[mM].get());
   }
 }

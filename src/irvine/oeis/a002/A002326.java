b55a87032f48c06a5d2562b9fc08daaea80882cc
Sean A. Irvine <sairvin@gmail.com>
2025-05-03 08:49:46 +1200

    Contribution from Georg Fischer, 2025-05-02.ack.2025-05-02.12.tgz

--- src/irvine/oeis/a002/A002326.java
+++ src/irvine/oeis/a002/A002326.java
@@ -3,12 +3,13 @@ package irvine.oeis.a002;
 import irvine.math.group.IntegersMod;
 import irvine.math.z.Z;
 import irvine.oeis.AbstractSequence;
+import irvine.oeis.DirectSequence;
 
 /**
  * A002326 Multiplicative order of 2 mod 2n+1.
  * @author Sean A. Irvine
  */
-public class A002326 extends AbstractSequence {
+public class A002326 extends AbstractSequence implements DirectSequence {
 
   /**
    * Constructor with offset.
@@ -30,4 +31,17 @@ public class A002326 extends AbstractSequence {
     mN = mN.add(2);
     return new IntegersMod(mN).ord(Z.TWO.mod(mN));
   }
+
+  @Override
+  public Z a(final Z n) {
+    final Z n21 = n.multiply(2).add(1);
+    return new IntegersMod(n21).ord(Z.TWO.mod(n21));
+  }
+
+  @Override
+  public Z a(final int n) {
+    final Z n21 = Z.valueOf(2 * n + 1);
+    return new IntegersMod(n21).ord(Z.TWO.mod(n21));
+  }
+
 }

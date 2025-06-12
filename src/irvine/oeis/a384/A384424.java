b71356769a84ab6f872e1da25fb4b22ff8ca5f79
Sean A. Irvine <sairvin@gmail.com>
2025-06-13 07:27:44 +1200

    Verbose

--- src/irvine/oeis/a384/A384424.java
+++ src/irvine/oeis/a384/A384424.java
@@ -1,9 +1,12 @@
 package irvine.oeis.a384;
 
+import java.util.Arrays;
+
 import irvine.math.graph.Graph;
 import irvine.math.graph.GraphFactory;
 import irvine.math.z.Z;
 import irvine.oeis.Sequence1;
+import irvine.util.string.StringUtils;
 
 /**
  * A384424 allocated for Yifan Xie.
@@ -11,33 +14,46 @@ import irvine.oeis.Sequence1;
  */
 public class A384424 extends Sequence1 {
 
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
   private Graph mGraph = null;
   private int mN = 0;
   private int mBad = 0;
+  private int[] mDist = null;
+  private int[] mPath = null;
   private boolean[] mUsed = null;
 
-  private long dist(final int px, final int py) {
+  private int dist(final int px, final int py) {
     // To avoid 1/2, scale up coordinates by 2
-    final long dx = 2L * px - (mN - 1);
-    final long dy = 2L * py - (mN - 1);
+    final int dx = 2 * px - (mN - 1);
+    final int dy = 2 * py - (mN - 1);
     return dx * dx + dy * dy;
   }
 
   private void search(final int point, final int remaining, final int badCount) {
-    if (remaining == 0) {
-      mBad = badCount;
-      System.out.println(mN + " New minimum bad steps: " + mBad + " final point " + point);
+    final int dp = mDist[point];
+    // Handle closing the loop
+    if (remaining <= 1) {
+      if (mGraph.isAdjacent(point, 0)) {
+        // The point (0,0) is always a maximum distance from the middle, so moving to
+        // this point at the end of the cycle is always "bad"
+        if (badCount + 1 < mBad) {
+          mBad = badCount + 1;
+          if (mVerbose) {
+            StringUtils.message(mN + " New minimum bad steps: " + mBad + " " + Arrays.toString(mPath));
+          }
+        }
+      }
       return;
     }
     // Try expanding path from current point
-    final long dp = dist(point % mN, point / mN);
     for (int q = mGraph.nextVertex(point, -1); q >= 0; q = mGraph.nextVertex(point, q)) {
-      if (!mUsed[q] || (remaining == 1 && q == 0)) {
-        final int qx = q % mN;
-        final int qy = q / mN;
-        final long dq = dist(qx, qy);
+      if (!mUsed[q]) {
+        final int dq = mDist[q];
         final int newBadCount = dq >= dp ? badCount + 1 : badCount;
         if (newBadCount < mBad) {
+          if (mVerbose) {
+            mPath[mPath.length - remaining] = q;
+          }
           mUsed[q] = true;
           search(q, remaining - 1, newBadCount);
           mUsed[q] = false;
@@ -51,8 +67,17 @@ public class A384424 extends Sequence1 {
     ++mN;
     mGraph = GraphFactory.kings(mN);
     mBad = mN * mN;
+    // Precompute (squared) distance to center for each point
+    // Note these values are scaled up to avoid 1/2,
+    // but we care only about relative distance.
+    mDist = new int[mN * mN];
+    for (int k = 0; k < mDist.length; ++k) {
+      mDist[k] = dist(k % mN, k / mN);
+    }
+    mPath = new int[mN * mN];
     mUsed = new boolean[mN * mN];
     mUsed[0] = true;
+    // todo this could exploit symmetry
     search(0, mN * mN, 0);
     return Z.valueOf(mN * mN - mBad);
   }

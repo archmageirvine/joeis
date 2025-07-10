94add4bcadda73fd27e641c7e2ea0e33e3d5e02f
Sean A. Irvine <sairvin@gmail.com>
2025-07-10 15:04:01 +1200

    A385081

---
+++ src/irvine/oeis/a385/A385081.java
@@ -0,0 +1,35 @@
+package irvine.oeis.a385;
+
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+import irvine.math.function.Functions;
+import irvine.math.group.SymmetricGroup;
+import irvine.math.polynomial.CycleIndex;
+import irvine.math.polynomial.MultivariateMonomial;
+import irvine.math.q.Q;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A385081 allocated for Gregory Gerard Wojnar.
+ * @author Sean A. Irvine
+ */
+public class A385081 extends Sequence1 {
+
+  private int mN = 1;
+  private final LinkedList<Z> mA = new LinkedList<>();
+
+  @Override
+  public Z next() {
+    if (mA.isEmpty()) {
+      final CycleIndex z = SymmetricGroup.create(++mN).cycleIndex().drop(1);
+      z.multiply(Q.valueOf(Functions.FACTORIAL.z(mN)));
+      //System.out.println(z);
+      for (final MultivariateMonomial mm : new TreeSet<>(z.values())) {
+        mA.add(mm.getCoefficient().toZ());
+      }
+    }
+    return mA.pollLast();
+  }
+}

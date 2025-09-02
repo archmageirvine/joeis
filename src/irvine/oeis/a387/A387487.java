cb5afbfe30334d2f8e9f9dd2995a699d33a3b46f
Sean A. Irvine <sairvin@gmail.com>
2025-09-03 08:04:24 +1200

    Contribution from Georg Fischer, 2025-09-02.ack.2025-09-02.11.tgz

---
+++ src/irvine/oeis/a387/A387487.java
@@ -0,0 +1,16 @@
+package irvine.oeis.a387;
+
+import irvine.oeis.FiniteSequence;
+
+/**
+ * A387487 Numbers in the ring of integers Z such that k * k = k, the idempotents of Z.
+ * @author Georg Fischer
+ */
+
+public class A387487 extends FiniteSequence {
+
+  /** Construct the sequence. */
+  public A387487() {
+    super(1, FINITE, "0,1");
+  }
+}

7420e2389093bff84bc3114aa9047b71cc8a00e2
Sean A. Irvine <sairvin@gmail.com>
2025-12-13 14:17:35 +1300

    Fix speed in Risk calculation

--- src/irvine/oeis/a371/A371949.java
+++ src/irvine/oeis/a371/A371949.java
@@ -1,9 +1,8 @@
 package irvine.oeis.a371;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
 
 import irvine.math.MemoryFunctionInt2;
@@ -11,7 +10,6 @@ import irvine.math.q.Q;
 import irvine.math.z.Z;
 import irvine.oeis.Sequence1;
 import irvine.util.Pair;
-import irvine.util.string.StringUtils;
 
 /**
  * A390514.
@@ -23,23 +21,22 @@ public class A371949 extends Sequence1 {
 
   private static final int DICE = 5;
   private static final Z DEN = Z.SIX.pow(DICE);
-  private static final int[] EMPTY = {};
   private int mN = 0;
   private int mM = -1;
 
   private static int[] getElims(final int[] a, final int[] d) {
     final int[] a2 = maxTwo(a);
     final int[] d2 = maxTwo(d);
-    final int len = Math.min(a2.length, d2.length);
-    final StringBuilder sb = new StringBuilder();
-    for (int k = 0, j = a2.length - 1, i = d2.length - 1; k < len; ++k, --j, --i) {
-      final boolean oGreater = a2[j] > d2[i];
-      sb.append(oGreater ? 'd' : 'a');
+    int dc = 0;
+    int ac = 0;
+    for (int j = a2.length - 1, i = d2.length - 1; j >= 0 && i >= 0; --j, --i) {
+      if (a2[j] > d2[i]) {
+        --dc;
+      } else {
+        --ac;
+      }
     }
-    // count 'a' and 'd' and negate
-    final int aCount = -StringUtils.count(sb, 'a');
-    final int dCount = -StringUtils.count(sb, 'd');
-    return new int[] {aCount, dCount};
+    return new int[] {ac, dc};
   }
 
   private static int[] maxTwo(final int[] a) {
@@ -107,32 +104,30 @@ public class A371949 extends Sequence1 {
     }
   };
 
-  private Map<Pair<Integer, Integer>, Q> getFinalResult(final int attack, int defend) {
-    final Map<Pair<Integer, Integer>, Q> d = new HashMap<>();
-    final List<Pair<Integer, Integer>> g = new ArrayList<>();
+  private Map<Pair<Integer, Integer>, Q> getFinalResult(final int attack, final int defend) {
+    final HashMap<Pair<Integer, Integer>, Q> d = new HashMap<>();
+    final LinkedList<Pair<Integer, Integer>> g = new LinkedList<>();
     final Pair<Integer, Integer> start = new Pair<>(attack, defend);
     d.put(start, Q.ONE);
     g.add(start);
     while (!g.isEmpty()) {
-      final Pair<Integer, Integer> w = g.remove(0);
+      final Pair<Integer, Integer> w = g.poll();
       final Q p = d.remove(w);
-      if (w.left() == 0 || w.right() == 0) {
+      if (w.left() == 0 || w.right() == 0 || p == null) {
         continue;
       }
       for (final Map.Entry<Pair<Integer, Integer>, Q> e : mResP.get(w.left(), w.right()).entrySet()) {
-        d.merge(e.getKey(), e.getValue().multiply(p), Q::add);
-      }
-      g.clear();
-      for (final Pair<Integer, Integer> k : d.keySet()) {
-        if (k.left() > 0 && k.right() > 0) {
-          g.add(k);
+        final Pair<Integer, Integer> key = e.getKey();
+        d.merge(key, e.getValue().multiply(p), Q::add);
+        if (key.left() > 0 && key.right() > 0) {
+          g.add(key);
         }
       }
     }
     return d;
   }
 
-  protected Z t(final int attack, final int defend) {
+  protected Q t(final int attack, final int defend) {
     final Map<Pair<Integer, Integer>, Q> r = getFinalResult(attack, defend);
     Q total = Q.ZERO;
     for (final Map.Entry<Pair<Integer, Integer>, Q> e : r.entrySet()) {
@@ -140,8 +135,11 @@ public class A371949 extends Sequence1 {
         total = total.add(e.getValue());
       }
     }
-    final Z mul = DEN.pow(attack + defend - 1);
-    return total.num().multiply(mul).divide(total.den());
+    return total;
+  }
+
+  private Z u(final int attack, final int defend) {
+    return t(attack, defend).multiply(DEN.pow(attack + defend - 1)).toZ();
   }
 
   @Override
@@ -150,7 +148,7 @@ public class A371949 extends Sequence1 {
       ++mN;
       mM = 0;
     }
-    return t(mM + 1, mN - mM + 1);
+    return u(mM + 1, mN - mM + 1);
   }
 }
 
--- src/irvine/oeis/a390/A390860.java
+++ src/irvine/oeis/a390/A390860.java
@@ -1,36 +1,24 @@
 package irvine.oeis.a390;
 
-import java.util.Random;
-
-import irvine.math.r.DoubleUtils;
+import irvine.math.q.Q;
 import irvine.math.z.Z;
-import irvine.oeis.Sequence1;
+import irvine.oeis.a371.A371949;
 
 /**
  * A390514.
  * @author Sean A. Irvine
  */
-public class A390860 extends Sequence1 {
+public class A390860 extends A371949 {
 
-  // temp experiment
-
-  private long mN = 0;
+  private int mN = 0;
+  private int mAttackers = 1;
 
   @Override
   public Z next() {
-    final Random r = new Random();
-    final long trials = 100000000;
-    long wins = 0;
-    for (long k = 0; k < trials; ++k) {
-      final int a1 = r.nextInt(6);
-      final int a2 = r.nextInt(6);
-      final int d1 = r.nextInt(6);
-      if (a1 > d1 || a2 > d1) {
-        ++wins;
-      }
+    ++mN;
+    while (t(mAttackers, mN).compareTo(Q.HALF) < 0) {
+      ++mAttackers;
     }
-    System.out.println(wins + "/" + trials + " = " + DoubleUtils.NF4.format((double) wins / trials));
-    return null;
+    return Z.valueOf(1 + mAttackers);
   }
 }
-

e913560d842ec3708bf1d633c26cd37f12531f97
Sean A. Irvine <sairvin@gmail.com>
2025-12-14 13:35:15 +1300

    Contribution from Georg Fischer, 2025-12-13.ack.2025-12-13.22.tgz

--- src/irvine/oeis/a001/A001453.java
+++ src/irvine/oeis/a001/A001453.java
@@ -1,22 +1,31 @@
 package irvine.oeis.a001;
 
+import irvine.math.function.Functions;
 import irvine.math.z.Z;
-import irvine.oeis.a000.A000108;
+import irvine.oeis.DirectSequence;
+import irvine.oeis.Sequence2;
 
 /**
  * A001453 Catalan numbers - 1.
  * @author Sean A. Irvine
  */
-public class A001453 extends A000108 {
+public class A001453 extends Sequence2 implements DirectSequence {
 
-  {
-    setOffset(2);
-    next();
-    next();
-  }
+  private int mN = 1;
 
   @Override
   public Z next() {
-    return super.next().subtract(1);
+    return a(++mN);
   }
+
+  @Override
+  public Z a(final Z n) {
+    return Functions.CATALAN.z(n).subtract(1);
+  }
+
+  @Override
+  public Z a(final int n) {
+    return Functions.CATALAN.z(n).subtract(1);
+  }
+
 }

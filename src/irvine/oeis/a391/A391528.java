09a21aadedf42a68381372eb41ca4341688843b9
Sean A. Irvine <sairvin@gmail.com>
2026-01-05 10:43:33 +1300

    A391528

--- src/irvine/oeis/a391/A391528.java
+++ src/irvine/oeis/a391/A391528.java
@@ -12,38 +12,41 @@ public class A391528 extends Sequence1 {
 
   private long mN = 0;
 
-  private void step(final long[] s) {
-    final long sum = Functions.SUM.l(s);
+  private void step(final Z[] s) {
+    final Z sum = Functions.SUM.z(s);
     System.arraycopy(s, 1, s, 0, s.length - 1);
     s[s.length - 1] = sum;
   }
 
-  private long[] start(final long k) {
-    final long[] s = new long[Functions.DIGIT_LENGTH.i(k)];
-    long t = k;
-    for (int j = s.length - 1; t != 0; --j, t /= 10) {
-      s[j] = t % 10;
+  private Z[] start(final Z k) {
+    final Z[] s = new Z[Functions.DIGIT_LENGTH.i(k)];
+    Z t = k;
+    for (int j = s.length - 1; j >= 0; --j) {
+      final Z[] qr = t.divideAndRemainder(Z.TEN);
+      s[j] = qr[1];
+      t = qr[0];
     }
     return s;
   }
 
   private boolean is(final long k, final long n) {
     // Run k for n steps
-    final long[] s = start(k);
+    final Z[] s = start(Z.valueOf(k));
     for (long j = 1; j < n; ++j) {
       step(s);
     }
     // See if running the result leads back to k
-    final long[] t = start(Functions.SUM.l(s));
+    final Z[] t = start(Functions.SUM.z(s));
     if (t.length < 2) {
       return false;
     }
     while (true) {
-      final long sum = Functions.SUM.l(t);
-      if (sum == k) {
+      final Z sum = Functions.SUM.z(t);
+      final int c = sum.compareTo(k);
+      if (c == 0) {
         return true;
       }
-      if (sum > k) {
+      if (c > 0) {
         return false;
       }
       step(t);

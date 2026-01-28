5d960d327d0556270f087ccd78471dbc20d5bc8f
Sean A. Irvine <sairvin@gmail.com>
2026-01-28 16:05:46 +1300

    WIP

---
+++ src/irvine/oeis/a389/A389272.java
@@ -0,0 +1,42 @@
+package irvine.oeis.a389;
+
+import irvine.factor.prime.Fast;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+import irvine.util.array.LongDynamicLongArray;
+
+/**
+ * A067274.
+ * @author Sean A. Irvine
+ */
+public class A389272 extends Sequence1 {
+
+  private final Fast mPrime = new Fast();
+  private final LongDynamicLongArray mSums = new LongDynamicLongArray();
+  private long mP = 1;
+  private long mM = 10;
+
+  @Override
+  public Z next() {
+    while (true) {
+      mP = mPrime.nextPrime(mP);
+      if (mP > mM) {
+        mM *= 10;
+      }
+      final long len = mSums.length();
+      for (long k = 0; k <= len; ++k) {
+        mSums.add(k, mP);
+      }
+      long m = mM;
+      for (long k = mSums.length() - 1; k >= 0; --k) {
+        final long s = mSums.get(k);
+        while (s > m) {
+          m *= 10;
+        }
+        if (s == m) {
+          return Z.valueOf(mP);
+        }
+      }
+    }
+  }
+}

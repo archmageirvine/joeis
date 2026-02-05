11b0581b78f8d4dca1a564a9ff4042640f0bd86e
Sean A. Irvine <sairvin@gmail.com>
2026-02-06 11:38:21 +1300

    WIP

--- src/irvine/oeis/a050/A050201.java
+++ src/irvine/oeis/a050/A050201.java
@@ -2,28 +2,21 @@ package irvine.oeis.a050;
 
 import irvine.math.z.Z;
 import irvine.oeis.a000.A000796;
+import irvine.util.string.StringUtils;
 
 /**
  * A050201 Starting positions of strings of 2 0's in the decimal expansion of Pi.
- * Derived from A035117.
  * @author Georg Fischer
+ * @author Sean A. Irvine
  */
 public class A050201 extends A000796 {
 
-  private final int mN = 0;
-  private final long mM = -1;
-  private final int mLast = 0;
-  private final long mLastPos = 0;
-
-  protected Z digit() {
-    return Z.ONE;
-  }
-
-  protected int mOccur; 
-  protected int mCount; 
-  protected int mDigit;
-  protected int mPos;
-  protected int mState;
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private final int mOccur;
+  private final int mDigit;
+  private int mCount;
+  private int mPos;
+  private boolean mState;
 
   /** Construct the sequence. */
   public A050201() {
@@ -31,112 +24,45 @@ public class A050201 extends A000796 {
   }
   
   /**
-   * Generic constructor with parameters
-   * @param parm1 base
-   * @param parm2 desired digits
+   * Generic constructor with parameters.
+   * @param occurrences number of occurrences required
+   * @param digit desired digit
    */
-  public A050201(final int parm1, final int parm2) {
-    mOccur = parm1;
-    mDigit = parm2;
+  public A050201(final int occurrences, final int digit) {
+    mOccur = occurrences;
+    mDigit = digit;
     mCount = 0;
     mPos = 0;
-    mState = 1;
+    mState = true;
   }
 
   @Override
   public Z next() {
-    int digit = -1;
-    boolean busy = true;
-    while (busy) {
-      switch (mState) {
-        case 2: // check whether there is a complete block with enough occurrences
-          if (mCount >= mOccur) {
-            mCount = mOccur - 1;
-            busy = false;
-          } else {
-            digit = super.next().intValue();
-            ++mPos;
-            if (digit == mDigit) {
-              ++mCount;
-            } else {
-              mState = 1;
-            }
-          }
-          break;
-        case 1: // get next digit
-        default:
-          digit = super.next().intValue();
+    while (true) {
+      if (mState) {
+        final int digit = super.next().intValue();
+        ++mPos;
+        if (digit == mDigit) {
+          mCount = 1;
+          mState = false;
+        }
+      } else {
+        if (mCount >= mOccur) {
+          mCount = mOccur - 1;
+          return Z.valueOf(mPos - mOccur);
+        } else {
+          final int digit = super.next().intValue();
           ++mPos;
           if (digit == mDigit) {
-            mCount = 1;
-            mState = 2;
-          } // else read next and test again
-          break;
-      } // switch
-    } // while
-    return Z.valueOf(mPos - mOccur);
+            ++mCount;
+          } else {
+            mState = true;
+          }
+        }
+      }
+      if (mVerbose && mPos % 100000 == 0) {
+        StringUtils.message("Search completed to " + mPos);
+      }
+    }
   }
-
-
-/*
-  @Override
-  public Z next() {
-    int digit = -1;
-    int result = -1;
-    boolean busy = true;
-    while (busy) {
-      switch (mState) {
-        default:
-        case 1: // get next digit
-          digit = super.next().intValue();
-          ++mPos;
-          if (digit == mDigit) {
-            mCount = 1;
-            mState = 2;
-            result = mPos;
-          } // else read next and test again
-          break;
-        case 2: // check whether there is a first complete block with enough occurrences
-          if (mCount >= mOccur) {
-            mState = 3; // first block is complete
-          } else {
-            digit = super.next().intValue();
-            ++mPos;
-            if (digit == mDigit) {
-              ++mCount;
-            } else {
-              mState = 1;
-            }
-          }
-          break;
-        case 3: // first block is complete, skip over successive blocks
-          digit = super.next().intValue();
-          ++mPos;
-          if (digit == mDigit) {
-            mCount = 1;
-            mState = 4;
-          } else {
-            mState = 1;
-            busy = false; // break loop
-          }
-          break;
-        case 4: // check whether the next block has enough occurrences
-          if (mCount >= mOccur) {
-            mState = 3; // next block is complete
-          } else { 
-            digit = super.next().intValue();
-            ++mPos;
-            if (digit == mDigit) {
-              ++mCount;
-            } else {
-              mState = 1;
-              busy = false;
-            }
-          }
-          break;
-      } // switch
-    } // while
-    return Z.valueOf(result);
-  }
-*/
 }

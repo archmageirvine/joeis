730736bf6bfb9bc3247e156b446009900f60bd17
Sean A. Irvine <sairvin@gmail.com>
2026-02-14 10:09:13 +1300

    Convert A05720x to new method

--- src/irvine/oeis/a057/A057207.java
+++ src/irvine/oeis/a057/A057207.java
@@ -1,63 +1,16 @@
 package irvine.oeis.a057;
 
 import irvine.math.z.Z;
+import irvine.oeis.a124.A124984;
 
 /**
  * A057207 a(1)=5, a(n) is the smallest prime dividing 4*Q^2 + 1 where Q is the product of all previous terms in the sequence.
  * @author Sean A. Irvine
  */
-public class A057207 extends A057205 {
+public class A057207 extends A124984 {
 
-  @Override
-  protected boolean checkProduct(final String factors) {
-    Z first = null;
-    Z n = getCandidate();
-    for (final String f : factors.split("\\.")) {
-      if (f.startsWith("P")) {
-        if (!n.isProbablePrime()) {
-          return false;
-        }
-        final int len = Integer.parseInt(f.substring(1));
-        return len == n.toString().length();
-      }
-      if (f.startsWith("C")) {
-        if (n.isProbablePrime()) {
-          return false;
-        }
-        final int len = Integer.parseInt(f.substring(1));
-        return len == n.toString().length();
-      }
-      final Z p = new Z(f);
-      if (!p.isProbablePrime()) {
-        return false;
-      }
-      if (!n.mod(p).isZero()) {
-        return false;
-      }
-      // Checks the ordering constraint
-      if (first == null) {
-        if (p.mod(4) != 1) {
-          return false;
-        }
-        first = p;
-      } else {
-        if (p.mod(4) == 1 && p.compareTo(first) < 0) {
-          return false;
-        }
-      }
-      n = n.divide(p);
-    }
-    return Z.ONE.equals(n);
+  /** Construct the sequence. */
+  public A057207() {
+    super(1, Z.FIVE, 1, 0, q -> q.square().multiply(4).add(1));
   }
-
-  @Override
-  protected Z getCandidate() {
-    return mProduct.shiftLeft(2).add(1);
-  }
-
-  @Override
-  protected void updateProduct(final Z res) {
-    mProduct = mProduct.multiply(res.square());
-  }
-
 }

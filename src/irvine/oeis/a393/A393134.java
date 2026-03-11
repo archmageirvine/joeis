80594fac44b98915d6dc97c93bf09bc52b21dd52
Sean A. Irvine <sairvin@gmail.com>
2026-03-11 17:06:57 +1300

    Sequences

---
+++ src/irvine/oeis/a393/A393134.java
@@ -0,0 +1,61 @@
+package irvine.oeis.a393;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import irvine.math.z.Z;
+import irvine.oeis.Sequence1;
+
+/**
+ * A393294 Iteration numbers k such that MD5^{k}(128 bits of 0) is a record high value.
+ * @author Sean A. Irvine
+ */
+public class A393134 extends Sequence1 {
+
+  private static final MessageDigest MD5;
+  static {
+    try {
+      MD5 = MessageDigest.getInstance("MD5");
+    } catch (final NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static byte[] md5(final byte[] s) {
+    MD5.reset();
+    return MD5.digest(s);
+  }
+
+  private byte[] mSmallest = null;
+  private byte[] mHash = null;
+  private long mIterations = 0;
+
+  @Override
+  public Z next() {
+    if (mHash == null) {
+      mHash = new byte[16];
+      Arrays.fill(mHash, (byte) -1);
+      mSmallest = mHash;
+      return Z.ZERO;
+    }
+    while (true) {
+      ++mIterations;
+      mHash = md5(mHash);
+      //System.out.println("hash 0x" + ByteUtils.toHex(mHash));
+      final int c = Arrays.compareUnsigned(mHash, mSmallest);
+      if (c == 0) {
+        // Actually this check is not strong enough to guarantee cycle
+        // detection, since we might enter a smaller cycle that does
+        // not contain the current largest value.  But short of doing
+        // a double step arrangement or remembering intermediate values
+        // this is the simplest test to try.
+        return null;
+      }
+      if (c < 0) {
+        mSmallest = mHash;
+        return Z.valueOf(mIterations);
+      }
+    }
+  }
+}

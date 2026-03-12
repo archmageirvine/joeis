a995f23061a9fbf0aaed92f0d888d42f60d700b4
Sean A. Irvine <sairvin@gmail.com>
2026-03-13 09:30:19 +1300

    Sequence

---
+++ src/irvine/oeis/a393/A393587.java
@@ -0,0 +1,26 @@
+package irvine.oeis.a393;
+
+import irvine.math.function.Functions;
+import irvine.math.z.Z;
+import irvine.oeis.DirectSequence;
+import irvine.oeis.ThreeParameterFormSequence;
+import irvine.oeis.a007.A007097;
+
+/**
+ * A393587.
+ * @author Sean A. Irvine
+ */
+public class A393587 extends ThreeParameterFormSequence {
+
+  private static final DirectSequence A007097 = DirectSequence.create(new A007097());
+
+  private static Z compute(final long k, final long b, final long c) {
+    return k == 0 ? A007097.a(b).multiply(A007097.a(c)) : Functions.PRIME.z(compute(k - 1, b, c));
+  }
+
+  /** Construct the sequence. */
+  public A393587() {
+    super(1, 0, 1, 1, A393587::compute);
+  }
+}
+

f8c7e3c06563a0bc92d955e626d881e06e37c945
Sean A. Irvine <sairvin@gmail.com>
2026-03-24 07:39:51 +1300

    Sequences

---
+++ src/irvine/oeis/a394/A394474.java
@@ -0,0 +1,46 @@
+package irvine.oeis.a394;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import irvine.math.function.Functions;
+import irvine.math.graph.GraphUtils;
+import irvine.math.nauty.GenerateGraphs;
+import irvine.math.z.Z;
+import irvine.oeis.ParallelGenerateGraphsSequence;
+import irvine.oeis.Sequence1;
+
+/**
+ * A394473 allocated for Sean A. Irvine.
+ * @author Sean A. Irvine
+ */
+public class A394474 extends Sequence1 {
+
+  private int mN = 0;
+  private int mM = 0;
+  private AtomicLong[] mCounts = null;
+
+  @Override
+  public Z next() {
+    if (++mM > mN) {
+      ++mN;
+      mM = 1;
+      mCounts = new AtomicLong[mN + 1];
+      for (int k = 1; k <= mN; ++k) {
+        mCounts[k] = new AtomicLong();
+      }
+      new ParallelGenerateGraphsSequence(mN, mN - 1, 0, 0, () -> graph -> {
+        mCounts[GraphUtils.chromaticNumber(graph)].addAndGet(Functions.FACTORIAL.z(graph.order()).divide(GraphUtils.automorphismSize(graph)).longValueExact());
+        return 1;
+      }) {
+        @Override
+        protected void graphGenInit(final GenerateGraphs gg) {
+          gg.setVertices(mN);
+          gg.setMinEdges(0);
+          gg.setMaxEdges(mN * (mN - 1) / 2);
+          gg.setConnectionLevel(1);
+        }
+      }.next();
+    }
+    return Z.valueOf(mCounts[mM].get());
+  }
+}

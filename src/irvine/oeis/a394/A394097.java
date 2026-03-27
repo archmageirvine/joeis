2e1db84c49436ec6c3352c5f1ae96524323d1deb
Sean A. Irvine <sairvin@gmail.com>
2026-03-28 08:49:46 +1300

    Sequences

--- src/irvine/oeis/a394/A394097.java
+++ src/irvine/oeis/a394/A394097.java
@@ -8,36 +8,53 @@ import irvine.oeis.Sequence1;
 import irvine.util.Quadruple;
 
 /**
- * A393796.
+ * A394097.
  * @author Sean A. Irvine
  */
 public class A394097 extends Sequence1 {
 
-  // todo this is somehow wrong
+  // We use a quadruple to hold (x,y), (u,v) the coordinate pairs of both points.
+  // Further, we pretend that both points start at (0,0) and reflect in the vertical
+  // axis using x' = n - x - y and y' = y when it is necessary to check where the
+  // second particle actually is.
+  // We keep track of the number of ways of reaching a configuration (x,y,u,v).
+
+  private static final int[] DELTA_X = {1, 0,  1};
+  private static final int[] DELTA_Y = {0, 1, -1};
 
   private int mN = 0;
-  private boolean isSameEdge(final int x0, final int y0, final int x1, final int y1, final int u0, final int v0, final int u1, final int v1) {
-    // Reflect u
-    final int w0 = mN - u0 - v0;
-    final int w1 = mN - u1 - v1;
-    return (w0 == x0 && v0 == y0 && w1 == x1 && v1 == y1) || (w1 == x0 && v1 == y0 && w0 == x1 && v0 == y1);
-  }
 
   private void update(final HashMap<Quadruple<Integer>, Z> map, final Z value, final int x0, final int y0, final int x1, final int y1, final int u0, final int v0, final int u1, final int v1) {
     if (x1 == mN - u1 - v1 && y1 == v1) {
-      return; // points coincide
+      return; // end points coincide
     }
-    if (isSameEdge(x0, y0, x1, y1, u0, v0, u1, v1)) {
+    // Check if particles traversed the same edge
+    // We only need this one way round because we already know (x0,y0) != (u0,v0)
+    // Reflect u
+    final int w0 = mN - u0 - v0;
+    final int w1 = mN - u1 - v1;
+    if (w1 == x0 && v1 == y0 && w0 == x1 && v0 == y1) {
       return; // used the same edge
     }
     map.merge(new Quadruple<>(x1, y1, u1, v1), value, Z::add);
+  }
+
+  private boolean check(final int x0, final int y0, final int x1, final int y1) {
+    // Check that particle remains inside the triangle.
+    if (x1 < 0 || y1 < 0 || x1 + y1 > mN || 2 * y1 > mN) {
+      return false;
+    }
+    // Check the distance from the origin increases
+    final int d0 = x0 * x0 + y0 * y0 + x0 * y0;
+    final int d1 = x1 * x1 + y1 * y1 + x1 * y1;
+    return d1 > d0; // distance from (0,0) did not increase
   }
 
   @Override
   public Z next() {
     ++mN;
     HashMap<Quadruple<Integer>, Z> counts = new HashMap<>();
-    counts.put(new Quadruple<>(mN, 0, mN, 0), Z.ONE);
+    counts.put(new Quadruple<>(0, 0, 0, 0), Z.ONE);
     Z total = Z.ZERO;
     while (!counts.isEmpty()) {
       final HashMap<Quadruple<Integer>, Z> newCounts = new HashMap<>();
@@ -48,64 +65,41 @@ public class A394097 extends Sequence1 {
         final int y = key.b();
         final int u = key.c();
         final int v = key.d();
-        if (x == 0 && y == 0 && u == 0 && v == 0) {
+        if (x == mN && y == 0 && u == mN && v == 0) {
+          // Both points are at end, contribute to the total sum.
           total = total.add(value);
-          continue;
-        }
-        if (x == 0 && y == 0) {
-          // (x,y) is already complete
-          if (u > 0) {
-            update(newCounts, value, x, y, x, y, u, v, u - 1, v);
+          // No further extension of the paths occurs
+        } else if (x == mN && y == 0) {
+          // (x,y) is already complete, only update second point
+          for (int k = 0; k < DELTA_X.length; ++k) {
+            final int nu = u + DELTA_X[k];
+            final int nv = v + DELTA_Y[k];
+            if (check(u, v, nu, nv)) {
+              update(newCounts, value, x, y, x, y, u, v, nu, nv);
+            }
           }
-          if (v > 0) {
-            update(newCounts, value, x, y, x, y, u, v, u, v - 1);
-          }
-          if (u > 0 && v < u - 1) {
-            update(newCounts, value, x, y, x, y, u, v, u - 1, v + 1);
-          }
-        } else if (u == 0 && v == 0) {
-          // (u,v) is already complete
-          if (x > 0) {
-            update(newCounts, value, x, y, x - 1, y, u, v, u, v);
-          }
-          if (y > 0) {
-            update(newCounts, value, x, y, x, y - 1, u, v, u, v);
-          }
-          if (x > 0 && y < x - 1) {
-            update(newCounts, value, x, y, x - 1, y + 1, u, v, u, v);
+        } else if (u == mN && v == 0) {
+          // (u,v) is already complete, only update first point
+          for (int k = 0; k < DELTA_X.length; ++k) {
+            final int nx = x + DELTA_X[k];
+            final int ny = y + DELTA_Y[k];
+            if (check(x, y, nx, ny)) {
+              update(newCounts, value, x, y, nx, ny, u, v, u, v);
+            }
           }
         } else {
-          if (x > 0) {
-            if (u > 0) {
-              update(newCounts, value, x, y, x - 1, y, u, v, u - 1, v);
-            }
-            if (v > 0) {
-              update(newCounts, value, x, y, x - 1, y, u, v, u, v - 1);
-            }
-            if (u > 0 && v < u - 1) {
-              update(newCounts, value, x, y, x - 1, y, u, v, u - 1, v + 1);
-            }
-          }
-          if (y > 0) {
-            if (u > 0) {
-              update(newCounts, value, x, y, x, y - 1, u, v, u - 1, v);
-            }
-            if (v > 0) {
-              update(newCounts, value, x, y, x, y - 1, u, v, u, v - 1);
-            }
-            if (u > 0 && v < u - 1) {
-              update(newCounts, value, x, y, x, y - 1, u, v, u - 1, v + 1);
-            }
-          }
-          if (x > 0 && y < x - 1) {
-            if (u > 0) {
-              update(newCounts, value, x, y, x - 1, y + 1, u, v, u - 1, v);
-            }
-            if (v > 0) {
-              update(newCounts, value, x, y, x - 1, y + 1, u, v, u, v - 1);
-            }
-            if (u > 0 && v < u - 1) {
-              update(newCounts, value, x, y, x - 1, y + 1, u, v, u - 1, v + 1);
+          // General situation, both points move
+          for (int k = 0; k < DELTA_X.length; ++k) {
+            final int nx = x + DELTA_X[k];
+            final int ny = y + DELTA_Y[k];
+            if (check(x, y, nx, ny)) {
+              for (int j = 0; j < DELTA_X.length; ++j) {
+                final int nu = u + DELTA_X[j];
+                final int nv = v + DELTA_Y[j];
+                if (check(u, v, nu, nv)) {
+                  update(newCounts, value, x, y, nx, ny, u, v, nu, nv);
+                }
+              }
             }
           }
         }
@@ -115,4 +109,3 @@ public class A394097 extends Sequence1 {
     return total;
   }
 }
-

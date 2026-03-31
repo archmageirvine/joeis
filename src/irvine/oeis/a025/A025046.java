1080da16e809316cda436ac340518399bc93f553
Sean A. Irvine <sairvin@gmail.com>
2026-03-31 15:29:17 +1300

    Fix bug identified by Jinyuan

--- src/irvine/oeis/a025/A025046.java
+++ src/irvine/oeis/a025/A025046.java
@@ -3,6 +3,8 @@ package irvine.oeis.a025;
 import irvine.factor.prime.Fast;
 import irvine.math.z.Z;
 import irvine.oeis.Sequence2;
+import irvine.util.array.DynamicIntArray;
+import irvine.util.string.StringUtils;
 
 /**
  * A025046 a(n) is the least odd prime p such that the maximum run length of consecutive quadratic residues modulo p is n.
@@ -10,38 +12,54 @@ import irvine.oeis.Sequence2;
  */
 public class A025046 extends Sequence2 {
 
+  private final boolean mVerbose = "true".equals(System.getProperty("oeis.verbose"));
+  private final DynamicIntArray mFirsts = new DynamicIntArray();
+  private final DynamicIntArray mFirstValues = new DynamicIntArray();
   private final Fast mPrime = new Fast();
   private int mN = 1;
 
-  private long least(final int p, final int n) {
-    final boolean[] seen = new boolean[p];
+  private boolean[] markQuadraticResidues(final long p) {
+    final boolean[] seen = new boolean[Math.toIntExact(p)];
     for (long k = 0; k < p; ++k) {
       seen[(int) ((k * k) % p)] = true;
     }
+    return seen;
+  }
+
+  private void process(final long p) {
+    final boolean[] seen = markQuadraticResidues(p);
+    int longest = 0;
+    int longestFirstValue = 0;
+    int runLength = 0;
+
     for (int k = 0; k < p; ++k) {
       if (!seen[k]) {
-        boolean ok = true;
-        int i = k;
-        for (int j = 0; j < n; ++j) {
-          if (++i >= p) {
-            i = 0;
-          }
-          if (!seen[i]) {
-            ok = false;
-            break;
-          }
+        if (runLength > longest) {
+          longest = runLength;
+          longestFirstValue = k - longest;
         }
-        if (ok) {
-          if (++i >= p) {
-            i = 0;
-          }
-          if (!seen[i]) {
-            return (k + 1) % p;
-          }
-        }
+        runLength = 0;
+      } else {
+        ++runLength;
       }
     }
-    return -1;
+    // Handle wrap around
+    long last = p;
+    for (int k = 0; k < p && seen[k]; ++k) {
+      ++runLength;
+      ++last;
+    }
+    if (runLength > longest) {
+      longest = runLength;
+      longestFirstValue = (int) (last - longest);
+    }
+    if (mFirsts.get(longest) == 0) {
+      mFirsts.set(longest, (int) p);
+      mFirstValues.set(longest, longestFirstValue);
+      if (mVerbose) {
+        StringUtils.message("Solution for n=" + longest + " is p=" + p + " [least residue=" + longestFirstValue + "]");
+      }
+    }
   }
 
   protected Z select(final long p, final long least) {
@@ -52,13 +70,11 @@ public class A025046 extends Sequence2 {
   public Z next() {
     ++mN;
     long p = 2;
-    while (true) {
+    while (mFirsts.get(mN) == 0) {
       p = mPrime.nextPrime(p);
-      final long least = least((int) p, mN);
-      if (least >= 0) {
-        return select(p, least);
-      }
+      process(p);
     }
+    return select(mFirsts.get(mN), mFirstValues.get(mN));
   }
 }
 

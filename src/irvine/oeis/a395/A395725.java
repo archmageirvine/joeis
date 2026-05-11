afd29897447854e81b79938217368970968c435e
Sean A. Irvine <sairvin@gmail.com>
2026-05-11 15:31:58 +1200

    A395725

--- src/irvine/oeis/a395/A395725.java
+++ src/irvine/oeis/a395/A395725.java
@@ -1,15 +1,17 @@
 package irvine.oeis.a395;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import irvine.math.z.Z;
 import irvine.oeis.ParallelPermutationSequence;
 
 /**
- * A393394 Number of separable derangements on [n].
+ * A393394.
  * @author Sean A. Irvine
  */
 public class A395725 extends ParallelPermutationSequence {
 
-  private long[] mRow = new long[0];
+  private AtomicLong[] mRow = new AtomicLong[0];
   private int mM = 0;
 
   /** Construct the sequence. */
@@ -21,31 +23,29 @@ public class A395725 extends ParallelPermutationSequence {
   protected long count(final int[] p) {
     for (int k = 0; k < p.length; ++k) {
       if (p[k] == p.length - 1) {
-        ++mRow[k];
+        mRow[k].incrementAndGet();
         break;
       }
     }
     return 0;
   }
 
-  /*
-  def avoids1324(p):
-
-    return not any(p[i]<p[k]<p[j]<p[l] for i, j, k, l in combinations(range(len(p)), 4))
-   */
-
   @Override
   protected boolean accept(final int[] p, final int sum, final int pos) {
-    if (pos < 4) {
+    if (pos < 3) {
       return true;
     }
     final int l = pos - 1;
-    // p[i] < p[k] < p[j] < p[l]
+    // test for i<j<k<l with p[i] < p[k] < p[j] < p[l]
     for (int k = 2; k < l; ++k) {
-      for (int j = 1; j < k; ++j) {
-        for (int i = 0; i < j; ++i) {
-          if (p[i] < p[k] && p[k] < p[j] && p[j] < p[l]) {
-            return false;
+      if (p[k] < p[l]) {
+        for (int j = 1; j < k; ++j) {
+          if (p[j] < p[l] && p[k] < p[j]) {
+            for (int i = 0; i < j; ++i) {
+              if (p[i] < p[k]) {
+                return false;
+              }
+            }
           }
         }
       }
@@ -56,10 +56,13 @@ public class A395725 extends ParallelPermutationSequence {
   @Override
   public Z next() {
     if (++mM >= mRow.length) {
-      mRow = new long[mN + 1];
+      mRow = new AtomicLong[mN + 1];
+      for (int k = 0; k < mRow.length; ++k) {
+        mRow[k] = new AtomicLong();
+      }
       super.next(); // trigger the parallel search
       mM = 0;
     }
-    return Z.valueOf(mRow[mM]);
+    return Z.valueOf(mRow[mM].longValue());
   }
 }

21fbf6ab955b698c334a928d43085c178af0f0b9
Sean A. Irvine <sairvin@gmail.com>
2026-05-27 08:24:36 +1200

    Sequences

---
+++ src/irvine/oeis/a396/A396177.java
@@ -0,0 +1,16 @@
+package irvine.oeis.a396;
+
+import irvine.oeis.DifferenceSequence;
+
+/**
+ * A396176 allocated for N. J. A. Sloane.
+ * @author Sean A. Irvine
+ */
+public class A396177 extends DifferenceSequence {
+
+  /** Construct the sequence. */
+  public A396177() {
+    super(new A396176());
+  }
+}
+

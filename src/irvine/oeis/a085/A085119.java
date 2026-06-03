1aabc2a101746fa1be9da15118e1b79f01fc4d94
Sean A. Irvine <sairvin@gmail.com>
2026-06-04 11:05:47 +1200

    A085119

---
+++ src/irvine/oeis/a085/A085119.java
@@ -0,0 +1,41 @@
+package irvine.oeis.a085;
+
+import java.util.HashSet;
+
+import irvine.math.MemoryFunctionInt2;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence2;
+
+/**
+ * A395764.
+ * @author Sean A. Irvine
+ */
+public class A085119 extends Sequence2 {
+
+  private long mN = 1;
+
+  @Override
+  public Z next() {
+    if (++mN == 1969) {
+      return Z.NEG_ONE; // Only known value that does not stabilize
+    }
+    final MemoryFunctionInt2<Long> acker = new MemoryFunctionInt2<>() {
+      @Override
+      protected Long compute(final int i, final int j) {
+        return i == 0 ? (j + 1) % mN : j == 0 ? get(i - 1, 1) : get(i - 1, get(i, j - 1));
+      }
+    };
+    long i = -1;
+    while (true) {
+      ++i;
+      final HashSet<Long> seen = new HashSet<>();
+      long j = -1;
+      while (seen.add(acker.get(i, ++j))) {
+        // do nothing
+      }
+      if (seen.size() == 1) {
+        return Z.valueOf(seen.iterator().next());
+      }
+    }
+  }
+}

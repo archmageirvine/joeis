55279ed37764187951e3ca612fbe1bfff4a5ef26
Sean A. Irvine <sairvin@gmail.com>
2026-06-25 17:03:09 +1200

    Remove Conjectural status from now proven formulas

--- src/irvine/oeis/a249/A249458.java
+++ src/irvine/oeis/a249/A249458.java
@@ -1,13 +1,12 @@
 package irvine.oeis.a249;
 
-import irvine.oeis.Conjectural;
 import irvine.oeis.recur.LinearRecurrence;
 
 /**
  * A249458 The numerators of curvatures of touching circles inscribed in a special way in the smaller segment of unit circle divided by a chord of length sqrt(84)/5.
  * @author Sean A. Irvine
  */
-public class A249458 extends LinearRecurrence implements Conjectural {
+public class A249458 extends LinearRecurrence {
 
 
   /** Construct the sequence. */

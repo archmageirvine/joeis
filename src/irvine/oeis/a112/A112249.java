ba4202c0b976d80155be76b0643c116c4baffe88
Sean A. Irvine <sairvin@gmail.com>
2026-07-04 08:53:06 +1200

    Sequences

---
+++ src/irvine/oeis/a112/A112249.java
@@ -0,0 +1,16 @@
+package irvine.oeis.a112;
+
+import irvine.math.LongUtils;
+import irvine.oeis.FilterNumberSequence;
+
+/**
+ * A112251 Numbers m such that m mod log_2(m) = 1.
+ * @author Sean A. Irvine
+ */
+public class A112249 extends FilterNumberSequence {
+
+  /** Construct the sequence. */
+  public A112249() {
+    super(1, 2, k -> k % LongUtils.floorLog2(k) == 0);
+  }
+}
---
+++ src/irvine/oeis/a397/A397372.java
@@ -0,0 +1,24 @@
+package irvine.oeis.a397;
+
+import irvine.math.q.Q;
+import irvine.math.series.Hypergeometric;
+import irvine.math.series.Series;
+import irvine.math.series.SeriesRing;
+import irvine.math.z.Z;
+import irvine.oeis.Sequence0;
+
+/**
+ * A000523.
+ * @author Sean A. Irvine
+ */
+public class A397372 extends Sequence0 {
+
+  private static final SeriesRing<Q> SQ = SeriesRing.SQ;
+  private long mN = -1;
+  private final Series<Q> mS = SQ.pow(Hypergeometric.s(new Q[] {Q.ONE_QUARTER, Q.ONE_QUARTER}, new Q[] {Q.ONE}, SQ.x()), 4);
+
+  @Override
+  public Z next() {
+    return mS.coeff(++mN).multiply(Z.ONE.shiftLeft(6 * mN)).floor();
+  }
+}

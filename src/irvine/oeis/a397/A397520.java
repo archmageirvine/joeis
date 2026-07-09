c5cf4b682ac9d1e22f3e4a6a25128867310a2100
Sean A. Irvine <sairvin@gmail.com>
2026-07-09 14:19:54 +1200

    Knights and more knights

--- src/irvine/oeis/a397/A397520.java
+++ src/irvine/oeis/a397/A397520.java
@@ -13,11 +13,18 @@ import irvine.util.array.LongDynamicBooleanArray;
  */
 public class A397520 extends Sequence0 {
 
-  // WIP, todo dead end's at the moment
+  private static final int NO_BIT = 0;
+  private static final int MATCHED = 1;
+  private static final int ALREADY_MATCHED = 2;
+  private static final long STUCK = -1;
+
+  // Lookahead of 2 is enough for the influence of a newly created knight to disappear
+  // (note this search ahead depth is independent of mA, mB previous rows)
+  private static final int LOOKAHEAD = 2;
 
   private static final class Row {
-    long mBits;
-    Map<Integer, Byte> mDegree;
+    private final long mBits;
+    private final Map<Integer, Byte> mDegree;
 
     private Row(final Row row) {
       mBits = row.mBits;
@@ -30,28 +37,28 @@ public class A397520 extends Sequence0 {
       long x = mBits;
       while (x != 0) {
         final int b = Long.numberOfTrailingZeros(x);
-        mDegree.put(b, (byte) 0);
+        mDegree.put(b, (byte) NO_BIT);
         x &= x - 1;
       }
     }
 
-    boolean hasBit(final int bit) {
-      return bit >= 0 && bit < Long.SIZE && mDegree.containsKey(bit);
+    private boolean hasBit(final int bit) {
+      return bit >= NO_BIT && bit < Long.SIZE && mDegree.containsKey(bit);
     }
 
-    int markMatched(final int bit) {
+    private int markMatched(final int bit) {
       final Byte d = mDegree.get(bit);
       if (d == null) {
-        return 0;              // no bit here
+        return NO_BIT;
       }
-      if (d != 0) {
-        return 2;              // already matched
+      if (d != NO_BIT) {
+        return ALREADY_MATCHED;
       }
-      mDegree.put(bit, (byte) 1);
-      return 1;                // newly matched
+      mDegree.put(bit, (byte) MATCHED);
+      return MATCHED;
     }
 
-    boolean allMatched() {
+    private boolean allMatched() {
       for (final byte d : mDegree.values()) {
         if (d != 1) {
           return false;
@@ -62,25 +69,45 @@ public class A397520 extends Sequence0 {
 
     @Override
     public String toString() {
-      return Long.toBinaryString(mBits) + " " + mDegree;
+      return mBits + " " + Long.toBinaryString(mBits) + " " + mDegree;
     }
   }
 
-
+  // Overall tracking of used values
   private final LongDynamicBooleanArray mUsed = new LongDynamicBooleanArray();
   private long mLeastUnused = 2;
+
+  // Previous two rows (the only rows that can contain unmatched knights)
   private Row mA = null;
   private Row mB = null;
 
-  private static boolean tryMatch(final Row x, final int bx,
-                                  final Row y, final int by) {
+  private static boolean isBlocked(final Row a, final Row b) {
+    for (final Map.Entry<Integer, Byte> e : a.mDegree.entrySet()) {
+      if (e.getValue() == NO_BIT) {
+        // We have an unmatched knight in a that we must match
+        final int pos = e.getKey();
+        if (
+          // try pos+1
+          (pos >= Long.SIZE || b.mDegree.containsKey(pos - 1) || b.mDegree.containsKey(pos + 3) || a.mDegree.containsKey(pos + 2))
+            // try pos-1
+            && (pos <= 0 || b.mDegree.containsKey(pos + 1) || b.mDegree.containsKey(pos - 3) || a.mDegree.containsKey(pos - 2))
+        ) {
+          // A knight in b prevents us from matching the knight in row a
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  private static boolean tryMatch(final Row x, final int bx, final Row y, final int by) {
     if (!y.hasBit(by)) {
       return true;
     }
-    if (x.markMatched(bx) != 1) {
+    if (x.markMatched(bx) != MATCHED) {
       return false;
     }
-    return y.markMatched(by) == 1;
+    return y.markMatched(by) == MATCHED;
   }
 
   private boolean process(final Row a, final Row b, final Row c) {
@@ -101,19 +128,40 @@ public class A397520 extends Sequence0 {
       }
       x &= x - 1;
     }
-    return a.allMatched();
+    return a.allMatched() && !isBlocked(b, c);
   }
 
   private void accept(final long k) {
     final Row c = new Row(k);
-    process(mA, mB, c);   // should always succeed
+    if (!process(mA, mB, c)) {
+      // This should never happen because we already checked k is valid
+      throw new RuntimeException();
+    }
     mUsed.set(k);
     mA = mB;
     mB = c;
   }
 
-  private boolean ok(final long k) {
-    return process(new Row(mA), new Row(mB), new Row(k));
+  private long extend(final Row a, final Row b, final int depth) {
+    long k = mLeastUnused;
+    while (true) {
+      if (!mUsed.isSet(k)) {
+        // Note "process" mutates contents of a and b, so we make copies
+        final Row a2 = new Row(a);
+        final Row b2 = new Row(b);
+        final Row c = new Row(k);
+        if (process(a2, b2, c)) {
+          if (depth == 1 || extend(b2, c, depth - 1) != STUCK) {
+            return k;
+          }
+        }
+      }
+      ++k;
+      // Conservative stuck test
+      if (k > 8 * mUsed.length()) {
+        return STUCK;
+      }
+    }
   }
 
   @Override
@@ -128,18 +176,15 @@ public class A397520 extends Sequence0 {
       mB = new Row(1);
       return Z.ONE;
     }
+    //System.out.println(mA + " " + mB + " blocked=" + isBlocked(mA, mB));
     while (mUsed.isSet(mLeastUnused)) {
       ++mLeastUnused;
     }
-    long k = mLeastUnused;
-    while (true) {
-      //System.out.println("Consider " + k + " " + ok(k));
-      if (!mUsed.isSet(k) && ok(k)) {
-        accept(k);
-        System.out.println(k + " " + Long.toBinaryString(k) + " " + mA + " " + mB);
-        return Z.valueOf(k);
-      }
-      ++k;
+    long k = extend(mA, mB, LOOKAHEAD);
+    if (k == STUCK) {
+      throw new RuntimeException();
     }
+    accept(k);
+    return Z.valueOf(k);
   }
 }

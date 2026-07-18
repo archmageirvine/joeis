fd82a5f79281f33bda2f0a7770ac2643e8b5a686
Sean A. Irvine <sairvin@gmail.com>
2026-07-19 07:53:29 +1200

    Sequences

--- src/irvine/oeis/a079/A079050.java
+++ src/irvine/oeis/a079/A079050.java
@@ -2,15 +2,15 @@ package irvine.oeis.a079;
 
 import irvine.math.function.Functions;
 import irvine.math.z.Z;
-import irvine.oeis.Sequence1;
+import irvine.oeis.Sequence0;
 
 /**
  * A079050 Sum of the digits of LookAndSay(n).
  * @author Sean A. Irvine
  */
-public class A079050 extends Sequence1 {
+public class A079050 extends Sequence0 {
 
-  private long mN = 0;
+  private long mN = -1;
 
   @Override
   public Z next() {

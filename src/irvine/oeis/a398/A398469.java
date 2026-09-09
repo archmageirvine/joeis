4175d49925e8d46e7aee15f30bb2a00bf2ddf83f
Sean A. Irvine <sairvin@gmail.com>
2026-09-10 11:45:29 +1200

    WIP 399

--- src/irvine/oeis/a398/A398469.java
+++ src/irvine/oeis/a398/A398469.java
@@ -1,6 +1,6 @@
 package irvine.oeis.a398;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 import irvine.math.q.Q;
 import irvine.math.z.Z;
@@ -18,17 +18,15 @@ public class A398469 extends Sequence1 {
   private static final long TERMINATED = -1;
   private final LongDynamicLongArray mPrevPos = new LongDynamicLongArray();
   private final LongDynamicLongArray mNextPos = new LongDynamicLongArray();
-  private final HashSet<Long> mForbidden = new HashSet<>();
+  private final HashMap<Long, Long> mForbidden = new HashMap<>();
   private long mN = 0;
 
-  private boolean isClaimed(final long v) {
-    return mForbidden.contains(v);
-//    for (long k = 0; k < mNextPos.length(); ++k) {
-//      if (mNextPos.get(k) == v) {
-//        return true;
-//      }
-//    }
-//    return false;
+  private void sanity(final long pos, final long k) {
+    for (long j = k + 1; j < mNextPos.length(); ++j) {
+      if (mNextPos.get(j) == pos) {
+        System.out.println("Multiple values have claimed " + pos);
+      }
+    }
   }
 
   @Override
@@ -39,34 +37,21 @@ public class A398469 extends Sequence1 {
     while (true) {
       final long pos = mNextPos.get(++k);
       if (pos == mN) {
+        //sanity(pos, k);
         final long prev = mPrevPos.get(k);
         final Q next = new Q(mN * mN, prev);
         mPrevPos.set(k, pos);
         if (next.isInteger()) {
           final long v = next.toZ().longValueExact();
           mNextPos.set(k, v);
-          mForbidden.add(v);
-//          final Q s = new Q(mN * v, prev);
-//          if (s.isInteger()) {
-//            mForbidden.add(s.toZ().longValueExact());
-//          }
-//          long x = v;
-//          for (long h = 0; h < 10; ++h) {
-//            mForbidden.add(x);
-//            final Q g = new Q(mN * x, prev);
-//            if (!g.isInteger()) {
-//              break;
-//            }
-//            x = g.toZ().longValueExact();
-//          }
+          final Long exists = mForbidden.get(v);
+          if (exists == null || exists == k) {
+            mForbidden.put(v, k);
+          } else {
+            mNextPos.set(k, TERMINATED);
+          }
         } else {
           mNextPos.set(k, TERMINATED);
-        }
-        // todo I think this next loop is pointless
-        for (long m = k + 1; m < mNextPos.length(); ++m) {
-          if (mNextPos.get(m) == mN) {
-            mNextPos.set(m, TERMINATED);
-          }
         }
         return Z.valueOf(k);
       } else if (pos == 0) {
@@ -75,28 +60,32 @@ public class A398469 extends Sequence1 {
         // Find a viable ratio
         long j = mN;
         while (true) {
-          final Q r = new Q(++j * mN, mN);
+          final Q ratio = new Q(++j, mN);
+          final Q r = ratio.multiply(mN);
           if (r.isInteger()) {
             final long v = r.toZ().longValueExact();
-            if (!isClaimed(v)) {
-              final Q s = new Q(j * v, mN);
+            if (!mForbidden.containsKey(v)) {
+              final Q s = ratio.multiply(v);
               if (s.isInteger()) {
                 final long w = s.toZ().longValueExact();
-                if (!isClaimed(w)) {
+                if (!mForbidden.containsKey(w)) {
                   mNextPos.set(k, v);
-                  if (mForbidden.add(v) && mForbidden.add(w)) {
-
-//                  long x = w;
-//                  for (long h = 0; h < 10; ++h) {
-//                    mForbidden.add(x);
-//                    final Q g = new Q(j * x, mN);
-//                    if (!g.isInteger()) {
-//                      break;
-//                    }
-//                    x = g.toZ().longValueExact();
-//                  }
-                    return Z.valueOf(k);
+                  // Reserve next two positions in this progression to ensure overall length >= 3
+                  mForbidden.put(v, k);
+                  mForbidden.put(w, k);
+                  if (!ratio.isInteger()) {
+                    // Generate entire progression
+                    long x = w;
+                    while (true) {
+                      final Q y = ratio.multiply(x);
+                      if (!y.isInteger()) {
+                        break;
+                      }
+                      x = y.toZ().longValueExact();
+                      mForbidden.put(x, k);
+                    }
                   }
+                  return Z.valueOf(k);
                 }
               }
             }
